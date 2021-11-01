7218f6a512dfd2e420356c7244074cb1bc7574e3
David Andrs <andrsd@gmail.com>
2021-11-01 08:27:54 -0600

    Adding GPetscLinearProblem

---
+++ include/problems/GPetscLinearProblem.h
@@ -0,0 +1,66 @@
+#pragma once
+
+#include "problems/GProblem.h"
+#include "petscksp.h"
+
+/// PETSc Linear problem
+///
+class GPetscLinearProblem : public GProblem
+{
+public:
+    GPetscLinearProblem(const InputParameters & parameters);
+    virtual ~GPetscLinearProblem();
+
+    virtual void create() override;
+    virtual void solve() override;
+    virtual bool converged() override;
+
+protected:
+    /// provide DM for the underlying KSP object
+    virtual const DM & getDM() = 0;
+    /// setup the problem to be solved
+    virtual void setupProblem() = 0;
+    /// Allocate Jacobian/residual objects
+    virtual void allocateObjects();
+    /// Setup computation of residual and Jacobian callbacks
+    virtual void setupCallbacks();
+    /// Setup monitors
+    virtual void setupMonitors();
+    /// Setup solver parameters
+    virtual void setupSolverParameters();
+    /// output
+    virtual void out() override;
+    /// Method to compute right-hand side. Called from the PETsc callback
+    virtual PetscErrorCode computeRhsCallback(Vec b);
+    /// Method to compute operators. Called from the PETsc callback
+    virtual PetscErrorCode computeOperatorsCallback(Mat A, Mat B);
+    /// KSP monitor
+    PetscErrorCode kspMonitorCallback(PetscInt it, PetscReal rnorm);
+
+    /// KSP object
+    KSP ksp;
+    /// The solution vector
+    Vec x;
+    /// The right-hand side vector
+    Vec b;
+    /// Linear operator matrix
+    Mat A;
+    /// Preconditioning matrix
+    Mat B;
+    /// Converged reason
+    KSPConvergedReason converged_reason;
+
+    /// Relative convergence tolerance for the linear solver
+    PetscReal lin_rel_tol;
+    /// Absolute convergence tolerance for the linear solver
+    PetscReal lin_abs_tol;
+    /// Maximum number of iterations for the linear solver
+    PetscInt lin_max_iter;
+
+public:
+    static InputParameters validParams();
+
+    friend PetscErrorCode __compute_rhs(KSP ksp, Vec b, void *ctx);
+    friend PetscErrorCode __compute_operators(KSP ksp, Mat A, Mat B, void *ctx);
+    friend PetscErrorCode __ksp_monitor_linear(KSP ksp, PetscInt it, PetscReal rnorm, void *ctx);
+};
---
+++ src/problems/GPetscLinearProblem.cpp
@@ -0,0 +1,180 @@
+#include "problems/GPetscLinearProblem.h"
+#include "base/CallStack.h"
+#include "mesh/GMesh.h"
+#include "utils/MooseUtils.h"
+
+PetscErrorCode
+__compute_rhs(KSP ksp, Vec b, void *ctx)
+{
+    GPetscLinearProblem * problem = static_cast<GPetscLinearProblem *>(ctx);
+    return problem->computeRhsCallback(b);
+}
+
+PetscErrorCode
+__compute_operators(KSP ksp, Mat A, Mat B, void *ctx)
+{
+    GPetscLinearProblem * problem = static_cast<GPetscLinearProblem *>(ctx);
+    return problem->computeOperatorsCallback(A, B);
+}
+
+PetscErrorCode
+__ksp_monitor_linear(KSP ksp, PetscInt it, PetscReal rnorm, void *ctx)
+{
+    GPetscLinearProblem * problem = static_cast<GPetscLinearProblem *>(ctx);
+    return problem->kspMonitorCallback(it, rnorm);
+}
+
+
+InputParameters
+GPetscLinearProblem::validParams()
+{
+    InputParameters params = GProblem::validParams();
+    params.addParam<PetscReal>("lin_rel_tol", 1e-5, "Relative convergence tolerance for the linear solver");
+    params.addParam<PetscReal>("lin_abs_tol", 1e-50, "Absolute convergence tolerance for the linear solver");
+    params.addParam<PetscInt>("lin_max_iter", 10000, "Maximum number of iterations for the linear solver");
+    return params;
+}
+
+GPetscLinearProblem::GPetscLinearProblem(const InputParameters & parameters) :
+    GProblem(parameters),
+    lin_rel_tol(getParam<PetscReal>("lin_rel_tol")),
+    lin_abs_tol(getParam<PetscReal>("lin_abs_tol")),
+    lin_max_iter(getParam<PetscInt>("lin_max_iter"))
+{
+    _F_;
+}
+
+GPetscLinearProblem::~GPetscLinearProblem()
+{
+    _F_;
+    KSPDestroy(&this->ksp);
+    VecDestroy(&this->b);
+    VecDestroy(&this->x);
+    if (this->A != this->B)
+        MatDestroy(&this->B);
+    MatDestroy(&this->A);
+}
+
+void
+GPetscLinearProblem::create()
+{
+    _F_;
+    PetscErrorCode ierr;
+    const DM & dm = getDM();
+
+    ierr = KSPCreate(comm().get(), &this->ksp);
+    ierr = KSPSetDM(this->ksp, dm);
+    ierr = DMSetApplicationContext(dm, this);
+
+    setupProblem();
+    allocateObjects();
+
+    setupSolverParameters();
+    setupMonitors();
+    setupCallbacks();
+}
+
+void
+GPetscLinearProblem::allocateObjects()
+{
+    _F_;
+    PetscErrorCode ierr;
+    const DM & dm = getDM();
+
+    ierr = DMCreateGlobalVector(dm, &this->b);
+    ierr = PetscObjectSetName((PetscObject) this->b, "");
+
+    ierr = DMCreateMatrix(dm, &this->A);
+    ierr = PetscObjectSetName((PetscObject) this->A, "");
+    // TODO: Add API for setting up preconditioners
+    this->A = this->B;
+
+    ierr = DMCreateGlobalVector(dm, &this->x);
+    ierr = PetscObjectSetName((PetscObject) this->x, "");
+}
+
+void
+GPetscLinearProblem::setupCallbacks()
+{
+    _F_;
+    PetscErrorCode ierr;
+
+    ierr = KSPSetComputeRHS(this->ksp, __compute_rhs, this);
+    ierr = KSPSetComputeOperators(this->ksp, __compute_operators, this);
+}
+
+void
+GPetscLinearProblem::setupMonitors()
+{
+    _F_;
+    PetscErrorCode ierr;
+
+    ierr = KSPMonitorSet(this->ksp, __ksp_monitor_linear, this, 0);
+}
+
+void
+GPetscLinearProblem::setupSolverParameters()
+{
+    _F_;
+    PetscErrorCode ierr;
+
+    ierr = KSPSetTolerances(this->ksp,
+        this->lin_rel_tol, this->lin_abs_tol, PETSC_DEFAULT,
+        this->lin_max_iter);
+    ierr = KSPSetFromOptions(ksp);
+}
+
+PetscErrorCode
+GPetscLinearProblem::computeRhsCallback(Vec b)
+{
+    _F_;
+    return 0;
+}
+
+PetscErrorCode
+GPetscLinearProblem::computeOperatorsCallback(Mat A, Mat B)
+{
+    _F_;
+    return 0;
+}
+
+PetscErrorCode
+GPetscLinearProblem::kspMonitorCallback(PetscInt it, PetscReal rnorm)
+{
+    _F_;
+    godzillaPrint(8, it, " Linear residual: ", std::scientific, rnorm);
+    return 0;
+}
+
+void
+GPetscLinearProblem::solve()
+{
+    _F_;
+    PetscErrorCode ierr;
+
+    ierr = KSPSolve(this->ksp, this->b, this->x);
+    ierr = KSPGetConvergedReason(this->ksp, &this->converged_reason);
+}
+
+bool
+GPetscLinearProblem::converged()
+{
+    _F_;
+    bool conv =
+        this->converged_reason == KSP_CONVERGED_RTOL_NORMAL ||
+        this->converged_reason == KSP_CONVERGED_ATOL_NORMAL ||
+        this->converged_reason == KSP_CONVERGED_RTOL ||
+        this->converged_reason == KSP_CONVERGED_ATOL ||
+        this->converged_reason == KSP_CONVERGED_ITS ||
+        this->converged_reason == KSP_CONVERGED_CG_NEG_CURVE ||
+        this->converged_reason == KSP_CONVERGED_CG_CONSTRAINED ||
+        this->converged_reason == KSP_CONVERGED_STEP_LENGTH ||
+        this->converged_reason == KSP_CONVERGED_HAPPY_BREAKDOWN;
+    return conv;
+}
+
+void
+GPetscLinearProblem::out()
+{
+    _F_;
+}
