5b1cd63abefb987f2c3977f34a622a296ae5daac
David Andrs <andrsd@gmail.com>
2021-11-06 12:43:59 -0600

    Run tests in threadsafe mode
    So the DEATH tests do not report warnings

--- test/src/main.cpp
+++ test/src/main.cpp
@@ -6,5 +6,6 @@ int main(int argc, char **argv)
 {
     ::testing::InitGoogleTest(&argc, argv);
     godzilla::Init init(argc, argv);
+    testing::GTEST_FLAG(death_test_style) = "threadsafe";
     return RUN_ALL_TESTS();
 }
