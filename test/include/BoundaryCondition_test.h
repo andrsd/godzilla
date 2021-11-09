3583b97d81390ae4a7eecd5866b4563a719804d2
David Andrs <andrsd@gmail.com>
2021-11-09 07:22:40 -0700

    Adding BoundaryCondition test

---
+++ test/include/BoundaryCondition_test.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "Factory.h"
+#include "gmock/gmock.h"
+#include "BoundaryCondition.h"
+#include "GodzillaApp_test.h"
+
+
+using namespace godzilla;
+
+class MockBoundaryCondition : public BoundaryCondition
+{
+public:
+    MockBoundaryCondition(const InputParameters & params) : BoundaryCondition(params) {}
+
+    MOCK_METHOD(PetscInt, getNumComponents, (), (const));
+    MOCK_METHOD((std::vector<DMBoundaryConditionType>), getBcType, (), (const));
+    MOCK_METHOD(void, evaluate, (PetscInt, PetscReal, const PetscReal x[], PetscInt Nc, PetscScalar u[]), ());
+};
+
+
+class BoundaryConditionTest : public GodzillaAppTest {
+protected:
+    MockBoundaryCondition *
+    gBoundaryCondition()
+    {
+        const std::string class_name = "MockBoundaryCondition";
+        InputParameters params = Factory::getValidParams(class_name);
+        params.set<const App *>("_app") = this->app;
+        params.set<std::vector<std::string>>("boundary") = {"side1"};
+        return Factory::create<MockBoundaryCondition>(class_name, "obj", params);
+    }
+};
---
+++ test/src/BoundaryCondition_test.cpp
@@ -0,0 +1,31 @@
+#include "GodzillaApp_test.h"
+#include "BoundaryCondition_test.h"
+#include "InputParameters.h"
+#include "petsc.h"
+
+
+using namespace godzilla;
+
+registerObject(MockBoundaryCondition);
+
+TEST_F(BoundaryConditionTest, api)
+{
+    auto bc = gBoundaryCondition();
+
+    EXPECT_EQ(bc->getFieldId(), 0);
+    EXPECT_EQ(bc->getBoundary().size(), 1);
+    EXPECT_EQ(bc->getBoundary()[0], "side1");
+}
+
+TEST_F(BoundaryConditionTest, evaluate)
+{
+    auto bc = gBoundaryCondition();
+
+    PetscInt dim = 2;
+    PetscReal time = 0.;
+    PetscReal x[] = { 0 };
+    PetscInt Nc = 3;
+    PetscScalar u[] = { 0, 0, 0};
+    EXPECT_CALL(*bc, evaluate(dim, time, x, Nc, u)).Times(1);
+    __boundary_condition_function(dim, time, x, Nc, u, bc);
+}
