0fd6cf4079d6b60233b0d662206aeb4d70ab8983
David Andrs <andrsd@gmail.com>
2021-11-23 18:53:15 -0700

    Adding a test: BC with a user-defined function

--- test/src/DirichletBC_test.cpp
+++ test/src/DirichletBC_test.cpp
@@ -2,6 +2,7 @@
 #include "Factory.h"
 #include "App.h"
 #include "DirichletBC.h"
+#include "PiecewiseLinear.h"
 
 using namespace godzilla;
 
@@ -27,4 +28,49 @@ TEST(DirichletBC, api)
     __boundary_condition_function(dim, time, x, Nc, u, obj);
 
     EXPECT_EQ(u[0], 37.5);
+}
+
+TEST(DirichletBC, with_user_defined_fn)
+{
+    class TestApp : public App {
+    public:
+        TestApp() : App("test", MPI_COMM_WORLD) {}
+
+        void
+        addFunction(Function * fn)
+        {
+            this->functions.push_back(fn);
+        }
+    } app;
+
+    Function * fn;
+    BoundaryCondition * bc;
+    {
+        std::vector<PetscReal> x = { 0., 1. };
+        std::vector<PetscReal> y = { 1., 2. };
+        std::string class_name = "PiecewiseLinear";
+        InputParameters & params = Factory::getValidParams(class_name);
+        params.set<const App *>("_app") = &app;
+        params.set<std::vector<PetscReal>>("x") = x;
+        params.set<std::vector<PetscReal>>("y") = y;
+        fn = Factory::create<PiecewiseLinear>(class_name, "ipol", params);
+        app.addFunction(fn);
+    }
+
+    {
+        InputParameters & params = Factory::getValidParams("DirichletBC");
+        params.set<const App *>("_app") = &app;
+        params.set<std::vector<std::string>>("value") = { "ipol(x)" };
+        bc = Factory::create<BoundaryCondition>("DirichletBC", "name", params);
+    }
+    bc->create();
+
+    PetscInt dim = 1;
+    PetscReal time = 0;
+    PetscReal x[] = { 0.5 };
+    PetscInt Nc = 1;
+    PetscScalar u[] = { 0 };
+    __boundary_condition_function(dim, time, x, Nc, u, bc);
+
+    EXPECT_EQ(u[0], 1.5);
 }
