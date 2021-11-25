07fd1a849a3e6524042ee81eaf65069ef6c0ec3e
David Andrs <andrsd@gmail.com>
2021-11-25 08:50:33 -0700

    Doco for CmdLineArgParser

--- include/CmdLineArgParser.h
+++ include/CmdLineArgParser.h
@@ -7,16 +7,27 @@ namespace godzilla {
 /// Wrapper class around [TCLAP](https://sourceforge.net/projects/tclap/) (Templated Command Line
 /// Argument Parser)
 ///
-///
 class CmdLineArgParser {
 public:
+    /// Construct a command line parser object
+    ///
+    /// @param prog_name The name of the binary (shown when printing usage)
+    /// @param version The version of the binary
     CmdLineArgParser(const std::string & prog_name, const std::string & version = "none");
 
+    /// Adds an argument
+    ///
+    /// @param a Comand line parser argument
     void add(TCLAP::Arg & a);
 
+    /// Parse the command line
+    ///
+    /// @param argc The number of arguments
+    /// @param argv The command line parameters
     void parse(int argc, const char * const * argv);
 
 protected:
+    /// TCALP command line object
     TCLAP::CmdLine cmd;
 };
 
