3db8c341f2a4bad2a9d06e0b5b814e6a796bbb67
David Andrs <andrsd@gmail.com>
2021-11-28 05:30:28 -0700

    heat-eqn example: Start of convective heat flux BC

---
+++ examples/heat-eqn/include/ConvectiveHeatFluxConstBC.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "NaturalBC.h"
+
+namespace godzilla {
+
+class ConvectiveHeatFluxConstBC : public NaturalBC {
+public:
+    ConvectiveHeatFluxConstBC(const InputParameters & params);
+
+    virtual PetscInt getFieldID() const;
+    virtual PetscInt getNumComponents() const;
+    virtual std::vector<PetscInt> getComponents() const;
+
+protected:
+    virtual void onSetWeakForm();
+
+    /// Convective heat transfer coefficient
+    const PetscReal & htc;
+
+    /// Ambient temperature
+    const PetscReal & T_infinity;
+
+public:
+    static InputParameters validParams();
+};
+
+} // namespace godzilla
---
+++ examples/heat-eqn/src/ConvectiveHeatFluxConstBC.cpp
@@ -0,0 +1,105 @@
+#include "Godzilla.h"
+#include "ConvectiveHeatFluxConstBC.h"
+#include "CallStack.h"
+
+namespace godzilla {
+
+registerObject(ConvectiveHeatFluxConstBC);
+
+void
+__f0_convective_heat_flux_const_bc(PetscInt dim,
+                                   PetscInt nf,
+                                   PetscInt nf_aux,
+                                   const PetscInt u_off[],
+                                   const PetscInt u_off_x[],
+                                   const PetscScalar u[],
+                                   const PetscScalar u_t[],
+                                   const PetscScalar u_x[],
+                                   const PetscInt a_off[],
+                                   const PetscInt a_off_x[],
+                                   const PetscScalar a[],
+                                   const PetscScalar a_t[],
+                                   const PetscScalar a_x[],
+                                   PetscReal t,
+                                   const PetscReal x[],
+                                   const PetscReal n[],
+                                   PetscInt num_constants,
+                                   const PetscScalar constants[],
+                                   PetscScalar f0[])
+{
+    PetscReal htc = 100;
+    PetscReal T_infinity = 400;
+    f0[0] = htc * (u[0] - T_infinity);
+}
+
+void
+__g0_convective_heat_flux_const_bc(PetscInt dim,
+                                   PetscInt nf,
+                                   PetscInt nf_aux,
+                                   const PetscInt u_off[],
+                                   const PetscInt u_off_x[],
+                                   const PetscScalar u[],
+                                   const PetscScalar u_t[],
+                                   const PetscScalar u_x[],
+                                   const PetscInt a_off[],
+                                   const PetscInt a_off_x[],
+                                   const PetscScalar a[],
+                                   const PetscScalar a_t[],
+                                   const PetscScalar a_x[],
+                                   PetscReal t,
+                                   PetscReal u_t_shift,
+                                   const PetscReal x[],
+                                   const PetscReal n[],
+                                   PetscInt num_constants,
+                                   const PetscScalar constants[],
+                                   PetscScalar g0[])
+{
+    PetscReal htc = 100;
+    g0[0] = htc;
+}
+
+InputParameters
+ConvectiveHeatFluxConstBC::validParams()
+{
+    InputParameters params = NaturalBC::validParams();
+    params.addRequiredParam<PetscReal>("htc", "Convective heat transfer coefficient");
+    params.addRequiredParam<PetscReal>("T_infinity", "Ambient temperature");
+    return params;
+}
+
+ConvectiveHeatFluxConstBC::ConvectiveHeatFluxConstBC(const InputParameters & params) :
+    NaturalBC(params),
+    htc(getParam<PetscReal>("htc")),
+    T_infinity(getParam<PetscReal>("T_infinity"))
+{
+    _F_;
+}
+
+PetscInt
+ConvectiveHeatFluxConstBC::getFieldID() const
+{
+    return 0;
+}
+
+PetscInt
+ConvectiveHeatFluxConstBC::getNumComponents() const
+{
+    return 1;
+}
+
+std::vector<PetscInt>
+ConvectiveHeatFluxConstBC::getComponents() const
+{
+    std::vector<PetscInt> comps = { 0 };
+    return comps;
+}
+
+void
+ConvectiveHeatFluxConstBC::onSetWeakForm()
+{
+    _F_;
+    setResidualBlock(__f0_convective_heat_flux_const_bc, nullptr);
+    setJacobianBlock(getFieldID(), __g0_convective_heat_flux_const_bc, nullptr, nullptr, nullptr);
+}
+
+} // namespace godzilla
