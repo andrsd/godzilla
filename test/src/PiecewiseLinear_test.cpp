fb20add301020a78e4baa19cfd0be5c4a38b5db7
David Andrs <andrsd@gmail.com>
2021-12-06 18:31:22 -0700

    Updating PiecewiseLinear tests

--- test/src/PiecewiseLinear_test.cpp
+++ test/src/PiecewiseLinear_test.cpp
@@ -1,16 +1,23 @@
+#include "gmock/gmock.h"
 #include "Godzilla.h"
-#include "PiecewiseLinear_test.h"
+#include "GodzillaApp_test.h"
+#include "PiecewiseLinear.h"
 
 using namespace godzilla;
 
-TEST_F(PiecewiseLinearTest, eval)
+TEST(PiecewiseLinearTest, eval)
 {
-    std::vector<PetscReal> x = { 1., 2., 3. };
-    std::vector<PetscReal> y = { 3., 1., 2. };
-    auto obj = buildPiecewiseLinear("ipol", x, y);
+    TestApp app;
+
+    InputParameters params = PiecewiseLinear::validParams();
+    params.set<const App *>("_app") = &app;
+    params.set<std::string>("_name") = "ipol";
+    params.set<std::vector<PetscReal>>("x") = { 1., 2., 3. };
+    params.set<std::vector<PetscReal>>("y") = { 3., 1., 2. };
+    PiecewiseLinear obj(params);
 
     mu::Parser parser;
-    obj->registerCallback(parser);
+    obj.registerCallback(parser);
 
     parser.SetExpr("ipol(0)");
     EXPECT_EQ(parser.Eval(), 3.);
