04e9dbfebc864e87c0cdd854fa2304f77589718e
David Andrs <andrsd@gmail.com>
2021-12-06 18:23:57 -0700

    Updating ExodusIIMesh tests

--- test/src/ExodusIIMesh_test.cpp
+++ test/src/ExodusIIMesh_test.cpp
@@ -1,40 +1,27 @@
+#include "gmock/gmock.h"
 #include "GodzillaConfig.h"
 #include "GodzillaApp_test.h"
-#include "ExodusIIMesh_test.h"
+#include "ExodusIIMesh.h"
 #include "InputParameters.h"
 #include "petsc.h"
 
 using namespace godzilla;
 
-registerObject(MockExodusIIMesh);
-
-TEST_F(ExodusIIMeshTest, gexodus_mesh)
+TEST(ExodusIIMeshTest, api)
 {
+    TestApp app;
     std::string file_name = std::string(GODZILLA_UNIT_TESTS_ROOT) + std::string("/assets/square.e");
-    auto obj = gExodusMesh(file_name);
 
-    EXPECT_EQ(obj->getFileName(), file_name);
-}
+    InputParameters params = ExodusIIMesh::validParams();
+    params.set<const App *>("_app") = &app;
+    params.set<std::string>("_name") = "obj";
+    params.set<std::string>("file") = file_name;
+    ExodusIIMesh mesh(params);
 
-TEST_F(ExodusIIMeshTest, gexodus_mesh_nonexitent_file)
-{
-    testing::internal::CaptureStderr();
+    EXPECT_EQ(mesh.getFileName(), file_name);
 
-    gExodusMesh("asdf.e");
-    this->app->checkIntegrity();
-
-    EXPECT_THAT(
-        testing::internal::GetCapturedStderr(),
-        testing::HasSubstr("obj: Unable to open 'asdf.e' for reading. Make sure it exists and you "
-                           "have read permissions."));
-}
-
-TEST_F(ExodusIIMeshTest, gexodus_mesh_create)
-{
-    std::string file_name = std::string(GODZILLA_UNIT_TESTS_ROOT) + std::string("/assets/square.e");
-    auto obj = gExodusMesh(file_name);
-    obj->create();
-    DM dm = obj->getDM();
+    mesh.create();
+    DM dm = mesh.getDM();
 
     PetscInt dim;
     DMGetDimension(dm, &dim);
@@ -53,4 +40,24 @@ TEST_F(ExodusIIMeshTest, gexodus_mesh_create)
     PetscInt n;
     VecGetSize(coords, &n);
     EXPECT_EQ(n, 18);
+}
+
+TEST(ExodusIIMeshTest, nonexitent_file)
+{
+    testing::internal::CaptureStderr();
+
+    TestApp app;
+
+    InputParameters params = ExodusIIMesh::validParams();
+    params.set<const App *>("_app") = &app;
+    params.set<std::string>("_name") = "obj";
+    params.set<std::string>("file") = "asdf.e";
+    ExodusIIMesh mesh(params);
+
+    app.checkIntegrity();
+
+    EXPECT_THAT(
+        testing::internal::GetCapturedStderr(),
+        testing::HasSubstr("obj: Unable to open 'asdf.e' for reading. Make sure it exists and you "
+                           "have read permissions."));
 }
