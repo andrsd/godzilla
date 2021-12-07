5e7816923ab57ce7b9674074b958301f6b5e10d9
David Andrs <andrsd@gmail.com>
2021-12-06 18:36:34 -0700

    Updating StructuredGrid1D tests

--- test/src/StructuredGrid1D_test.cpp
+++ test/src/StructuredGrid1D_test.cpp
@@ -1,23 +1,24 @@
 #include "GodzillaApp_test.h"
-#include "StructuredGrid1D_test.h"
+#include "StructuredGrid1D.h"
 #include "InputParameters.h"
 #include "petsc.h"
 
 using namespace godzilla;
 
-registerObject(MockStructuredGrid1D);
-
-TEST_F(StructuredGrid1DTest, ctor)
+TEST(StructuredGrid1DTest, api)
 {
-    auto obj = gMesh(3);
-    EXPECT_EQ(obj->getNx(), 3);
-}
+    TestApp app;
 
-TEST_F(StructuredGrid1DTest, create)
-{
-    auto obj = gMesh(3);
-    obj->create();
-    DM dm = obj->getDM();
+    InputParameters params = StructuredGrid1D::validParams();
+    params.set<const App *>("_app") = &app;
+    params.set<std::string>("_name") = "obj";
+    params.set<PetscInt>("nx") = 3;
+    StructuredGrid1D obj(params);
+
+    EXPECT_EQ(obj.getNx(), 3);
+
+    obj.create();
+    DM dm = obj.getDM();
 
     PetscInt dim;
     DMGetDimension(dm, &dim);
