4ae425d75c3ac61aac55edb69e1828e54f834d2f
David Andrs <andrsd@gmail.com>
2022-04-18 15:03:06 -0600

    Clang-format :face_palm:

--- include/Hex.h
+++ include/Hex.h
@@ -12,7 +12,7 @@ public:
     Hex();
     Hex(Index v[]);
     Hex(Index v1, Index v2, Index v3, Index v4, Index v5, Index v6, Index v7, Index v8);
-    Hex(const Hex &o);
+    Hex(const Hex & o);
 
 public:
     static const int NUM_VERTICES = 8;
