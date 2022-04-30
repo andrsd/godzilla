61845664adae6529b695143cd80d2fea9988e6af
David Andrs <andrsd@gmail.com>
2022-04-30 14:09:52 -0600

    Adding test for PetscMatrix::add(DenseMatrix...)

--- test/src/PetscMatrix_test.cpp
+++ test/src/PetscMatrix_test.cpp
@@ -11,6 +11,7 @@ TEST(PetscMatrixTest, test)
 
     Mat mat;
     MatCreateSeqAIJ(app.get_comm(), 3, 3, 1, nullptr, &mat);
+    MatSetOption(mat, MAT_NEW_NONZERO_ALLOCATION_ERR, PETSC_FALSE);
 
     PetscMatrix m(mat);
 
@@ -26,6 +27,26 @@ TEST(PetscMatrixTest, test)
     m.zero();
     EXPECT_EQ(m.get(0, 0), 0.);
     EXPECT_EQ(m.get(1, 1), 0.);
+    EXPECT_EQ(m.get(2, 2), 0.);
+
+    m.zero();
+    DenseMatrix<Scalar> dm(2);
+    dm[0][0] = 3;
+    dm[0][1] = 1;
+    dm[1][0] = 5;
+    dm[1][1] = 7;
+    PetscInt rows[] = { 0, 1 };
+    PetscInt cols[] = { 0, 2 };
+    m.add(dm, rows, cols);
+    m.finish();
+    EXPECT_EQ(m.get(0, 0), 3.);
+    EXPECT_EQ(m.get(0, 1), 0.);
+    EXPECT_EQ(m.get(0, 2), 1.);
+    EXPECT_EQ(m.get(1, 0), 5.);
+    EXPECT_EQ(m.get(1, 1), 0.);
+    EXPECT_EQ(m.get(1, 2), 7.);
+    EXPECT_EQ(m.get(2, 0), 0.);
+    EXPECT_EQ(m.get(2, 1), 0.);
     EXPECT_EQ(m.get(2, 2), 0.);
 
     MatDestroy(&mat);
