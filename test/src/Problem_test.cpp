b2a0577d159093c11070a7b43c71edfa24a1a3ae
David Andrs <andrsd@gmail.com>
2022-04-30 14:47:30 -0600

    Adding tests for Problem class

---
+++ test/src/Problem_test.cpp
@@ -0,0 +1,49 @@
+#include "gtest/gtest.h"
+#include "GodzillaApp_test.h"
+#include "Problem.h"
+
+using namespace godzilla;
+
+TEST(ProblemTest, test)
+{
+    TestApp app;
+
+    class TestProblem : public Problem {
+    public:
+        TestProblem(const InputParameters & params) : Problem(params) {}
+
+        virtual void
+        run()
+        {
+        }
+        virtual void
+        solve()
+        {
+        }
+        virtual bool
+        converged()
+        {
+            return false;
+        }
+        virtual DM
+        get_dm() const
+        {
+            return nullptr;
+        }
+
+        virtual Vec
+        get_solution_vector() const
+        {
+            return nullptr;
+        }
+    };
+
+    InputParameters prob_params = Problem::valid_params();
+    prob_params.set<const App *>("_app") = &app;
+    TestProblem problem(prob_params);
+
+    problem.create();
+    problem.check();
+
+    EXPECT_EQ(problem.get_time(), 0.);
+}
