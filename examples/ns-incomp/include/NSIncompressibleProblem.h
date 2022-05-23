7f719af6e003fced18800c47e929e81b63ebe8f0
David Andrs <andrsd@gmail.com>
2022-05-23 10:04:42 -0600

    Adding Navier-Stokes (incompressible) example

---
+++ examples/ns-incomp/include/NSIncompressibleProblem.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "ImplicitFENonlinearProblem.h"
+
+using namespace godzilla;
+
+/// Heat equation solver
+///
+class NSIncompressibleProblem : public ImplicitFENonlinearProblem {
+public:
+    NSIncompressibleProblem(const InputParameters & parameters);
+    virtual ~NSIncompressibleProblem();
+
+protected:
+    virtual void set_up_fields() override;
+    virtual void set_up_weak_form() override;
+    virtual void set_up_matrix_properties() override;
+    virtual void set_up_field_null_space(DM dm) override;
+    virtual void set_up_preconditioning() override;
+
+    /// Reynolds number
+    const PetscReal & Re;
+
+public:
+    static InputParameters valid_params();
+
+    static const PetscInt velocity_id = 0;
+    static const PetscInt pressure_id = 1;
+
+    static const PetscInt ffn_aid = 0;
+
+    static const PetscInt Re_idx = 0;
+};
