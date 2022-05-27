cd88e8430cbe9d41ad66c00a5ac348ad68b9dcfe
David Andrs <andrsd@gmail.com>
2022-05-27 06:39:44 -0600

    Adding tests for BoundaryCondition

--- test/include/FENonlinearProblem_test.h
+++ test/include/FENonlinearProblem_test.h
@@ -6,10 +6,9 @@
 #include "InitialCondition.h"
 #include "GodzillaApp_test.h"
 #include "GTestFENonlinearProblem.h"
+#include "GTest2FieldsFENonlinearProblem.h"
 #include "petsc.h"
 
-
-/// Test fixture
 class FENonlinearProblemTest : public GodzillaAppTest {
 public:
     void
@@ -41,4 +40,37 @@ public:
 
     Mesh * mesh;
     GTestFENonlinearProblem * prob;
+};
+
+class FENonlinear2FieldProblemTest : public GodzillaAppTest {
+public:
+    void
+    SetUp() override
+    {
+        GodzillaAppTest::SetUp();
+
+        {
+            const std::string class_name = "LineMesh";
+            InputParameters * params = Factory::get_valid_params(class_name);
+            params->set<PetscInt>("nx") = 2;
+            this->mesh = this->app->build_object<Mesh>(class_name, "mesh", params);
+        }
+        {
+            const std::string class_name = "GTest2FieldsFENonlinearProblem";
+            InputParameters * params = Factory::get_valid_params(class_name);
+            params->set<const Mesh *>("_mesh") = this->mesh;
+            this->prob =
+                this->app->build_object<GTest2FieldsFENonlinearProblem>(class_name, "prob", params);
+        }
+        this->app->problem = this->prob;
+    }
+
+    void
+    TearDown() override
+    {
+        GodzillaAppTest::TearDown();
+    }
+
+    Mesh * mesh;
+    GTest2FieldsFENonlinearProblem * prob;
 };
--- test/src/BoundaryCondition_test.cpp
+++ test/src/BoundaryCondition_test.cpp
@@ -1,51 +1,113 @@
 #include "gmock/gmock.h"
-#include "GodzillaApp_test.h"
 #include "LineMesh.h"
-#include "GTestFENonlinearProblem.h"
+#include "FENonlinearProblem_test.h"
 #include "BoundaryCondition.h"
 #include "InputParameters.h"
 #include "petsc.h"
 
 using namespace godzilla;
 
-TEST(BoundaryConditionTest, api)
+class BoundaryConditionTest : public FENonlinearProblemTest {
+public:
+};
+
+class BoundaryCondition2FieldTest : public FENonlinear2FieldProblemTest {
+public:
+};
+
+class MockBoundaryCondition : public BoundaryCondition {
+public:
+    explicit MockBoundaryCondition(const InputParameters & params) : BoundaryCondition(params) {}
+
+    MOCK_METHOD(PetscInt, get_num_components, (), (const));
+    MOCK_METHOD((DMBoundaryConditionType), get_bc_type, (), (const));
+    MOCK_METHOD(void,
+                evaluate,
+                (PetscInt, PetscReal, const PetscReal x[], PetscInt Nc, PetscScalar u[]),
+                ());
+    MOCK_METHOD(std::vector<PetscInt>, get_components, (), (const));
+    virtual void
+    set_up_callback() override
+    {
+    }
+};
+
+TEST_F(BoundaryConditionTest, api)
 {
-    class MockBoundaryCondition : public BoundaryCondition {
-    public:
-        explicit MockBoundaryCondition(const InputParameters & params) : BoundaryCondition(params)
-        {
-        }
-
-        MOCK_METHOD(PetscInt, get_num_components, (), (const));
-        MOCK_METHOD((DMBoundaryConditionType), get_bc_type, (), (const));
-        MOCK_METHOD(void,
-                    evaluate,
-                    (PetscInt, PetscReal, const PetscReal x[], PetscInt Nc, PetscScalar u[]),
-                    ());
-        MOCK_METHOD(std::vector<PetscInt>, get_components, (), (const));
-        MOCK_METHOD(void, set_up_callback, ());
-    };
-
-    TestApp app;
-
-    InputParameters mesh_pars = LineMesh::valid_params();
-    mesh_pars.set<const App *>("_app") = &app;
-    mesh_pars.set<PetscInt>("nx") = 2;
-    LineMesh mesh(mesh_pars);
-
-    InputParameters prob_pars = GTestFENonlinearProblem::valid_params();
-    prob_pars.set<const App *>("_app") = &app;
-    prob_pars.set<const Mesh *>("_mesh") = &mesh;
-    GTestFENonlinearProblem problem(prob_pars);
-    app.problem = &problem;
-
     InputParameters params = BoundaryCondition::valid_params();
-    params.set<const App *>("_app") = &app;
-    params.set<const FEProblemInterface *>("_fepi") = &problem;
+    params.set<const App *>("_app") = this->app;
+    params.set<const FEProblemInterface *>("_fepi") = this->prob;
     params.set<std::string>("_name") = "obj";
     params.set<std::string>("boundary") = "side1";
     MockBoundaryCondition bc(params);
 
     EXPECT_EQ(bc.get_boundary(), "side1");
     EXPECT_EQ(bc.get_field_id(), -1);
+}
+
+TEST_F(BoundaryCondition2FieldTest, test)
+{
+    InputParameters params = BoundaryCondition::valid_params();
+    params.set<const App *>("_app") = this->app;
+    params.set<const FEProblemInterface *>("_fepi") = this->prob;
+    params.set<std::string>("_name") = "obj";
+    params.set<std::string>("boundary") = "left";
+    params.set<std::string>("field") = "u";
+    MockBoundaryCondition bc(params);
+
+    this->prob->add_boundary_condition(&bc);
+
+    this->mesh->create();
+    this->prob->create();
+
+    this->app->check_integrity();
+
+    EXPECT_EQ(bc.get_field_id(), 0);
+}
+
+TEST_F(BoundaryCondition2FieldTest, no_field_param)
+{
+    testing::internal::CaptureStderr();
+
+    InputParameters params = BoundaryCondition::valid_params();
+    params.set<const App *>("_app") = this->app;
+    params.set<const FEProblemInterface *>("_fepi") = this->prob;
+    params.set<std::string>("_name") = "obj";
+    params.set<std::string>("boundary") = "left";
+    MockBoundaryCondition bc(params);
+
+    this->prob->add_boundary_condition(&bc);
+
+    this->mesh->create();
+    this->prob->create();
+
+    this->app->check_integrity();
+
+    EXPECT_THAT(
+        testing::internal::GetCapturedStderr(),
+        testing::HasSubstr(
+            "Use the 'field' parameter to assign this boundary condition to an existing field"));
+}
+
+TEST_F(BoundaryCondition2FieldTest, non_existing_field)
+{
+    testing::internal::CaptureStderr();
+
+    InputParameters params = BoundaryCondition::valid_params();
+    params.set<const App *>("_app") = this->app;
+    params.set<const FEProblemInterface *>("_fepi") = this->prob;
+    params.set<std::string>("_name") = "obj";
+    params.set<std::string>("boundary") = "left";
+    params.set<std::string>("field") = "asdf";
+    MockBoundaryCondition bc(params);
+
+    this->prob->add_boundary_condition(&bc);
+
+    this->mesh->create();
+    this->prob->create();
+
+    this->app->check_integrity();
+
+    EXPECT_THAT(testing::internal::GetCapturedStderr(),
+                testing::HasSubstr("Field 'asdf' does not exists. Typo?"));
 }
