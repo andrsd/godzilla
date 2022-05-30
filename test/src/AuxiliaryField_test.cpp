2e20e286df4e70a1076e0d61bb1fd73a13f9c134
David Andrs <andrsd@gmail.com>
2022-05-30 13:34:11 -0600

    Refactoring AuxiliaryFieldTest

--- test/src/AuxiliaryField_test.cpp
+++ test/src/AuxiliaryField_test.cpp
@@ -3,28 +3,16 @@
 #include "LineMesh.h"
 #include "FENonlinearProblem.h"
 #include "AuxiliaryField.h"
-#include "GTestFENonlinearProblem.h"
+#include "FENonlinearProblem_test.h"
 
 using namespace godzilla;
 
-TEST(AuxiliaryFieldTest, non_existent_id)
+class AuxiliaryFieldTest : public FENonlinearProblemTest {
+public:
+};
+
+TEST_F(AuxiliaryFieldTest, non_existent_id)
 {
-    testing::internal::CaptureStderr();
-
-    TestApp app;
-
-    InputParameters mesh_pars = LineMesh::valid_params();
-    mesh_pars.set<const App *>("_app") = &app;
-    mesh_pars.set<PetscInt>("nx") = 2;
-    LineMesh mesh(mesh_pars);
-    mesh.create();
-
-    InputParameters prob_pars = GTestFENonlinearProblem::valid_params();
-    prob_pars.set<const App *>("_app") = &app;
-    prob_pars.set<const Mesh *>("_mesh") = &mesh;
-    GTestFENonlinearProblem prob(prob_pars);
-    prob.add_aux_fe(0, "aux1", 1, 1);
-
     class TestAuxFld : public AuxiliaryField {
     public:
         explicit TestAuxFld(const InputParameters & params) : AuxiliaryField(params) {}
@@ -45,39 +33,29 @@ TEST(AuxiliaryFieldTest, non_existent_id)
         }
     };
 
-    InputParameters params = AuxiliaryField::valid_params();
-    params.set<const App *>("_app") = &app;
-    params.set<std::string>("_name") = "aux";
-    params.set<FEProblemInterface *>("_fepi") = &prob;
-    auto aux = TestAuxFld(params);
-    prob.add_auxiliary_field(&aux);
-    prob.create();
+    testing::internal::CaptureStderr();
 
-    app.check_integrity();
+    prob->add_aux_fe(0, "aux1", 1, 1);
+
+    InputParameters params = AuxiliaryField::valid_params();
+    params.set<const App *>("_app") = app;
+    params.set<std::string>("_name") = "aux";
+    params.set<FEProblemInterface *>("_fepi") = prob;
+    auto aux = TestAuxFld(params);
+    prob->add_auxiliary_field(&aux);
+
+    mesh->create();
+    prob->create();
+
+    app->check_integrity();
 
     EXPECT_THAT(testing::internal::GetCapturedStderr(),
                 testing::HasSubstr("Auxiliary field 'aux' is set on auxiliary field with ID '1', "
                                    "but such ID does not exist."));
 }
 
-TEST(AuxiliaryFieldTest, inconsistent_comp_number)
+TEST_F(AuxiliaryFieldTest, inconsistent_comp_number)
 {
-    testing::internal::CaptureStderr();
-
-    TestApp app;
-
-    InputParameters mesh_pars = LineMesh::valid_params();
-    mesh_pars.set<const App *>("_app") = &app;
-    mesh_pars.set<PetscInt>("nx") = 2;
-    LineMesh mesh(mesh_pars);
-    mesh.create();
-
-    InputParameters prob_pars = GTestFENonlinearProblem::valid_params();
-    prob_pars.set<const App *>("_app") = &app;
-    prob_pars.set<const Mesh *>("_mesh") = &mesh;
-    GTestFENonlinearProblem prob(prob_pars);
-    prob.add_aux_fe(0, "aux1", 1, 1);
-
     class TestAuxFld : public AuxiliaryField {
     public:
         explicit TestAuxFld(const InputParameters & params) : AuxiliaryField(params) {}
@@ -98,15 +76,21 @@ TEST(AuxiliaryFieldTest, inconsistent_comp_number)
         }
     };
 
-    InputParameters params = AuxiliaryField::valid_params();
-    params.set<const App *>("_app") = &app;
-    params.set<std::string>("_name") = "aux";
-    params.set<FEProblemInterface *>("_fepi") = &prob;
-    auto aux = TestAuxFld(params);
-    prob.add_auxiliary_field(&aux);
-    prob.create();
+    testing::internal::CaptureStderr();
 
-    app.check_integrity();
+    prob->add_aux_fe(0, "aux1", 1, 1);
+
+    InputParameters params = AuxiliaryField::valid_params();
+    params.set<const App *>("_app") = app;
+    params.set<std::string>("_name") = "aux";
+    params.set<FEProblemInterface *>("_fepi") = prob;
+    auto aux = TestAuxFld(params);
+    prob->add_auxiliary_field(&aux);
+
+    mesh->create();
+    prob->create();
+
+    app->check_integrity();
 
     EXPECT_THAT(testing::internal::GetCapturedStderr(),
                 testing::HasSubstr("Auxiliary field 'aux' has 2 component(s), but is set on a "
