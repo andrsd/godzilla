2234f309d419553a1cc4e4d493808455486d18f1
David Andrs <andrsd@gmail.com>
2022-05-30 13:41:34 -0600

    AuxiliaryField test

--- test/src/AuxiliaryField_test.cpp
+++ test/src/AuxiliaryField_test.cpp
@@ -96,3 +96,46 @@ TEST_F(AuxiliaryFieldTest, inconsistent_comp_number)
                 testing::HasSubstr("Auxiliary field 'aux' has 2 component(s), but is set on a "
                                    "field with 1 component(s)."));
 }
+
+TEST_F(AuxiliaryFieldTest, non_existent_region)
+{
+    class TestAuxFld : public AuxiliaryField {
+    public:
+        explicit TestAuxFld(const InputParameters & params) : AuxiliaryField(params) {}
+        virtual PetscInt
+        get_field_id() const
+        {
+            return 0;
+        }
+        virtual PetscInt
+        get_num_components() const
+        {
+            return 1;
+        }
+        virtual PetscFunc *
+        get_func() const
+        {
+            return nullptr;
+        }
+    };
+
+    testing::internal::CaptureStderr();
+
+    prob->add_aux_fe(0, "aux1", 1, 1);
+
+    InputParameters params = AuxiliaryField::valid_params();
+    params.set<const App *>("_app") = app;
+    params.set<std::string>("_name") = "aux";
+    params.set<FEProblemInterface *>("_fepi") = prob;
+    params.set<std::string>("region") = "asdf";
+    auto aux = TestAuxFld(params);
+    prob->add_auxiliary_field(&aux);
+
+    mesh->create();
+    prob->create();
+
+    app->check_integrity();
+
+    EXPECT_THAT(testing::internal::GetCapturedStderr(),
+                testing::HasSubstr("Region 'asdf' does not exists. Typo?"));
+}
