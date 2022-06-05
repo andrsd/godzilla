d4cb4b63139c7a08591ff44a9fb2dd0fc8e7a361
David Andrs <andrsd@gmail.com>
2022-06-05 10:24:30 -0600

    Refactoring ExodusIIOutput unit tests

--- test/src/ExodusIIOutput_test.cpp
+++ test/src/ExodusIIOutput_test.cpp
@@ -1,76 +1,86 @@
 #include "gmock/gmock.h"
 #include "GodzillaApp_test.h"
-#include "ExodusIIOutput_test.h"
+#include "FENonlinearProblem_test.h"
+#include "ExodusIIOutput.h"
 #include "petsc.h"
-#include "petscviewer.h"
+
+using namespace godzilla;
+
+class ExodusIIOutputTest : public FENonlinearProblemTest {
+};
 
 TEST_F(ExodusIIOutputTest, get_file_ext)
 {
-    auto mesh = gMesh1d();
-    mesh->create();
-    auto prob = gProblem1d(mesh);
-    prob->create();
-    auto out = gOutput(prob, "out");
-    EXPECT_EQ(out->get_file_ext(), "exo");
+    InputParameters params = ExodusIIOutput::valid_params();
+    params.set<const App *>("_app") = this->app;
+    params.set<const Problem *>("_problem") = this->prob;
+    params.set<std::string>("file") = "out";
+    ExodusIIOutput out(params);
+
+    EXPECT_EQ(out.get_file_ext(), "exo");
 }
 
 TEST_F(ExodusIIOutputTest, create)
 {
-    auto mesh = gMesh1d();
-    mesh->create();
-    auto prob = gProblem1d(mesh);
-    prob->create();
-    auto out = gOutput(prob, "out");
-    prob->add_output(out);
-    out->create();
+    InputParameters params = ExodusIIOutput::valid_params();
+    params.set<const App *>("_app") = this->app;
+    params.set<const Problem *>("_problem") = this->prob;
+    ExodusIIOutput out(params);
+
+    this->prob->add_output(&out);
+    out.create();
 }
 
 TEST_F(ExodusIIOutputTest, check)
 {
-    auto mesh = gMesh1d();
-    mesh->create();
-    auto prob = gProblem1d(mesh);
-    prob->create();
-    auto out = gOutput(prob, "out");
-    out->check();
+    InputParameters params = ExodusIIOutput::valid_params();
+    params.set<const App *>("_app") = this->app;
+    params.set<const Problem *>("_problem") = this->prob;
+    ExodusIIOutput out(params);
+
+    out.check();
 }
 
 TEST_F(ExodusIIOutputTest, output)
 {
-    auto mesh = gMesh1d();
-    mesh->create();
-    auto prob = gFEProblem(mesh);
-    prob->create();
-    auto out = gOutput(prob, "out");
-    out->create();
-    out->check();
+    InputParameters params = ExodusIIOutput::valid_params();
+    params.set<const App *>("_app") = this->app;
+    params.set<const Problem *>("_problem") = this->prob;
+    ExodusIIOutput out(params);
+
+    this->mesh->create();
+    this->prob->create();
+
+    out.check();
     this->app->check_integrity();
 
-    out->output_step();
+    out.output_step();
 }
 
 TEST_F(ExodusIIOutputTest, set_file_name)
 {
-    auto mesh = gMesh2d();
-    mesh->create();
-    auto prob = gProblem2d(mesh);
-    prob->create();
-    auto out = gOutput(prob, "out");
-    out->create();
+    InputParameters params = ExodusIIOutput::valid_params();
+    params.set<const App *>("_app") = this->app;
+    params.set<const Problem *>("_problem") = this->prob;
+    params.set<std::string>("file") = "out";
+    ExodusIIOutput out(params);
 
-    out->set_file_name();
-    EXPECT_EQ(out->get_file_name(), "out.exo");
+    out.create();
+
+    out.set_file_name();
+    EXPECT_EQ(out.get_file_name(), "out.exo");
 }
 
 TEST_F(ExodusIIOutputTest, set_seq_file_name)
 {
-    auto mesh = gMesh2d();
-    mesh->create();
-    auto prob = gProblem2d(mesh);
-    prob->create();
-    auto out = gOutput(prob, "out");
-    out->create();
+    InputParameters params = ExodusIIOutput::valid_params();
+    params.set<const App *>("_app") = this->app;
+    params.set<const Problem *>("_problem") = this->prob;
+    params.set<std::string>("file") = "out";
+    ExodusIIOutput out(params);
 
-    out->set_sequence_file_name(2);
-    EXPECT_EQ(out->get_file_name(), "out.2.exo");
+    out.create();
+
+    out.set_sequence_file_name(2);
+    EXPECT_EQ(out.get_file_name(), "out.2.exo");
 }
