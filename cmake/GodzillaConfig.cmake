2bac1b39698233c115264c83527c347fef9b8590
David Andrs <andrsd@gmail.com>
2022-06-25 14:19:30 -0600

    CMake changes

--- cmake/GodzillaConfig.cmake
+++ cmake/GodzillaConfig.cmake
@@ -6,5 +6,7 @@ find_package(Boost
     COMPONENTS
         filesystem
 )
+find_dependency(yaml-cpp REQUIRED)
+find_dependency(muparser REQUIRED)
 
 include("${CMAKE_CURRENT_LIST_DIR}/GodzillaTargets.cmake")
