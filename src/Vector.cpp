44a032800f1445f22cde3b943113e3f2f9bb44fa
David Andrs <andrsd@gmail.com>
2023-02-06 16:30:23 -0700

    Adding wrapper around PETSc Vector

---
+++ src/Vector.cpp
@@ -0,0 +1,307 @@
+#include "Vector.h"
+#include "Error.h"
+#include "CallStack.h"
+#include <cassert>
+
+namespace godzilla {
+
+Vector::Vector() : vec(nullptr) {}
+
+Vector::Vector(MPI_Comm comm)
+{
+    _F_;
+    create(comm);
+}
+
+Vector::Vector(Vec vec) : vec(vec)
+{
+    _F_;
+}
+
+Vector::~Vector() {}
+
+void
+Vector::create(MPI_Comm comm)
+{
+    _F_;
+    PETSC_CHECK(VecCreate(comm, &this->vec));
+}
+
+void
+Vector::destroy()
+{
+    _F_;
+    if (this->vec)
+        PETSC_CHECK(VecDestroy(&this->vec));
+    this->vec = nullptr;
+}
+
+void
+Vector::set_name(const std::string & name)
+{
+    PETSC_CHECK(PetscObjectSetName((PetscObject) this->vec, name.c_str()));
+}
+
+void
+Vector::assembly_begin()
+{
+    _F_;
+    PETSC_CHECK(VecAssemblyBegin(this->vec));
+}
+
+void
+Vector::assembly_end()
+{
+    _F_;
+    PETSC_CHECK(VecAssemblyEnd(this->vec));
+}
+
+Int
+Vector::get_size() const
+{
+    _F_;
+    Int sz;
+    PETSC_CHECK(VecGetSize(this->vec, &sz));
+    return sz;
+}
+
+Int
+Vector::get_local_size() const
+{
+    _F_;
+    Int sz;
+    PETSC_CHECK(VecGetLocalSize(this->vec, &sz));
+    return sz;
+}
+
+void
+Vector::get_values(const std::vector<Int> & idx, std::vector<Scalar> & y) const
+{
+    _F_;
+    PETSC_CHECK(VecGetValues(this->vec, (Int) idx.size(), idx.data(), y.data()));
+}
+
+void
+Vector::abs()
+{
+    _F_;
+    PETSC_CHECK(VecAbs(this->vec));
+}
+
+Scalar
+Vector::dot(const Vector & y) const
+{
+    _F_;
+    Scalar val;
+    PETSC_CHECK(VecDot(this->vec, y.vec, &val));
+    return val;
+}
+
+void
+Vector::scale(Scalar alpha)
+{
+    _F_;
+    PETSC_CHECK(VecScale(this->vec, alpha));
+}
+
+void
+Vector::duplicate(Vector & b) const
+{
+    _F_;
+    VecDuplicate(this->vec, &b.vec);
+}
+
+void
+Vector::copy(Vector & y)
+{
+    _F_;
+    PETSC_CHECK(VecCopy(this->vec, y.vec));
+}
+
+void
+Vector::normalize()
+{
+    _F_;
+    PETSC_CHECK(VecNormalize(this->vec, nullptr));
+}
+
+Scalar
+Vector::min() const
+{
+    _F_;
+    Scalar val;
+    PETSC_CHECK(VecMin(this->vec, nullptr, &val));
+    return val;
+}
+
+Scalar
+Vector::max() const
+{
+    _F_;
+    Scalar val;
+    PETSC_CHECK(VecMax(this->vec, nullptr, &val));
+    return val;
+}
+
+void
+Vector::chop(Real tol)
+{
+    _F_;
+    PETSC_CHECK(VecChop(this->vec, tol));
+}
+
+void
+Vector::axpy(Scalar alpha, const Vector & x)
+{
+    _F_;
+    PETSC_CHECK(VecAXPY(this->vec, alpha, (Vec) x));
+}
+
+void
+Vector::aypx(Scalar beta, const Vector & x)
+{
+    _F_;
+    PETSC_CHECK(VecAYPX(this->vec, beta, (Vec) x));
+}
+
+void
+Vector::shift(Scalar shift)
+{
+    _F_;
+    PETSC_CHECK(VecShift(this->vec, shift));
+}
+
+void
+Vector::set(Scalar alpha)
+{
+    _F_;
+    PETSC_CHECK(VecSet(this->vec, alpha));
+}
+
+void
+Vector::set_sizes(Int n, Int N)
+{
+    _F_;
+    if (n == PETSC_DECIDE && N == PETSC_DECIDE)
+        error(
+            "Calling Vector::set_sizes with n = PETSC_DECIDE and N = PETSC_DECIDE is not allowed.");
+    PETSC_CHECK(VecSetSizes(this->vec, n, N));
+}
+
+void
+Vector::set_type(VecType method)
+{
+    _F_;
+    PETSC_CHECK(VecSetType(this->vec, method));
+}
+
+void
+Vector::set_value(Int row, Scalar value, InsertMode mode)
+{
+    _F_;
+    PETSC_CHECK(VecSetValue(this->vec, row, value, mode));
+}
+
+void
+Vector::set_values(const std::vector<Int> & ix, const std::vector<Scalar> & y, InsertMode mode)
+{
+    _F_;
+    assert(ix.size() == y.size());
+    PETSC_CHECK(VecSetValues(this->vec, ix.size(), ix.data(), y.data(), mode));
+}
+
+void
+Vector::set_values_local(const std::vector<Int> & ix,
+                         const std::vector<Scalar> & y,
+                         InsertMode mode)
+{
+    _F_;
+    assert(ix.size() == y.size());
+    PETSC_CHECK(VecSetValuesLocal(this->vec, ix.size(), ix.data(), y.data(), mode));
+}
+
+Scalar
+Vector::sum() const
+{
+    _F_;
+    Scalar sum;
+    PETSC_CHECK(VecSum(this->vec, &sum));
+    return sum;
+}
+
+void
+Vector::zero()
+{
+    _F_;
+    PETSC_CHECK(VecZeroEntries(this->vec));
+}
+
+Scalar
+Vector::operator[](Int idx) const
+{
+    _F_;
+    Scalar val;
+    VecGetValues(this->vec, 1, &idx, &val);
+    return val;
+}
+
+Scalar *
+Vector::get_array()
+{
+    _F_;
+    Scalar * array;
+    PETSC_CHECK(VecGetArray(this->vec, &array));
+    return array;
+}
+
+void
+Vector::restore_array(Scalar * array)
+{
+    _F_;
+    PETSC_CHECK(VecRestoreArray(this->vec, &array));
+}
+
+Vector::operator Vec() const
+{
+    _F_;
+    return this->vec;
+}
+
+Vector
+Vector::create_seq(MPI_Comm comm, Int n)
+{
+    _F_;
+    Vector v(comm);
+    PETSC_CHECK(VecCreateSeq(comm, n, &v.vec));
+    return v;
+}
+
+void
+Vector::pointwise_min(const Vector & w, const Vector & x, const Vector & y)
+{
+    _F_;
+    PETSC_CHECK(VecPointwiseMin((Vec) w, (Vec) x, (Vec) y));
+}
+
+void
+Vector::pointwise_max(const Vector & w, const Vector & x, const Vector & y)
+{
+    _F_;
+    PETSC_CHECK(VecPointwiseMax((Vec) w, (Vec) x, (Vec) y));
+}
+
+void
+Vector::pointwise_mult(const Vector & w, const Vector & x, const Vector & y)
+{
+    _F_;
+    PETSC_CHECK(VecPointwiseMult((Vec) w, (Vec) x, (Vec) y));
+}
+
+void
+Vector::pointwise_divide(const Vector & w, const Vector & x, const Vector & y)
+{
+    _F_;
+    PETSC_CHECK(VecPointwiseDivide((Vec) w, (Vec) x, (Vec) y));
+}
+
+} // namespace godzilla
---
+++ test/src/Vector_test.cpp
@@ -0,0 +1,353 @@
+#include "gmock/gmock.h"
+#include "Vector.h"
+#include "PerfLog.h"
+
+using namespace godzilla;
+
+TEST(VectorTest, assembly)
+{
+    // TODO: this should really be tested with MPI (n_proc > 1)
+    Vector v = Vector::create_seq(MPI_COMM_WORLD, 3);
+    v.assembly_begin();
+    v.assembly_end();
+    v.destroy();
+}
+
+TEST(VectorTest, set_sizes)
+{
+    Vector v(MPI_COMM_WORLD);
+    v.set_type(VECSEQ);
+    v.set_sizes(3);
+    EXPECT_EQ(v.get_size(), 3);
+    EXPECT_EQ(v.get_local_size(), 3);
+
+    EXPECT_DEATH(
+        v.set_sizes(PETSC_DECIDE, PETSC_DECIDE),
+        "\\[ERROR\\] Calling Vector::set_sizes with n = PETSC_DECIDE and N = PETSC_DECIDE is "
+        "not allowed.");
+    v.destroy();
+}
+
+TEST(VectorTest, get_size)
+{
+    Vector v = Vector::create_seq(MPI_COMM_WORLD, 10);
+    EXPECT_EQ(v.get_size(), 10);
+    EXPECT_EQ(v.get_local_size(), 10);
+    v.destroy();
+}
+
+TEST(VectorTest, get_values)
+{
+    Vector v = Vector::create_seq(MPI_COMM_WORLD, 3);
+    v.set_values({ 0, 1, 2 }, { 3, 5, 9 });
+    std::vector<Scalar> vals(2);
+    v.get_values({ 0, 2 }, vals);
+    EXPECT_DOUBLE_EQ(vals[0], 3.);
+    EXPECT_DOUBLE_EQ(vals[1], 9.);
+    v.destroy();
+}
+
+TEST(VectorTest, set)
+{
+    Vector v = Vector::create_seq(MPI_COMM_WORLD, 10);
+    v.set(21.);
+    for (Int i = 0; i < 10; i++)
+        EXPECT_DOUBLE_EQ(v[i], 21.);
+    v.destroy();
+}
+
+TEST(VectorTest, set_values)
+{
+    Vector v = Vector::create_seq(MPI_COMM_WORLD, 3);
+    v.set_values({ 0, 1, 2 }, { 3, 5, 9 });
+    EXPECT_DOUBLE_EQ(v[0], 3.);
+    EXPECT_DOUBLE_EQ(v[1], 5.);
+    EXPECT_DOUBLE_EQ(v[2], 9.);
+    v.destroy();
+}
+
+TEST(VectorTest, set_values_local)
+{
+    // TODO: this should be tested with MPI n_proc > 1
+    Vector v = Vector::create_seq(MPI_COMM_WORLD, 3);
+    v.set_values_local({ 0, 1, 2 }, { 3, 5, 9 });
+    EXPECT_DOUBLE_EQ(v[0], 3.);
+    EXPECT_DOUBLE_EQ(v[1], 5.);
+    EXPECT_DOUBLE_EQ(v[2], 9.);
+    v.destroy();
+}
+
+TEST(VectorTest, duplicate)
+{
+    Vector v = Vector::create_seq(MPI_COMM_WORLD, 3);
+    v.set_value(0, 1.);
+    v.set_value(1, 3.);
+    v.set_value(2, 7.);
+    Vector dup;
+    v.duplicate(dup);
+    v.copy(dup);
+    EXPECT_DOUBLE_EQ(dup[0], 1.);
+    EXPECT_DOUBLE_EQ(dup[1], 3.);
+    EXPECT_DOUBLE_EQ(dup[2], 7.);
+    v.destroy();
+}
+
+TEST(VectorTest, sum)
+{
+    Vector v = Vector::create_seq(MPI_COMM_WORLD, 3);
+    v.set_value(0, 1.);
+    v.set_value(1, 3.);
+    v.set_value(2, 7.);
+    EXPECT_DOUBLE_EQ(v.sum(), 11.);
+    v.destroy();
+}
+
+TEST(VectorTest, zero)
+{
+    Vector v = Vector::create_seq(MPI_COMM_WORLD, 3);
+    v.set_value(0, 1.);
+    v.set_value(1, 3.);
+    v.set_value(2, 7.);
+    v.zero();
+    EXPECT_DOUBLE_EQ(v[0], 0.);
+    EXPECT_DOUBLE_EQ(v[1], 0.);
+    EXPECT_DOUBLE_EQ(v[2], 0.);
+    v.destroy();
+}
+
+TEST(VectorTest, dot)
+{
+    Vector v = Vector::create_seq(MPI_COMM_WORLD, 3);
+    v.set_value(0, 1.);
+    v.set_value(1, 3.);
+    v.set_value(2, 7.);
+
+    Vector u = Vector::create_seq(MPI_COMM_WORLD, 3);
+    u.set_value(0, 3.);
+    u.set_value(1, 5.);
+    u.set_value(2, 2.);
+
+    Scalar dot = v.dot(u);
+    EXPECT_DOUBLE_EQ(dot, 32.);
+
+    v.destroy();
+    u.destroy();
+}
+
+TEST(VectorTest, get_restore_array)
+{
+    Vector v = Vector::create_seq(MPI_COMM_WORLD, 3);
+    Scalar * arr = v.get_array();
+    arr[0] = 3.;
+    arr[1] = 5.;
+    arr[2] = 7.;
+    v.restore_array(arr);
+
+    EXPECT_EQ(v[0], 3.);
+    EXPECT_EQ(v[1], 5.);
+    EXPECT_EQ(v[2], 7.);
+
+    v.destroy();
+}
+
+TEST(VectorTest, abs)
+{
+    Vector v = Vector::create_seq(MPI_COMM_WORLD, 3);
+    v.set_value(0, -3.);
+    v.set_value(1, 5.);
+    v.set_value(2, -7.);
+    v.abs();
+    EXPECT_DOUBLE_EQ(v[0], 3.);
+    EXPECT_DOUBLE_EQ(v[1], 5.);
+    EXPECT_DOUBLE_EQ(v[2], 7.);
+    v.destroy();
+}
+
+TEST(VectorTest, scale)
+{
+    Vector v = Vector::create_seq(MPI_COMM_WORLD, 2);
+    v.set_value(0, -3.);
+    v.set_value(1, 5.);
+    v.scale(-1.);
+    EXPECT_DOUBLE_EQ(v[0], 3.);
+    EXPECT_DOUBLE_EQ(v[1], -5.);
+    v.destroy();
+}
+
+TEST(VectorTest, normalize)
+{
+    Vector v = Vector::create_seq(MPI_COMM_WORLD, 2);
+    v.set_value(0, 3.);
+    v.set_value(1, 4.);
+    v.normalize();
+    EXPECT_DOUBLE_EQ(v[0], 0.6);
+    EXPECT_DOUBLE_EQ(v[1], 0.8);
+    v.destroy();
+}
+
+TEST(VectorTest, shift)
+{
+    Vector v = Vector::create_seq(MPI_COMM_WORLD, 2);
+    v.set_value(0, 3.);
+    v.set_value(1, 4.);
+    v.shift(1.);
+    EXPECT_DOUBLE_EQ(v[0], 4.);
+    EXPECT_DOUBLE_EQ(v[1], 5);
+    v.destroy();
+}
+
+TEST(VectorTest, min)
+{
+    Vector v = Vector::create_seq(MPI_COMM_WORLD, 2);
+    v.set_value(0, 3.);
+    v.set_value(1, 4.);
+    EXPECT_DOUBLE_EQ(v.min(), 3.);
+    v.destroy();
+}
+
+TEST(VectorTest, max)
+{
+    Vector v = Vector::create_seq(MPI_COMM_WORLD, 2);
+    v.set_value(0, 3.);
+    v.set_value(1, 4.);
+    EXPECT_DOUBLE_EQ(v.max(), 4.);
+    v.destroy();
+}
+
+TEST(VectorTest, chop)
+{
+    Vector v = Vector::create_seq(MPI_COMM_WORLD, 2);
+    v.set_value(0, 3.);
+    v.set_value(1, 4.);
+    v.chop(3.5);
+    EXPECT_DOUBLE_EQ(v[0], 0.);
+    EXPECT_DOUBLE_EQ(v[1], 4.);
+    v.destroy();
+}
+
+TEST(VectorTest, axpy)
+{
+    Vector y = Vector::create_seq(MPI_COMM_WORLD, 2);
+    y.set_value(0, 3.);
+    y.set_value(1, 4.);
+
+    Vector x = Vector::create_seq(MPI_COMM_WORLD, 2);
+    x.set_value(0, 2.);
+    x.set_value(1, 5.);
+
+    y.axpy(3., x);
+    EXPECT_DOUBLE_EQ(y[0], 9.);
+    EXPECT_DOUBLE_EQ(y[1], 19.);
+    x.destroy();
+    y.destroy();
+}
+
+TEST(VectorTest, aypx)
+{
+    Vector y = Vector::create_seq(MPI_COMM_WORLD, 2);
+    y.set_value(0, 3.);
+    y.set_value(1, 4.);
+
+    Vector x = Vector::create_seq(MPI_COMM_WORLD, 2);
+    x.set_value(0, 2.);
+    x.set_value(1, 5.);
+
+    y.aypx(3., x);
+    EXPECT_DOUBLE_EQ(y[0], 11.);
+    EXPECT_DOUBLE_EQ(y[1], 17.);
+
+    x.destroy();
+    y.destroy();
+}
+
+TEST(VectorTest, pointwise_min)
+{
+    Vector x = Vector::create_seq(MPI_COMM_WORLD, 2);
+    Vector y;
+    x.duplicate(y);
+    Vector w;
+    x.duplicate(w);
+
+    x.set_value(0, 2.);
+    x.set_value(1, 5.);
+
+    y.set_value(0, 3.);
+    y.set_value(1, 4.);
+
+    Vector::pointwise_min(w, x, y);
+    EXPECT_DOUBLE_EQ(w[0], 2.);
+    EXPECT_DOUBLE_EQ(w[1], 4.);
+
+    x.destroy();
+    y.destroy();
+    w.destroy();
+}
+
+TEST(VectorTest, pointwise_max)
+{
+    Vector x = Vector::create_seq(MPI_COMM_WORLD, 2);
+    Vector y;
+    x.duplicate(y);
+    Vector w;
+    x.duplicate(w);
+
+    x.set_value(0, 2.);
+    x.set_value(1, 5.);
+
+    y.set_value(0, 3.);
+    y.set_value(1, 4.);
+
+    Vector::pointwise_max(w, x, y);
+    EXPECT_DOUBLE_EQ(w[0], 3.);
+    EXPECT_DOUBLE_EQ(w[1], 5.);
+
+    x.destroy();
+    y.destroy();
+    w.destroy();
+}
+
+TEST(VectorTest, pointwise_mult)
+{
+    Vector x = Vector::create_seq(MPI_COMM_WORLD, 2);
+    Vector y;
+    x.duplicate(y);
+    Vector w;
+    x.duplicate(w);
+
+    x.set_value(0, 2.);
+    x.set_value(1, 5.);
+
+    y.set_value(0, 3.);
+    y.set_value(1, 4.);
+
+    Vector::pointwise_mult(w, x, y);
+    EXPECT_DOUBLE_EQ(w[0], 6.);
+    EXPECT_DOUBLE_EQ(w[1], 20.);
+
+    x.destroy();
+    y.destroy();
+    w.destroy();
+}
+
+TEST(VectorTest, pointwise_divide)
+{
+    Vector x = Vector::create_seq(MPI_COMM_WORLD, 2);
+    Vector y;
+    x.duplicate(y);
+    Vector w;
+    x.duplicate(w);
+
+    x.set_value(0, 8.);
+    x.set_value(1, 18.);
+
+    y.set_value(0, 2.);
+    y.set_value(1, 3.);
+
+    Vector::pointwise_divide(w, x, y);
+    EXPECT_DOUBLE_EQ(w[0], 4.);
+    EXPECT_DOUBLE_EQ(w[1], 6.);
+
+    x.destroy();
+    y.destroy();
+    w.destroy();
+}
