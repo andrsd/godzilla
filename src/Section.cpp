30a9797ed5805a88e88e28bb51de4f82c2c88ea8
David Andrs <andrsd@gmail.com>
2023-03-03 18:23:19 -0700

    Adding a wrapper for Section
    Closes #185

---
+++ src/Section.cpp
@@ -0,0 +1,303 @@
+#include "Section.h"
+#include "CallStack.h"
+#include "Error.h"
+
+namespace godzilla {
+
+Section::Section() : section(nullptr) {}
+
+Section::Section(PetscSection s) : section(s) {}
+
+void
+Section::create(MPI_Comm comm)
+{
+    _F_;
+    PETSC_CHECK(PetscSectionCreate(comm, &this->section));
+}
+
+void
+Section::destroy()
+{
+    _F_;
+    PETSC_CHECK(PetscSectionDestroy(&this->section));
+    this->section = nullptr;
+}
+
+void
+Section::reset()
+{
+    _F_;
+    PETSC_CHECK(PetscSectionReset(this->section));
+}
+
+void
+Section::view(PetscViewer viewer) const
+{
+    _F_;
+    PETSC_CHECK(PetscSectionView(this->section, viewer));
+}
+
+void
+Section::set_chart(Int start, Int end)
+{
+    _F_;
+    PETSC_CHECK(PetscSectionSetChart(this->section, start, end));
+}
+
+void
+Section::get_chart(Int & start, Int & end) const
+{
+    _F_;
+    PETSC_CHECK(PetscSectionGetChart(this->section, &start, &end));
+}
+
+void
+Section::set_up()
+{
+    _F_;
+    PETSC_CHECK(PetscSectionSetUp(this->section));
+}
+
+void
+Section::set_point_major(bool pm)
+{
+    _F_;
+    PETSC_CHECK(PetscSectionSetPointMajor(this->section, pm ? PETSC_TRUE : PETSC_FALSE));
+}
+
+bool
+Section::get_point_major() const
+{
+    _F_;
+    PetscBool pm;
+    PETSC_CHECK(PetscSectionGetPointMajor(this->section, &pm));
+    return pm == PETSC_TRUE;
+}
+
+Int
+Section::get_max_dof() const
+{
+    _F_;
+    Int max_dof;
+    PETSC_CHECK(PetscSectionGetMaxDof(this->section, &max_dof));
+    return max_dof;
+}
+
+void
+Section::set_num_fields(Int n_fields)
+{
+    _F_;
+    PETSC_CHECK(PetscSectionSetNumFields(this->section, n_fields));
+}
+
+Int
+Section::get_num_fields() const
+{
+    _F_;
+    Int n_fields;
+    PETSC_CHECK(PetscSectionGetNumFields(this->section, &n_fields));
+    return n_fields;
+}
+
+void
+Section::add_dof(Int point, Int n_dofs)
+{
+    _F_;
+    PETSC_CHECK(PetscSectionAddDof(this->section, point, n_dofs));
+}
+
+void
+Section::set_dof(Int point, Int n_dofs)
+{
+    _F_;
+    PETSC_CHECK(PetscSectionSetDof(this->section, point, n_dofs));
+}
+
+Int
+Section::get_dof(Int point) const
+{
+    _F_;
+    Int n_dof;
+    PETSC_CHECK(PetscSectionGetDof(this->section, point, &n_dof));
+    return n_dof;
+}
+
+Section
+Section::get_field(Int field) const
+{
+    _F_;
+    PetscSection s;
+    PETSC_CHECK(PetscSectionGetField(this->section, field, &s));
+    return { s };
+}
+
+void
+Section::set_field_components(Int field, Int n_comps)
+{
+    _F_;
+    PETSC_CHECK(PetscSectionSetFieldComponents(this->section, field, n_comps));
+}
+
+Int
+Section::get_field_components(Int field) const
+{
+    _F_;
+    Int n_comps;
+    PETSC_CHECK(PetscSectionGetFieldComponents(this->section, field, &n_comps));
+    return n_comps;
+}
+
+void
+Section::add_field_constraint_dof(Int point, Int field, Int n_dofs)
+{
+    _F_;
+    PETSC_CHECK(PetscSectionAddFieldConstraintDof(this->section, point, field, n_dofs));
+}
+
+void
+Section::set_field_constraint_dof(Int point, Int field, Int n_dofs)
+{
+    _F_;
+    PETSC_CHECK(PetscSectionSetFieldConstraintDof(this->section, point, field, n_dofs));
+}
+
+Int
+Section::get_field_constraint_dof(Int point, Int field) const
+{
+    _F_;
+    Int n_dofs;
+    PETSC_CHECK(PetscSectionGetFieldConstraintDof(this->section, point, field, &n_dofs));
+    return n_dofs;
+}
+
+void
+Section::add_field_dof(Int point, Int field, Int n_dofs)
+{
+    _F_;
+    PETSC_CHECK(PetscSectionAddFieldDof(this->section, point, field, n_dofs));
+}
+
+void
+Section::set_field_dof(Int point, Int field, Int n_dofs)
+{
+    _F_;
+    PETSC_CHECK(PetscSectionSetFieldDof(this->section, point, field, n_dofs));
+}
+
+Int
+Section::get_field_dof(Int point, Int field) const
+{
+    _F_;
+    Int n_dofs;
+    PETSC_CHECK(PetscSectionGetFieldDof(this->section, point, field, &n_dofs));
+    return n_dofs;
+}
+
+void
+Section::set_field_name(Int field, const std::string & name)
+{
+    _F_;
+    PETSC_CHECK(PetscSectionSetFieldName(this->section, field, name.c_str()));
+}
+
+std::string
+Section::get_field_name(Int field) const
+{
+    _F_;
+    const char * name;
+    PETSC_CHECK(PetscSectionGetFieldName(this->section, field, &name));
+    return { name };
+}
+
+Int
+Section::get_offset(Int point) const
+{
+    _F_;
+    Int offset;
+    PETSC_CHECK(PetscSectionGetOffset(this->section, point, &offset));
+    return offset;
+}
+
+void
+Section::get_offset_range(Int & start, Int & end) const
+{
+    _F_;
+    PETSC_CHECK(PetscSectionGetOffsetRange(this->section, &start, &end));
+}
+
+Int
+Section::get_field_offset(Int point, Int field) const
+{
+    _F_;
+    Int offset;
+    PETSC_CHECK(PetscSectionGetFieldOffset(this->section, point, field, &offset));
+    return offset;
+}
+
+Int
+Section::get_field_point_offset(Int point, Int field) const
+{
+    _F_;
+    Int offset;
+    PETSC_CHECK(PetscSectionGetFieldPointOffset(this->section, point, field, &offset));
+    return offset;
+}
+
+Int
+Section::get_storage_size() const
+{
+    _F_;
+    Int sz;
+    PetscSectionGetStorageSize(this->section, &sz);
+    return sz;
+}
+
+bool
+Section::has_constraints() const
+{
+    _F_;
+    PetscBool hc;
+    PETSC_CHECK(PetscSectionHasConstraints(this->section, &hc));
+    return hc == PETSC_TRUE;
+}
+
+void
+Section::set_component_name(Int field, Int comp, const std::string & name)
+{
+    _F_;
+    PETSC_CHECK(PetscSectionSetComponentName(this->section, field, comp, name.c_str()));
+}
+
+std::string
+Section::get_component_name(Int field, Int comp) const
+{
+    _F_;
+    const char * name;
+    PETSC_CHECK(PetscSectionGetComponentName(this->section, field, comp, &name));
+    return { name };
+}
+
+void
+Section::add_constraint_dof(Int point, Int n_dofs)
+{
+    _F_;
+    PETSC_CHECK(PetscSectionAddConstraintDof(this->section, point, n_dofs));
+}
+
+void
+Section::set_constraint_dof(Int point, Int n_dofs)
+{
+    _F_;
+    PETSC_CHECK(PetscSectionSetConstraintDof(this->section, point, n_dofs));
+}
+
+Int
+Section::get_constraint_dof(Int point) const
+{
+    _F_;
+    Int n_dofs;
+    PETSC_CHECK(PetscSectionGetConstraintDof(this->section, point, &n_dofs));
+    return n_dofs;
+}
+
+} // namespace godzilla
---
+++ test/src/Section_test.cpp
@@ -0,0 +1,295 @@
+#include "gmock/gmock.h"
+#include "Section.h"
+
+using namespace godzilla;
+
+TEST(SectionTest, dofs)
+{
+    Section s;
+    s.create(MPI_COMM_WORLD);
+    s.set_num_fields(2);
+    s.set_chart(0, 10);
+    for (Int i = 0; i < 10; i++) {
+        s.set_dof(i, 4);
+        s.set_field_dof(i, 0, 1);
+        s.set_field_dof(i, 1, 2);
+        s.add_field_dof(i, 1, 1);
+    }
+    s.set_up();
+
+    EXPECT_EQ(s.get_num_fields(), 2);
+
+    Int start, end;
+    s.get_chart(start, end);
+    EXPECT_EQ(start, 0);
+    EXPECT_EQ(end, 10);
+
+    for (Int pt = 0; pt < 10; pt++) {
+        EXPECT_EQ(s.get_dof(pt), 4);
+        EXPECT_EQ(s.get_field_dof(pt, 0), 1);
+        EXPECT_EQ(s.get_field_dof(pt, 1), 3);
+    }
+
+    s.destroy();
+}
+
+TEST(SectionTest, field_name)
+{
+    Section s;
+    s.create(MPI_COMM_WORLD);
+    s.set_num_fields(2);
+    s.set_field_name(0, "fld1");
+    s.set_field_name(1, "fld2");
+    s.set_chart(0, 1);
+    s.set_up();
+    EXPECT_EQ(s.get_field_name(0), "fld1");
+    EXPECT_EQ(s.get_field_name(1), "fld2");
+    s.destroy();
+}
+
+TEST(SectionTest, get_max_dof)
+{
+    Section s;
+    s.create(MPI_COMM_WORLD);
+    s.set_num_fields(1);
+    s.set_chart(0, 3);
+    Int dofs[] = { 5, 3, 1 };
+    for (Int i = 0; i < 3; i++)
+        s.set_dof(i, dofs[i]);
+    s.set_up();
+    EXPECT_EQ(s.get_max_dof(), 5);
+    s.destroy();
+}
+
+TEST(SectionTest, reset)
+{
+    Section s;
+    s.create(MPI_COMM_WORLD);
+    s.set_num_fields(1);
+    s.set_chart(0, 1);
+    s.set_dof(0, 10);
+    s.set_up();
+    s.reset();
+
+    Int start, end;
+    s.get_chart(start, end);
+    EXPECT_EQ(start, -1);
+    EXPECT_EQ(end, -1);
+
+    s.destroy();
+}
+
+TEST(SectionTest, point_major)
+{
+    Section s;
+    s.create(MPI_COMM_WORLD);
+    s.set_num_fields(1);
+    s.set_chart(0, 2);
+    s.set_dof(0, 2);
+    s.set_dof(1, 4);
+    s.set_point_major();
+    s.set_up();
+
+    EXPECT_TRUE(s.get_point_major());
+
+    s.destroy();
+}
+
+TEST(SectionTest, add_dof)
+{
+    Section s;
+    s.create(MPI_COMM_WORLD);
+    s.set_num_fields(1);
+    s.set_chart(0, 2);
+    s.set_dof(0, 1);
+    s.set_dof(1, 1);
+    s.add_dof(0, 10);
+    s.add_dof(1, 20);
+    s.set_up();
+    EXPECT_EQ(s.get_dof(0), 11);
+    EXPECT_EQ(s.get_dof(1), 21);
+    s.destroy();
+}
+
+TEST(SectionTest, field_components)
+{
+    Section s;
+    s.create(MPI_COMM_WORLD);
+    s.set_num_fields(2);
+    s.set_field_components(0, 2);
+    s.set_component_name(0, 0, "f0c0");
+    s.set_component_name(0, 1, "f0c1");
+    s.set_field_components(1, 4);
+    s.set_component_name(1, 0, "f1c1");
+    s.set_component_name(1, 1, "f1c2");
+    s.set_component_name(1, 2, "f1c3");
+    s.set_component_name(1, 3, "f1c4");
+    s.set_up();
+    EXPECT_EQ(s.get_field_components(0), 2);
+    EXPECT_EQ(s.get_component_name(0, 0), "f0c0");
+    EXPECT_EQ(s.get_component_name(0, 1), "f0c1");
+    EXPECT_EQ(s.get_field_components(1), 4);
+    EXPECT_EQ(s.get_component_name(1, 0), "f1c1");
+    EXPECT_EQ(s.get_component_name(1, 1), "f1c2");
+    EXPECT_EQ(s.get_component_name(1, 2), "f1c3");
+    EXPECT_EQ(s.get_component_name(1, 3), "f1c4");
+    s.destroy();
+}
+
+TEST(SectionTest, get_offset)
+{
+    Section s;
+    s.create(MPI_COMM_WORLD);
+    s.set_num_fields(1);
+    s.set_chart(0, 2);
+    s.set_dof(0, 3);
+    s.set_dof(1, 7);
+    s.set_up();
+    EXPECT_EQ(s.get_offset(1), 3);
+
+    Int offst_start, offst_end;
+    s.get_offset_range(offst_start, offst_end);
+    EXPECT_EQ(offst_start, 0);
+    EXPECT_EQ(offst_end, 10);
+
+    s.destroy();
+}
+
+TEST(SectionTest, get_field_offset)
+{
+    Section s;
+    s.create(MPI_COMM_WORLD);
+    s.set_num_fields(2);
+    s.set_chart(0, 2);
+
+    s.set_dof(0, 3);
+    s.set_field_dof(0, 0, 1);
+    s.set_field_dof(0, 1, 2);
+
+    s.set_dof(1, 6);
+    s.set_field_dof(1, 0, 2);
+    s.set_field_dof(1, 1, 4);
+
+    s.set_up();
+
+    EXPECT_EQ(s.get_field_offset(0, 0), 0);
+    EXPECT_EQ(s.get_field_offset(0, 1), 1);
+    EXPECT_EQ(s.get_field_offset(1, 0), 3);
+    EXPECT_EQ(s.get_field_offset(1, 1), 5);
+
+    EXPECT_EQ(s.get_field_point_offset(0, 0), 0);
+    EXPECT_EQ(s.get_field_point_offset(0, 1), 1);
+    EXPECT_EQ(s.get_field_point_offset(1, 0), 0);
+    EXPECT_EQ(s.get_field_point_offset(1, 1), 2);
+
+    s.destroy();
+}
+
+TEST(SectionTest, get_storage_size)
+{
+    Section s;
+    s.create(MPI_COMM_WORLD);
+    s.set_num_fields(2);
+    s.set_chart(0, 2);
+    s.set_dof(0, 3);
+    s.set_dof(1, 6);
+    s.set_up();
+
+    EXPECT_EQ(s.get_storage_size(), 9);
+
+    s.destroy();
+}
+
+TEST(SectionTest, ceds)
+{
+    Section s;
+    s.create(MPI_COMM_WORLD);
+    s.set_num_fields(1);
+    s.set_chart(0, 3);
+    s.set_constraint_dof(0, 1);
+    s.set_dof(1, 2);
+    s.set_constraint_dof(2, 1);
+    s.add_constraint_dof(2, 1);
+    s.set_up();
+
+    EXPECT_TRUE(s.has_constraints());
+    EXPECT_EQ(s.get_constraint_dof(0), 1);
+    EXPECT_EQ(s.get_constraint_dof(1), 0);
+    EXPECT_EQ(s.get_constraint_dof(2), 2);
+
+    s.destroy();
+}
+
+TEST(SectionTest, field_ceds)
+{
+    Section s;
+    s.create(MPI_COMM_WORLD);
+    s.set_num_fields(2);
+    s.set_chart(0, 2);
+    s.set_dof(0, 1);
+    s.set_constraint_dof(0, 1);
+    s.set_field_constraint_dof(0, 0, 1);
+    s.set_field_dof(0, 1, 1);
+
+    s.set_dof(1, 1);
+    s.set_constraint_dof(1, 2);
+    s.set_field_dof(1, 0, 1);
+    s.set_field_constraint_dof(1, 1, 1);
+    s.add_field_constraint_dof(1, 1, 1);
+
+    s.set_up();
+
+    EXPECT_TRUE(s.has_constraints());
+    EXPECT_EQ(s.get_field_constraint_dof(0, 0), 1);
+    EXPECT_EQ(s.get_field_dof(0, 1), 1);
+    EXPECT_EQ(s.get_field_dof(1, 0), 1);
+    EXPECT_EQ(s.get_field_constraint_dof(1, 1), 2);
+
+    s.destroy();
+}
+
+TEST(SectionTest, get_field)
+{
+    Section s;
+    s.create(MPI_COMM_WORLD);
+    s.set_num_fields(2);
+    s.set_chart(0, 2);
+    s.set_dof(0, 3);
+    s.set_field_dof(0, 0, 2);
+    s.set_field_dof(0, 1, 1);
+    s.set_dof(1, 8);
+    s.set_field_dof(1, 0, 3);
+    s.set_field_dof(1, 1, 5);
+    s.set_point_major(true);
+    s.set_up();
+
+    Section f1 = s.get_field(0);
+    Int f1s, f1e;
+    f1.get_offset_range(f1s, f1e);
+    EXPECT_EQ(f1s, 0);
+    EXPECT_EQ(f1e, 6);
+
+    Section f2 = s.get_field(1);
+    Int f2s, f2e;
+    f2.get_offset_range(f2s, f2e);
+    EXPECT_EQ(f2s, 2);
+    EXPECT_EQ(f2e, 11);
+}
+
+TEST(SectionTest, view)
+{
+    testing::internal::CaptureStdout();
+
+    Section s;
+    s.create(MPI_COMM_WORLD);
+    s.set_chart(0, 2);
+    s.set_dof(0, 2);
+    s.set_dof(1, 3);
+    s.set_up();
+    s.view();
+    s.destroy();
+
+    auto output = testing::internal::GetCapturedStdout();
+    EXPECT_THAT(output, testing::HasSubstr("(   0) dim  2 offset   0"));
+    EXPECT_THAT(output, testing::HasSubstr("(   1) dim  3 offset   2"));
+}
