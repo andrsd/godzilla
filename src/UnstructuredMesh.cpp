677551f1bac25a34cb0ae0fa90feea67a63a62bb
David Andrs <andrsd@gmail.com>
2023-03-11 09:33:53 -0700

    Don't crash if there is no 'Face Sets' label in the mesh

--- src/UnstructuredMesh.cpp
+++ src/UnstructuredMesh.cpp
@@ -188,19 +188,21 @@ UnstructuredMesh::create_face_set_labels(const std::map<Int, std::string> & name
 {
     _F_;
     DMLabel fs_label = get_label("Face Sets");
-    Int n_fs;
-    DMLabelGetNumValues(fs_label, &n_fs);
-    IndexSet fs_ids = IndexSet::values_from_label(fs_label);
-    fs_ids.get_indices();
-    for (Int i = 0; i < n_fs; i++) {
-        Int id = fs_ids[i];
-        create_face_set(id);
-    }
-    fs_ids.restore_indices();
-    fs_ids.destroy();
+    if (fs_label) {
+        Int n_fs;
+        DMLabelGetNumValues(fs_label, &n_fs);
+        IndexSet fs_ids = IndexSet::values_from_label(fs_label);
+        fs_ids.get_indices();
+        for (Int i = 0; i < n_fs; i++) {
+            Int id = fs_ids[i];
+            create_face_set(id);
+        }
+        fs_ids.restore_indices();
+        fs_ids.destroy();
 
-    for (auto & it : names)
-        set_face_set_name(it.first, it.second);
+        for (auto & it : names)
+            set_face_set_name(it.first, it.second);
+    }
 }
 
 void
