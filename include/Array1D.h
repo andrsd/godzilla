9c49c4a7c76beb3e09ff02da9f4ae00a6b288331
David Andrs <andrsd@gmail.com>
2023-03-11 06:04:37 -0700

    Adding Array1D

---
+++ include/Array1D.h
@@ -0,0 +1,203 @@
+#pragma once
+
+#include "Types.h"
+#include "Error.h"
+#include <cassert>
+
+namespace godzilla {
+
+template <typename T>
+class Array1D {
+public:
+    /// Create an empty array
+    Array1D() : n(-1), data(nullptr) {}
+
+    /// Create an array with specified number of entries
+    ///
+    /// @param size Number of entries in the array
+    explicit Array1D(Int size) : n(size), data(new T[size]) { MEM_CHECK(this->data); }
+
+    /// Allocate memory for the array with specified number of entries
+    ///
+    /// @param size Number of entries in the array
+    void
+    create(Int size)
+    {
+        this->n = size;
+        this->data = new T[size];
+        MEM_CHECK(this->data);
+    }
+
+    /// Get number of entries in the array
+    ///
+    /// @return Number of entries in the array
+    Int
+    get_size() const
+    {
+        return this->n;
+    }
+
+    /// Get the entry at a specified location for reading
+    ///
+    /// @param i Index fo the entry
+    /// @return Entry at the `ith` location
+    const T &
+    get(Int i) const
+    {
+        assert(this->data != nullptr);
+        assert((i >= 0) && (i < this->n));
+        return this->data[i];
+    }
+
+    /// Get the entry at a specified location for writing
+    ///
+    /// @param i Index fo the entry
+    /// @return Entry at the `ith` location
+    T &
+    set(Int i)
+    {
+        assert(this->data != nullptr);
+        assert((i >= 0) && (i < this->n));
+        return this->data[i];
+    }
+
+    /// Set all entries in the array to zero
+    void
+    zero()
+    {
+        set_values(0.);
+    }
+
+    /// Free memory allocated by this array
+    void
+    destroy()
+    {
+        delete[] this->data;
+        this->data = nullptr;
+        this->n = -1;
+    }
+
+    /// Assign a value into all vector entries, i.e. `vec[i] = val`
+    ///
+    /// @param val Value to assign
+    void
+    set_values(const T & val)
+    {
+        assert(this->data != nullptr);
+        for (Int i = 0; i < this->n; i++)
+            this->data[i] = val;
+    }
+
+    /// Set multiple values into the vector
+    ///
+    /// @param vals Values to set into this vector
+    void
+    set_values(const std::vector<T> & vals)
+    {
+        assert(this->data != nullptr);
+        assert(this->n == vals.size());
+        for (Int i = 0; i < this->n; i++)
+            this->data[i] = vals[i];
+    }
+
+    /// Multiply all entries in the array by a value
+    ///
+    /// @param alpha Value to multiply all entries with
+    void
+    scale(Real alpha) const
+    {
+        for (Int i = 0; i < this->n; i++)
+            this->data[i] *= alpha;
+    }
+
+    /// Add alpha * x to this vector
+    ///
+    /// @param alpha Scalar value
+    /// @param x Vector to add
+    void
+    axpy(Real alpha, const Array1D<T> & x) const
+    {
+        error("AXPY not implemented");
+    }
+
+    /// Compute dot product of (this * `a`)
+    ///
+    /// @param a Vector to multiply with
+    /// @return Dot product
+    T
+    dot(const Array1D<T> & a) const
+    {
+        error("Dot product not implemented");
+    }
+
+    // operators
+
+    const T &
+    operator()(Int i) const
+    {
+        return get(i);
+    }
+
+    T &
+    operator()(Int i)
+    {
+        return set(i);
+    }
+
+    T
+    operator*(const Array1D<T> & a) const
+    {
+        return dot(a);
+    }
+
+    //
+
+    // Do your best to avoid abusing this API
+    T *
+    get_data() const
+    {
+        return this->data;
+    }
+
+private:
+    /// Number of entries in the array
+    Int n;
+    /// Array containing the values
+    T * data;
+};
+
+template <>
+void
+Array1D<Real>::axpy(Real alpha, const Array1D<Real> & x) const
+{
+    for (Int i = 0; i < this->n; i++)
+        this->data[i] = alpha * x(i) + this->data[i];
+}
+
+template <>
+Real
+Array1D<Real>::dot(const Array1D<Real> & a) const
+{
+    Real res = 0;
+    for (Int i = 0; i < this->n; i++)
+        res += get(i) * a(i);
+    return res;
+}
+
+// Output
+
+template <typename T>
+std::ostream &
+operator<<(std::ostream & os, const Array1D<T> & obj)
+{
+    os << "(";
+    for (Int i = 0; i < obj.get_size(); i++) {
+        os << obj(i);
+        if (i < obj.get_size() - 1)
+            os << ", ";
+    }
+    os << ")";
+    return os;
+}
+
+} // namespace godzilla
---
+++ test/src/Array1D_test.cpp
@@ -0,0 +1,121 @@
+#include "gmock/gmock.h"
+#include "Array1D.h"
+
+using namespace godzilla;
+
+TEST(Array1DTest, create)
+{
+    Array1D<Real> arr;
+    arr.create(10);
+    EXPECT_EQ(arr.get_size(), 10);
+    arr.destroy();
+}
+
+TEST(Array1DTest, const_op)
+{
+    Array1D<Real> arr;
+    arr.create(10);
+    for (Int i = 0; i < 10; i++)
+        arr(i) = i;
+
+    const Array1D<Real> & ca = arr;
+    for (Int i = 0; i < 10; i++)
+        EXPECT_EQ(ca(i), i);
+
+    arr.destroy();
+}
+
+TEST(Array1DTest, zero)
+{
+    Array1D<Real> arr(10);
+    for (Int i = 0; i < 10; i++)
+        arr(i) = i;
+    arr.zero();
+    for (Int i = 0; i < 10; i++)
+        EXPECT_EQ(arr(i), 0.);
+    arr.destroy();
+}
+
+TEST(Array1DTest, set_values)
+{
+    Array1D<Real> arr(10);
+    arr.set_values(1234);
+    for (Int i = 0; i < 10; i++)
+        EXPECT_EQ(arr(i), 1234.);
+    arr.destroy();
+}
+
+TEST(Array1DTest, scale)
+{
+    Array1D<Real> arr(5);
+    arr.set_values({ 2., 3., 1., -2., 0. });
+    arr.scale(3.);
+    Real gold[] = { 6., 9., 3., -6, 0. };
+    for (Int i = 0; i < 5; i++)
+        EXPECT_EQ(arr(i), gold[i]);
+    arr.destroy();
+}
+
+TEST(Array1DTest, axpy)
+{
+    Array1D<Real> y(5);
+    y.set_values({ 2., 3., 1., -2., 0. });
+    Array1D<Real> x(5);
+    x.set_values({ 1., -1., 3., 0., 2. });
+    y.axpy(2., x);
+    Real gold[] = { 4., 1., 7., -2., 4. };
+    for (Int i = 0; i < 5; i++)
+        EXPECT_EQ(y(i), gold[i]);
+    x.destroy();
+    y.destroy();
+}
+
+TEST(Array1DTest, get_data)
+{
+    Array1D<Real> y(5);
+    y.set_values({ 2., 3., 1., -2., 0. });
+    Real * raw = y.get_data();
+    EXPECT_EQ(raw[0], 2.);
+    EXPECT_EQ(raw[1], 3.);
+    EXPECT_EQ(raw[2], 1.);
+    EXPECT_EQ(raw[3], -2.);
+    EXPECT_EQ(raw[4], 0.);
+    y.destroy();
+}
+
+TEST(Array1DTest, dot)
+{
+    Array1D<Real> y(5);
+    y.set_values({ 2., 3., 1., -2., 0. });
+    Array1D<Real> x(5);
+    x.set_values({ 1., -1., 3., 0., 2. });
+    Real dot = x.dot(y);
+    EXPECT_EQ(dot, 2.);
+    y.destroy();
+    x.destroy();
+}
+
+TEST(Array1DTest, op_dot)
+{
+    Array1D<Real> y(5);
+    y.set_values({ 2., 3., 1., -2., 0. });
+    Array1D<Real> x(5);
+    x.set_values({ 1., -1., 3., 0., 2. });
+    Real dot = x * y;
+    EXPECT_EQ(dot, 2.);
+    x.destroy();
+    y.destroy();
+}
+
+TEST(Array1DTest, op_to_stream)
+{
+    testing::internal::CaptureStdout();
+
+    Array1D<Real> x(5);
+    x.set_values({ 1., -1., 3., 0., 2. });
+    std::cout << x;
+
+    EXPECT_THAT(testing::internal::GetCapturedStdout(), testing::HasSubstr("(1, -1, 3, 0, 2)"));
+
+    x.destroy();
+}
