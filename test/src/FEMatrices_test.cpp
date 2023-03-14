89c98163cec6e08378480f4cc8adbd8011ceb98e
David Andrs <andrsd@gmail.com>
2023-03-14 11:36:12 -0600

    Adding functions for FE assembly using analytical integration

---
+++ test/src/FEMatrices_test.cpp
@@ -0,0 +1,115 @@
+#include "gmock/gmock.h"
+#include "FEMatrices.h"
+
+using namespace godzilla;
+
+TEST(FEMatricesTest, mass_edge2)
+{
+    auto m = fe::matrix::mass<EDGE2>();
+    EXPECT_EQ(m(0, 0), 2.);
+    EXPECT_EQ(m(0, 1), 1.);
+    EXPECT_EQ(m(1, 0), 1.);
+    EXPECT_EQ(m(1, 1), 2.);
+}
+
+TEST(FEMatricesTest, mass_tri3)
+{
+    auto m = fe::matrix::mass<TRI3>();
+    EXPECT_EQ(m(0, 0), 2.);
+    EXPECT_EQ(m(0, 1), 1.);
+    EXPECT_EQ(m(0, 2), 1.);
+    EXPECT_EQ(m(1, 0), 1.);
+    EXPECT_EQ(m(1, 1), 2.);
+    EXPECT_EQ(m(1, 2), 1.);
+    EXPECT_EQ(m(2, 0), 1.);
+    EXPECT_EQ(m(2, 1), 1.);
+    EXPECT_EQ(m(2, 2), 2.);
+}
+
+TEST(FEMatricesTest, mass_quad4)
+{
+    EXPECT_DEATH(fe::matrix::mass<QUAD4>(), "Mass matrix in not implemented for QUAD4");
+}
+
+TEST(FEMatricesTest, mass_tet4)
+{
+    auto m = fe::matrix::mass<TET4>();
+    EXPECT_EQ(m(0, 0), 2.);
+    EXPECT_EQ(m(0, 1), 1.);
+    EXPECT_EQ(m(0, 2), 1.);
+    EXPECT_EQ(m(0, 3), 1.);
+    EXPECT_EQ(m(1, 0), 1.);
+    EXPECT_EQ(m(1, 1), 2.);
+    EXPECT_EQ(m(1, 2), 1.);
+    EXPECT_EQ(m(1, 3), 1.);
+    EXPECT_EQ(m(2, 0), 1.);
+    EXPECT_EQ(m(2, 1), 1.);
+    EXPECT_EQ(m(2, 2), 2.);
+    EXPECT_EQ(m(2, 3), 1.);
+    EXPECT_EQ(m(3, 0), 1.);
+    EXPECT_EQ(m(3, 1), 1.);
+    EXPECT_EQ(m(3, 2), 1.);
+    EXPECT_EQ(m(3, 3), 2.);
+}
+
+TEST(FEMatricesTest, mass_hex8)
+{
+    EXPECT_DEATH(fe::matrix::mass<HEX8>(), "Mass matrix in not implemented for HEX8");
+}
+
+TEST(FEMatricesTest, stiffness_edge2)
+{
+    auto m = fe::matrix::stiffness<EDGE2>();
+    EXPECT_EQ(m(0, 0), 1.);
+    EXPECT_EQ(m(0, 1), -1.);
+    EXPECT_EQ(m(1, 0), -1.);
+    EXPECT_EQ(m(1, 1), 1.);
+}
+
+TEST(FEMatricesTest, stiffness_tri3)
+{
+    auto m = fe::matrix::stiffness<TRI3>();
+    EXPECT_EQ(m(0, 0), 1.);
+    EXPECT_EQ(m(0, 1), -1.);
+    EXPECT_EQ(m(0, 2), 0.);
+    EXPECT_EQ(m(1, 0), -1.);
+    EXPECT_EQ(m(1, 1), 2.);
+    EXPECT_EQ(m(1, 2), -1.);
+    EXPECT_EQ(m(2, 0), 0.);
+    EXPECT_EQ(m(2, 1), -1.);
+    EXPECT_EQ(m(2, 2), 1);
+}
+
+TEST(FEMatricesTest, stiffness_quad4)
+{
+    EXPECT_DEATH(fe::matrix::stiffness<QUAD4>(), "Stiffness matrix in not implemented for QUAD4");
+}
+
+TEST(FEMatricesTest, stiffness_tet4)
+{
+    auto m = fe::matrix::stiffness<TET4>();
+    EXPECT_EQ(m(0, 0), 1.);
+    EXPECT_EQ(m(0, 1), -1.);
+    EXPECT_EQ(m(0, 2), 0.);
+    EXPECT_EQ(m(0, 3), 0.);
+
+    EXPECT_EQ(m(1, 0), -1.);
+    EXPECT_EQ(m(1, 1), 2.);
+    EXPECT_EQ(m(1, 2), -1.);
+    EXPECT_EQ(m(1, 3), 0.);
+
+    EXPECT_EQ(m(2, 0), 0.);
+    EXPECT_EQ(m(2, 1), -1.);
+    EXPECT_EQ(m(2, 2), 2.);
+    EXPECT_EQ(m(2, 3), -1.);
+
+    EXPECT_EQ(m(3, 0), 0.);
+    EXPECT_EQ(m(3, 1), 0.);
+    EXPECT_EQ(m(3, 2), -1.);
+    EXPECT_EQ(m(3, 3), 1.);
+}
+
+TEST(FEMatricesTest, stiffness_hex8)
+{
+    EXPECT_DEATH(fe::matrix::stiffness<HEX8>(), "Stiffness matrix in not implemented for HEX8");
+}
---
+++ test/src/FEVolumes_test.cpp
@@ -0,0 +1,100 @@
+#include "gmock/gmock.h"
+#include "FEVolumes.h"
+
+using namespace godzilla;
+
+TEST(FEVolumesTest, volume_edge2)
+{
+    DenseVector<DenseVector<Real, 1>, 2> coords;
+    coords(0) = DenseVector<Real, 1>({ 1. });
+    coords(1) = DenseVector<Real, 1>({ 3. });
+    EXPECT_EQ(fe::volume<EDGE2>(coords), 2.);
+}
+
+TEST(FEVolumesTest, volume_edge2_2d)
+{
+    DenseVector<DenseVector<Real, 2>, 2> coords;
+    coords(0) = DenseVector<Real, 2>({ 4., 9. });
+    coords(1) = DenseVector<Real, 2>({ 1., 5. });
+    EXPECT_EQ(fe::volume<EDGE2>(coords), 5.);
+}
+
+TEST(FEVolumesTest, volume_tri3)
+{
+    DenseVector<DenseVector<Real, 2>, 3> coords;
+    coords(0) = DenseVector<Real, 2>({ 0., 0. });
+    coords(1) = DenseVector<Real, 2>({ 1., 0. });
+    coords(2) = DenseVector<Real, 2>({ 0., 1. });
+    EXPECT_EQ(fe::volume<TRI3>(coords), 0.5);
+}
+
+TEST(FEVolumesTest, volume_tet4)
+{
+    DenseVector<DenseVector<Real, 3>, 4> coords;
+    coords(0) = DenseVector<Real, 3>({ 0., 0., 0 });
+    coords(1) = DenseVector<Real, 3>({ 1., 0., 0 });
+    coords(2) = DenseVector<Real, 3>({ 0., 1., 0. });
+    coords(3) = DenseVector<Real, 3>({ 0., 0., 1. });
+    EXPECT_EQ(fe::volume<TET4>(coords), 1. / 6.);
+}
+
+TEST(FEVolumesTest, calc_volumes)
+{
+    const Int N_NODES = 5;
+    const Int N_ELEMS = 4;
+    Array1D<DenseVector<Real, 2>> coords;
+    Array1D<DenseVector<Int, 3>> connect;
+    Array1D<godzilla::Real> fe_volume;
+    coords.create(N_NODES);
+    connect.create(N_ELEMS);
+    fe_volume.create(N_ELEMS);
+
+    coords(0) = DenseVector<Real, 2>({ 0, 0 });
+    coords(1) = DenseVector<Real, 2>({ 1, 0 });
+    coords(2) = DenseVector<Real, 2>({ 1, 1 });
+    coords(3) = DenseVector<Real, 2>({ 0, 1 });
+    coords(4) = DenseVector<Real, 2>({ 0.4, 0.2 });
+
+    connect(0) = DenseVector<Int, 3>({ 0, 1, 4 });
+    connect(1) = DenseVector<Int, 3>({ 1, 2, 4 });
+    connect(2) = DenseVector<Int, 3>({ 2, 3, 4 });
+    connect(3) = DenseVector<Int, 3>({ 3, 0, 4 });
+
+    fe::calc_volumes<TRI3>(coords, connect, fe_volume);
+
+    EXPECT_DOUBLE_EQ(fe_volume(0), 0.1);
+    EXPECT_DOUBLE_EQ(fe_volume(1), 0.3);
+    EXPECT_DOUBLE_EQ(fe_volume(2), 0.4);
+    EXPECT_DOUBLE_EQ(fe_volume(3), 0.2);
+
+    coords.destroy();
+    connect.destroy();
+    fe_volume.destroy();
+}
+
+TEST(FEVolumesTest, face_area_edge2)
+{
+    DenseVector<DenseVector<Real, 1>, 1> coords;
+    coords(0) = DenseVector<Real, 1>({ 0. });
+    auto A = fe::face_area<EDGE2>(coords);
+    EXPECT_DOUBLE_EQ(A, 1.);
+}
+
+TEST(FEVolumesTest, face_area_tri3)
+{
+    DenseVector<DenseVector<Real, 2>, 2> coords;
+    coords(0) = DenseVector<Real, 2>({ 0., 0. });
+    coords(1) = DenseVector<Real, 2>({ 1., 1. });
+    auto A = fe::face_area<TRI3>(coords);
+    EXPECT_DOUBLE_EQ(A, std::sqrt(2));
+}
+
+TEST(FEVolumesTest, face_area_tet4)
+{
+    DenseVector<DenseVector<Real, 3>, 3> coords;
+    coords(0) = DenseVector<Real, 3>({ 0., 0., 0. });
+    coords(1) = DenseVector<Real, 3>({ 1., 0., 0. });
+    coords(2) = DenseVector<Real, 3>({ 0., 1., 0. });
+    EXPECT_DEATH(fe::face_area<TET4>(coords),
+                 "Face area calculation for TET4 in 3 dimensions is not implemented.");
+}
