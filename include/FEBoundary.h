e737cf390ed2b43f1a87318ce8bbf7a853a93f17
David Andrs <andrsd@gmail.com>
2023-04-01 13:26:53 -0600

    Adding fe::BoundaryInfo

---
+++ include/FEBoundary.h
@@ -0,0 +1,234 @@
+#pragma once
+
+#include "CallStack.h"
+#include "Types.h"
+#include "IndexSet.h"
+#include "UnstructuredMesh.h"
+#include "DenseVector.h"
+#include "DenseMatrix.h"
+#include "Array1D.h"
+#include "Array2D.h"
+#include "FEGeometry.h"
+#include "FEVolumes.h"
+
+namespace godzilla {
+
+namespace fe {
+
+class BoundaryInfoAbstract {
+public:
+    /// Create boundary info
+    virtual void create() = 0;
+
+    /// Destroy boundary info
+    virtual void destroy() = 0;
+};
+
+template <ElementType ELEM_TYPE, Int DIM, Int N_ELEM_NODES = get_num_element_nodes(ELEM_TYPE)>
+class BoundaryInfo : public BoundaryInfoAbstract {
+public:
+    BoundaryInfo(const UnstructuredMesh * mesh,
+                 const Array1D<DenseVector<Real, DIM>> * coords,
+                 const Array1D<DenseVector<Int, N_ELEM_NODES>> * connect,
+                 const Array1D<std::vector<Int>> * nelcom,
+                 const Array1D<Real> * fe_volume,
+                 const Array1D<DenseVector<DenseVector<Real, DIM>, N_ELEM_NODES>> * grad_phi,
+                 const IndexSet & facets) :
+        mesh(mesh),
+        coords(coords),
+        connect(connect),
+        nelcom(nelcom),
+        fe_volume(fe_volume),
+        grad_phi(grad_phi),
+        facets(facets)
+    {
+        _F_;
+    }
+
+protected:
+    void
+    compute_face_normals()
+    {
+        _F_;
+        if (!this->facets.empty()) {
+            this->facets.get_indices();
+            Int n = this->facets.get_local_size();
+            this->length.create(n);
+            this->normal.create(n);
+
+            calc_face_length();
+            calc_face_normals();
+        }
+    }
+
+    void
+    compute_nodal_normals()
+    {
+        this->vertices = this->mesh->get_cone_recursive_vertices(this->facets);
+        this->vertices.sort_remove_dups();
+        if (!this->vertices.empty()) {
+            this->vertices.get_indices();
+            Int n = this->vertices.get_local_size();
+            this->nodal_normal.create(n);
+            calc_nodal_normals();
+        }
+    }
+
+    void
+    free()
+    {
+        if (!this->facets.empty()) {
+            this->facets.restore_indices();
+            this->facets.destroy();
+            this->length.destroy();
+            this->normal.destroy();
+        }
+        if (!this->vertices.empty()) {
+            this->vertices.restore_indices();
+            this->vertices.destroy();
+            this->nodal_normal.destroy();
+        }
+    }
+
+private:
+    Int
+    get_local_face_index(const std::vector<Int> & elem_conn, const std::vector<Int> & face_conn)
+    {
+        // this only works for simplexes
+        std::set<Int> ec(elem_conn.begin(), elem_conn.end());
+        std::set<Int> fc(face_conn.begin(), face_conn.end());
+        std::vector<Int> diff;
+        std::set_difference(ec.begin(),
+                            ec.end(),
+                            fc.begin(),
+                            fc.end(),
+                            std::inserter(diff, diff.begin()));
+        assert(diff.size() == 1);
+        auto it = std::find(elem_conn.begin(), elem_conn.end(), diff[0]);
+        return std::distance(elem_conn.begin(), it);
+    }
+
+    /// Compute face normals
+    void
+    calc_face_normals()
+    {
+        _F_;
+        for (Int i = 0; i < this->facets.get_local_size(); i++) {
+            auto face_conn = this->mesh->get_cell_connectivity(this->facets(i));
+            auto support = this->mesh->get_support(this->facets(i));
+            Int ie = support[0];
+            auto volume = (*this->fe_volume)(ie);
+            auto elem_conn = this->mesh->get_cell_connectivity(ie);
+            int local_idx = get_local_face_index(elem_conn, face_conn);
+            auto edge_length = this->length(i);
+            auto grad = (*this->grad_phi)(ie) (local_idx);
+            auto normal = fe::normal<ELEM_TYPE>(volume, edge_length, grad);
+            this->normal(i) = normal;
+        }
+    }
+
+    void
+    calc_face_length()
+    {
+        for (Int i = 0; i < this->facets.get_local_size(); i++) {
+            auto face_conn = this->mesh->get_cell_connectivity(this->facets(i));
+            // works for simplexes
+            DenseVector<Int, N_ELEM_NODES - 1> idx({ face_conn });
+            auto coords = this->coords->get_values(idx);
+            auto edge_length = fe::face_area<ELEM_TYPE>(coords);
+            this->length(i) = edge_length;
+        }
+    }
+
+    /// Compute nodal normals
+    void
+    calc_nodal_normals()
+    {
+        _F_;
+        auto vertex_range = this->mesh->get_vertex_range();
+        for (Int i = 0; i < this->vertices.get_local_size(); i++) {
+            Int vertex = this->vertices(i);
+            Int node = vertex - vertex_range.get_first();
+            DenseVector<Real, DIM> sum;
+            sum.zero();
+            for (Int iec = 0; iec < (*this->nelcom)(node).size(); iec++) {
+                auto ie = (*this->nelcom)(node)[iec];
+                auto idx = (*this->connect)(ie);
+                auto lnne = node_index(idx, node);
+                auto vol = (*this->fe_volume)(ie);
+                auto inc = vol * (*this->grad_phi)(ie) (lnne);
+                sum += inc;
+            }
+            auto mag = sum.magnitude();
+            this->nodal_normal(i) = (1. / mag) * sum;
+        }
+        correct_nodal_normals();
+    }
+
+    void
+    correct_nodal_normals()
+    {
+        _F_;
+    }
+
+    Int
+    node_index(const DenseVector<Int, N_ELEM_NODES> & idx, Int node_id)
+    {
+        for (Int i = 0; i < N_ELEM_NODES; i++)
+            if (idx(i) == node_id)
+                return i;
+        error("Did not find {} in indices", node_id);
+    }
+
+    /// Mesh
+    const UnstructuredMesh * mesh;
+    /// Coordinates
+    const Array1D<DenseVector<Real, DIM>> * coords;
+    /// Connectivity array
+    const Array1D<DenseVector<Int, N_ELEM_NODES>> * connect;
+    /// Indices of elements common to a node
+    const Array1D<std::vector<Int>> * nelcom;
+    /// Element volume
+    const Array1D<Real> * fe_volume;
+    /// Gradients of shape functions
+    const Array1D<DenseVector<DenseVector<Real, DIM>, N_ELEM_NODES>> * grad_phi;
+
+public:
+    /// IndexSet with boundary facets
+    IndexSet facets;
+    /// Boundary face length
+    Array1D<Real> length;
+    /// Boundary face unit outward normal
+    Array1D<DenseVector<Real, DIM>> normal;
+    /// IndexSet with boundary vertices
+    IndexSet vertices;
+    /// Boundary node unit outward normal
+    Array1D<DenseVector<Real, DIM>> nodal_normal;
+};
+
+template <>
+inline void
+BoundaryInfo<TRI3, 2, 3>::correct_nodal_normals()
+{
+    _F_;
+    std::map<Int, Int> idx_of;
+    for (Int i = 0; i < this->facets.get_local_size(); i++)
+        idx_of.insert(std::pair<Int, Int>(this->facets[i], i));
+
+    for (Int i = 0; i < this->vertices.get_local_size(); i++) {
+        Int vertex = this->vertices(i);
+        auto support = this->mesh->get_support(vertex);
+        IndexSet support_is = IndexSet::create_general(this->mesh->get_comm(), support);
+        auto common_edges = IndexSet::intersect(this->facets, support_is);
+        if (common_edges.get_size() == 1) {
+            common_edges.get_indices();
+            Int face_normal_idx = idx_of[common_edges[0]];
+            this->nodal_normal(i) = this->normal(face_normal_idx);
+            common_edges.restore_indices();
+        }
+    }
+}
+
+} // namespace fe
+
+} // namespace godzilla
---
+++ test/src/FEBoundary2D_test.cpp
@@ -0,0 +1,198 @@
+#include "gmock/gmock.h"
+#include "TestApp.h"
+#include "FEGeometry.h"
+#include "FEVolumes.h"
+#include "FEShapeFns.h"
+#include "FEBoundary.h"
+
+using namespace godzilla;
+using namespace testing;
+
+namespace {
+
+IndexSet
+points_from_label(const Label & label)
+{
+    auto is = label.get_values();
+    is.get_indices();
+    auto ids = is.to_std_vector();
+    is.restore_indices();
+    is.destroy();
+
+    return label.get_stratum(ids[0]);
+}
+
+}; // namespace
+
+namespace {
+
+class TestMesh2D : public godzilla::UnstructuredMesh {
+public:
+    explicit TestMesh2D(const godzilla::Parameters & parameters) : UnstructuredMesh(parameters) {}
+
+protected:
+    void
+    create_dm() override
+    {
+        const PetscInt DIM = 2;
+        const PetscInt N_ELEM_NODES = 3;
+
+        PetscInt n_cells = 2;
+        PetscInt n_verts = 4;
+        PetscInt cells[] = { 0, 1, 2, 1, 3, 2 };
+        double coords[] = { 0, 0, 1, 0, 0, 1, 1, 1 };
+        PETSC_CHECK(DMPlexCreateFromCellListPetsc(get_comm(),
+                                                  DIM,
+                                                  n_cells,
+                                                  n_verts,
+                                                  N_ELEM_NODES,
+                                                  PETSC_TRUE,
+                                                  cells,
+                                                  DIM,
+                                                  coords,
+                                                  &this->dm));
+
+        // create "side sets"
+        DMLabel face_sets = create_label("Face Sets");
+
+        create_side_set(face_sets, 1, { 8 }, "left");
+        create_side_set(face_sets, 2, { 6 }, "bottom");
+        create_side_set(face_sets, 3, { 9, 10 }, "top_right");
+
+        std::map<Int, std::string> face_set_names;
+        face_set_names[1] = "left";
+        face_set_names[2] = "bottom";
+        face_set_names[3] = "top_right";
+        create_face_set_labels(face_set_names);
+        for (auto it : face_set_names)
+            set_face_set_name(it.first, it.second);
+    }
+
+    void
+    create_side_set(const DMLabel & face_sets,
+                    Int id,
+                    const std::vector<Int> & faces,
+                    const char * name)
+    {
+        for (auto & f : faces) {
+            PETSC_CHECK(DMLabelSetValue(face_sets, f, id));
+            PETSC_CHECK(DMSetLabelValue(this->dm, name, f, id));
+        }
+    }
+
+public:
+    static godzilla::Parameters parameters();
+};
+
+class TestBoundary2D : public fe::BoundaryInfo<TRI3, 2, 3> {
+public:
+    TestBoundary2D(const UnstructuredMesh * mesh,
+                   const Array1D<DenseVector<Real, 2>> * coords,
+                   const Array1D<DenseVector<Int, 3>> * connect,
+                   const Array1D<std::vector<Int>> * nelcom,
+                   const Array1D<Real> * fe_volume,
+                   const Array1D<DenseVector<DenseVector<Real, 2>, 3>> * grad_phi,
+                   const IndexSet & facets) :
+        fe::BoundaryInfo<TRI3, 2, 3>(mesh, coords, connect, nelcom, fe_volume, grad_phi, facets)
+    {
+    }
+
+    void
+    create() override
+    {
+        _F_;
+        compute_face_normals();
+        compute_nodal_normals();
+    }
+
+    void
+    destroy() override
+    {
+        _F_;
+        free();
+    }
+};
+
+Parameters
+TestMesh2D::parameters()
+{
+    Parameters params = UnstructuredMesh::parameters();
+    return params;
+}
+
+} // namespace
+
+TEST(FEBoundaryTest, nodal_normals_2d)
+{
+    TestApp app;
+
+    Parameters mesh_pars = TestMesh2D::parameters();
+    mesh_pars.set<const godzilla::App *>("_app") = &app;
+    TestMesh2D mesh(mesh_pars);
+    mesh.create();
+
+    auto coords = fe::coordinates<2>(mesh);
+    auto connect = fe::connectivity<2, 3>(mesh);
+    auto fe_volume = fe::calc_volumes<TRI3, 2>(coords, connect);
+    auto grad_phi = fe::calc_grad_shape<TRI3, 2>(coords, connect, fe_volume);
+
+    auto n_nodes = mesh.get_num_vertices();
+
+    Array1D<std::vector<Int>> nelcom;
+    nelcom.create(n_nodes);
+    fe::common_elements_by_node(connect, nelcom);
+
+    {
+        auto label = mesh.get_label("left");
+        IndexSet bnd_facets = points_from_label(label);
+        TestBoundary2D bnd(&mesh, &coords, &connect, &nelcom, &fe_volume, &grad_phi, bnd_facets);
+        bnd.create();
+        EXPECT_DOUBLE_EQ(bnd.normal(0)(0), -1);
+        EXPECT_DOUBLE_EQ(bnd.normal(0)(1), 0);
+
+        EXPECT_DOUBLE_EQ(bnd.nodal_normal(0)(0), -1);
+        EXPECT_DOUBLE_EQ(bnd.nodal_normal(0)(1), 0);
+
+        EXPECT_DOUBLE_EQ(bnd.nodal_normal(1)(0), -1);
+        EXPECT_DOUBLE_EQ(bnd.nodal_normal(1)(1), 0);
+        bnd.destroy();
+    }
+
+    {
+        auto label = mesh.get_label("bottom");
+        IndexSet bnd_facets = points_from_label(label);
+        TestBoundary2D bnd(&mesh, &coords, &connect, &nelcom, &fe_volume, &grad_phi, bnd_facets);
+        bnd.create();
+        EXPECT_DOUBLE_EQ(bnd.normal(0)(0), 0);
+        EXPECT_DOUBLE_EQ(bnd.normal(0)(1), -1);
+
+        EXPECT_DOUBLE_EQ(bnd.nodal_normal(0)(0), 0);
+        EXPECT_DOUBLE_EQ(bnd.nodal_normal(0)(1), -1);
+
+        EXPECT_DOUBLE_EQ(bnd.nodal_normal(1)(0), 0);
+        EXPECT_DOUBLE_EQ(bnd.nodal_normal(1)(1), -1);
+        bnd.destroy();
+    }
+
+    {
+        auto label = mesh.get_label("top_right");
+        IndexSet bnd_facets = points_from_label(label);
+        TestBoundary2D bnd(&mesh, &coords, &connect, &nelcom, &fe_volume, &grad_phi, bnd_facets);
+        bnd.create();
+        EXPECT_DOUBLE_EQ(bnd.normal(0)(0), 1);
+        EXPECT_DOUBLE_EQ(bnd.normal(0)(1), 0);
+
+        EXPECT_DOUBLE_EQ(bnd.normal(1)(0), 0);
+        EXPECT_DOUBLE_EQ(bnd.normal(1)(1), 1);
+
+        EXPECT_DOUBLE_EQ(bnd.nodal_normal(0)(0), 1);
+        EXPECT_DOUBLE_EQ(bnd.nodal_normal(0)(1), 0);
+
+        EXPECT_DOUBLE_EQ(bnd.nodal_normal(1)(0), 0);
+        EXPECT_DOUBLE_EQ(bnd.nodal_normal(1)(1), 1);
+
+        EXPECT_DOUBLE_EQ(bnd.nodal_normal(2)(0), 1. / std::sqrt(2));
+        EXPECT_DOUBLE_EQ(bnd.nodal_normal(2)(1), 1. / std::sqrt(2));
+        bnd.destroy();
+    }
+}
