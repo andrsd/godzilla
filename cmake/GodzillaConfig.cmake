88f13aa69283275d7647f19799cc13ac9eed70b2
David Andrs <andrsd@gmail.com>
2023-07-24 14:39:44 -0600

    cmake: improving GodzillaConfig.cmake

--- cmake/GodzillaConfig.cmake
+++
@@ -1,8 +0,0 @@
-include(CMakeFindDependencyMacro)
-
-enable_language(C)
-find_dependency(HDF5 REQUIRED)
-find_dependency(fmt REQUIRED)
-find_dependency(yaml-cpp REQUIRED)
-
-include("${CMAKE_CURRENT_LIST_DIR}/GodzillaTargets.cmake")
