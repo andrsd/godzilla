a811f43d07b7ceb2eb5071f9db6a61a357b29dbd
David Andrs <andrsd@gmail.com>
2023-08-19 14:41:48 -0600

    Improving guarding of access to LateBindArray

--- include/FieldValue.h
+++ include/FieldValue.h
@@ -12,7 +12,7 @@ namespace godzilla {
 template <typename T>
 class LateBindArray {
 public:
-    LateBindArray() : data(nullptr) {}
+    LateBindArray(Int size) : size(size), data(nullptr) {}
 
     void
     set(T * new_data)
@@ -26,72 +26,56 @@ public:
         return this->data;
     }
 
+    T
+    operator()(unsigned int idx) const
+    {
+        assert(this->data != nullptr);
+        assert(idx < this->size);
+        return this->data[idx];
+    }
+
 protected:
+    /// Number of elements stored in `data`
+    Int size;
+    /// The elements of the array
     T * data;
 };
 
 /// Used for field values during assembling
 class FieldValue : public LateBindArray<Scalar> {
 public:
-    Scalar
-    operator()(unsigned int idx) const
-    {
-        assert(this->data != nullptr);
-        return this->data[idx];
-    }
+    /// Constructor
+    ///
+    /// @param nc Number of field components
+    explicit FieldValue(Int nc) : LateBindArray<Scalar>(nc) {}
 };
 
 /// Used for field gradient values during assembling
 class FieldGradient : public LateBindArray<Scalar> {
 public:
-    explicit FieldGradient(const Int & dim) : LateBindArray<Scalar>(), dim(dim) {}
-
-    FieldGradient(const FieldGradient & other) = default;
-
-    Scalar
-    operator()(unsigned int idx) const
-    {
-        assert(this->data != nullptr);
-        assert(idx < this->dim);
-        return this->data[idx];
-    }
-
-protected:
-    const Int & dim;
+    /// Constructor
+    ///
+    /// @param dim Spatial dimension
+    /// @param nc Number of field components
+    explicit FieldGradient(Int dim, Int nc) : LateBindArray<Scalar>(nc * dim) {}
 };
 
 /// Used for vector values during assembling (for example normals)
 class Normal : public LateBindArray<Real> {
 public:
-    explicit Normal(const Int & dim) : LateBindArray<Real>(), dim(dim) {}
-
-    Real
-    operator()(unsigned int idx) const
-    {
-        assert(this->data != nullptr);
-        assert(idx < this->dim);
-        return this->data[idx];
-    }
-
-protected:
-    const Int & dim;
+    /// Constructor
+    ///
+    /// @param dim Spatial dimension
+    explicit Normal(Int dim) : LateBindArray<Real>(dim) {}
 };
 
 /// Used for points during assembling (for example physical coordinates)
 class Point : public LateBindArray<Real> {
 public:
-    explicit Point(const Int & dim) : LateBindArray<Real>(), dim(dim) {}
-
-    Real
-    operator()(unsigned int idx) const
-    {
-        assert(this->data != nullptr);
-        assert(idx < this->dim);
-        return this->data[idx];
-    }
-
-protected:
-    const Int & dim;
+    /// Constructor
+    ///
+    /// @param dim Spatial dimension
+    explicit Point(Int dim) : LateBindArray<Real>(dim) {}
 };
 
 } // namespace godzilla
--- test/src/BndJacobianFunc_test.cpp
+++ test/src/BndJacobianFunc_test.cpp
@@ -122,9 +122,9 @@ TEST(BndJacobianFuncTest, test)
 
     Int dim;
     EXPECT_CALL(prob, get_spatial_dimension()).Times(1).WillOnce(ReturnRef(dim));
-    FieldValue val;
+    FieldValue val(1);
     EXPECT_CALL(prob, get_field_value(_)).Times(1).WillOnce(ReturnRef(val));
-    FieldGradient grad(1);
+    FieldGradient grad(1, 1);
     EXPECT_CALL(prob, get_field_gradient(_)).Times(1).WillOnce(ReturnRef(grad));
     Real time;
     EXPECT_CALL(prob, get_assembly_time()).Times(1).WillOnce(ReturnRef(time));
