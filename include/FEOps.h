67477372352eb96cd6ad88a7e59db9c2845b2ed6
David Andrs <andrsd@gmail.com>
2023-08-30 08:47:25 -0600

    Adding fe::gradient variant with vals coming as DenseMatrix

--- include/FEOps.h
+++ include/FEOps.h
@@ -64,6 +64,29 @@ gradient(const DenseVector<DenseVector<Real, N_COMPS>, N_VALS> & vals,
     return grad;
 }
 
+/// Compute gradient of a vector-valued quantity
+///
+/// @tparam N_COMPS Number of vector component
+/// @tparam D Spatial dimension
+/// @tparam N_VALS Number of values per element
+/// @param vals Nodal values
+/// @param grad_phi Gradient of test functions
+/// @return Computed gradient
+template <Int N_COMPS, Int D, Int N_VALS>
+inline DenseMatrix<Real, N_COMPS, D>
+gradient(const DenseMatrix<Real, N_VALS, N_COMPS> & vals,
+         const DenseVector<DenseVector<Real, D>, N_VALS> & grad_phi)
+{
+    DenseMatrix<Real, N_COMPS, D> grad;
+    grad.set_values(0.);
+    for (Int i = 0; i < N_COMPS; i++) {
+        for (Int j = 0; j < N_VALS; j++)
+            for (Int k = 0; k < D; k++)
+                grad(i, k) += vals(j, i) * grad_phi(j)(k);
+    }
+    return grad;
+}
+
 /// Compute gradient(u) * gradient(phi) (at nodes)
 ///
 /// @tparam DIM Spatial dimension
