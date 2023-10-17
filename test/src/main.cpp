bfb9d173b6b714d3982ff4303e7b02d627411cd0
David Andrs <andrsd@gmail.com>
2023-10-17 15:00:03 -0600

    Marking death test with threadsafe flag

--- test/src/main.cpp
+++ test/src/main.cpp
@@ -5,6 +5,7 @@ int
 main(int argc, char ** argv)
 {
     ::testing::InitGoogleTest(&argc, argv);
+    GTEST_FLAG_SET(death_test_style, "threadsafe");
     godzilla::Init init(argc, argv);
     return RUN_ALL_TESTS();
 }
