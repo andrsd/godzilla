4421b55d39ca9df6add928b7a674a111240cbce3
David Andrs <andrsd@gmail.com>
2024-01-01 10:31:04 -0700

    Adding unit tests for `StarForest::view`

--- test/src/StarForrest_test.cpp
+++ test/src/StarForrest_test.cpp
@@ -3,6 +3,7 @@
 #include "TestApp.h"
 
 using namespace godzilla;
+using namespace testing;
 
 TEST(StarForestTest, test)
 {
@@ -36,4 +37,31 @@ TEST(StarForestTest, test)
     EXPECT_EQ(remote_leaves_arr[0].index, 0);
 
     sf.destroy();
+}
+
+TEST(StarForestTest, view)
+{
+    testing::internal::CaptureStdout();
+
+    TestApp app;
+    StarForest sf;
+    sf.create(app.get_comm());
+    Int n_leaves = 1;
+    Int n_roots = 1;
+    std::vector<Int> ilocal(n_leaves);
+    for (Int i = 0; i < n_leaves; i++)
+        ilocal[i] = i;
+    std::vector<StarForest::Node> iremote(n_leaves);
+    iremote[0].rank = 0;
+    iremote[0].index = 0;
+    sf.set_graph(n_roots, n_leaves, ilocal, iremote);
+    sf.view();
+    sf.destroy();
+
+    auto out = testing::internal::GetCapturedStdout();
+    EXPECT_THAT(out, HasSubstr("PetscSF Object: 1 MPI process"));
+    EXPECT_THAT(out, HasSubstr("type: basic"));
+    EXPECT_THAT(out, HasSubstr("[0] Number of roots=1, leaves=1, remote ranks=1"));
+    EXPECT_THAT(out, HasSubstr("[0] 0 <- (0,0)"));
+    EXPECT_THAT(out, HasSubstr("MultiSF sort=rank-order"));
 }
