636cf73e8ee102518d6467a7dd6e4cbd9db47b70
David Andrs <andrsd@gmail.com>
2024-01-15 08:10:04 -0700

    Adding `TimeStepAdapt` wrapper
    Light wrapper around `TSAdapt`

---
+++ include/godzilla/TimeStepAdapt.h
@@ -0,0 +1,158 @@
+// SPDX-FileCopyrightText: 2024 David Andrs <andrsd@gmail.com>
+// SPDX-License-Identifier: MIT
+
+#pragma once
+
+#include "petscts.h"
+#include "godzilla/Types.h"
+#include "godzilla/Vector.h"
+
+namespace godzilla {
+
+class TimeStepAdapt {
+public:
+    enum Type { NONE, BASIC, DSP, CFL, GLEE, HISTORY };
+
+    struct Candidate {
+        Int order;
+        Int stage_order;
+        Real ccfl;
+        Real cost;
+    };
+
+    TimeStepAdapt();
+    explicit TimeStepAdapt(TSAdapt tsadapt);
+
+    /// Add a candidate scheme for the adaptive controller to select from
+    ///
+    /// @param name Name of the candidate scheme to add
+    /// @param order Order of the candidate scheme
+    /// @param stage_order Stage order of the candidate scheme
+    /// @param ccfl Stability coefficient relative to explicit Euler, used for CFL constraints
+    /// @param cost Relative measure of the amount of work required for the candidate scheme
+    /// @param inuse Indicates that this scheme is the one currently in use, this flag can only be
+    /// set for one scheme
+    void candidate_add(const std::string & name,
+                       Int order,
+                       Int stage_order,
+                       Real ccfl,
+                       Real cost,
+                       bool inuse);
+
+    /// Clear any previously set candidate schemes
+    void candidates_clear();
+
+    std::vector<Candidate> get_candidates() const;
+
+    /// Checks whether to accept a stage, (e.g. reject and change time step size if nonlinear
+    /// solve fails or solution vector is infeasible)
+    ///
+    /// @param ts Time stepper
+    /// @param t Current simulation time
+    /// @param Y Current solution vector
+    /// @return `true` to accept the stage, `false` to reject
+    bool check_stage(TS ts, Real t, const Vector & Y) const;
+
+    /// Choose which method and step size to use for the next step
+    ///
+    /// @param ts Time stepper
+    /// @param h Current step size
+    /// @return [next_sc, next_h, accept]
+    /// - next_sc: scheme to use for the next step
+    /// - next_h: step size to use for the next step
+    /// - accept: `true` to accept the current step, `false` to repeat the current step with the
+    ///   new step size
+    std::tuple<Int, Real, bool> choose(TS ts, Real h);
+
+    /// Gets the admissible decrease/increase factor in step size in the time step adapter
+    ///
+    /// @return [low, high]
+    /// - low: admissible decrease factor,
+    /// - high: admissible increase factor
+    std::tuple<Real, Real> get_clip() const;
+
+    /// Get error estimation threshold. Solution components below this threshold value will not be
+    /// considered when computing error norms for time step adaptivity (in absolute value).
+    ///
+    /// @return Threshold for solution components that are ignored during error estimation
+    Real get_max_ignore() const;
+
+    /// Get safety factors for time step adapter
+    ///
+    /// @return [safety, reject_safety]
+    /// - safety: safety factor relative to target error/stability goal
+    /// - reject_safety: extra safety factor to apply if the last step was rejected
+    std::tuple<Real, Real> get_safety() const;
+
+    /// Gets the admissible decrease/increase factor in step size
+    ///
+    /// @return Scale factor
+    Real get_scale_solve_failed() const;
+
+    /// Get the minimum and maximum step sizes to be considered by the time step controller
+    ///
+    /// @return [hmin, hmax]
+    /// - hmin: minimum time step
+    /// - hmax: maximum time step
+    std::tuple<Real, Real> get_step_limits() const;
+
+    /// Get the adapter method type (as a string)
+    ///
+    /// @return The name of adapter method
+    std::string get_type() const;
+
+    /// Reset the time stepper to its defaults
+    void reset();
+
+    /// Set whether to always accept steps regardless of any error or stability condition not
+    /// meeting the prescribed goal.
+    ///
+    /// @param flag Whether to always accept steps
+    void set_always_accept(bool flag);
+
+    /// Sets the admissible decrease/increase factor in step size in the time step adapter
+    ///
+    /// @param low Admissible decrease factor
+    /// @param high Admissible increase factor
+    void set_clip(Real low, Real high);
+
+    /// Set error estimation threshold. Solution components below this threshold value will not be
+    /// considered when computing error norms for time step adaptivity (in absolute value). A
+    /// negative value (default) of the threshold leads to considering all solution components.
+    ///
+    /// @param max_ignore Threshold for solution components that are ignored during error estimation
+    void set_max_ignore(Real max_ignore);
+
+    /// Set safety factors for time step adaptor
+    ///
+    /// @param safety Safety factor relative to target error/stability goal
+    /// @param reject_safety Extra safety factor to apply if the last step was rejected
+    void set_safety(Real safety, Real reject_safety);
+
+    /// Scale step size by this factor if solve fails
+    ///
+    /// @param scale Scale
+    void set_scale_solve_failed(Real scale);
+
+    /// Set the minimum and maximum step sizes to be considered by the time step controller
+    ///
+    /// @param hmin Minimum time step
+    /// @param hmax Maximum time step
+    void set_step_limits(Real hmin, Real hmax);
+
+    /// Set the adapter method
+    ///
+    /// @param type Adapter method
+    void set_type(const std::string & type);
+
+    /// Set the adapter method
+    void set_type(Type type);
+
+private:
+    TSAdapt tsadapt;
+
+public:
+    static TimeStepAdapt from_ts(TS ts);
+};
+
+} // namespace godzilla
---
+++ src/TimeStepAdapt.cpp
@@ -0,0 +1,219 @@
+// SPDX-FileCopyrightText: 2024 David Andrs <andrsd@gmail.com>
+// SPDX-License-Identifier: MIT
+
+#include "godzilla/TimeStepAdapt.h"
+#include "godzilla/CallStack.h"
+#include "godzilla/Error.h"
+#include "godzilla/Exception.h"
+
+namespace godzilla {
+
+TimeStepAdapt::TimeStepAdapt() : tsadapt(nullptr) {}
+
+TimeStepAdapt::TimeStepAdapt(TSAdapt tsadapt) : tsadapt(tsadapt) {}
+
+void
+TimeStepAdapt::candidate_add(const std::string & name,
+                             Int order,
+                             Int stage_order,
+                             Real ccfl,
+                             Real cost,
+                             bool inuse)
+{
+    CALL_STACK_MSG();
+    PETSC_CHECK(TSAdaptCandidateAdd(this->tsadapt,
+                                    name.c_str(),
+                                    order,
+                                    stage_order,
+                                    ccfl,
+                                    cost,
+                                    inuse ? PETSC_TRUE : PETSC_FALSE));
+}
+
+void
+TimeStepAdapt::candidates_clear()
+{
+    CALL_STACK_MSG();
+    PETSC_CHECK(TSAdaptCandidatesClear(this->tsadapt));
+}
+
+std::vector<TimeStepAdapt::Candidate>
+TimeStepAdapt::get_candidates() const
+{
+    CALL_STACK_MSG();
+    Int n;
+    const Int * order;
+    const Int * stage_order;
+    const Real * ccfl;
+    const Real * cost;
+    PETSC_CHECK(TSAdaptCandidatesGet(this->tsadapt, &n, &order, &stage_order, &ccfl, &cost));
+    std::vector<TimeStepAdapt::Candidate> candidates(n);
+    for (Int i = 0; i < n; i++) {
+        candidates[i].order = order[i];
+        candidates[i].stage_order = stage_order[i];
+        candidates[i].ccfl = ccfl[i];
+        candidates[i].cost = cost[i];
+    }
+    return candidates;
+}
+
+bool
+TimeStepAdapt::check_stage(TS ts, Real t, const Vector & Y) const
+{
+    CALL_STACK_MSG();
+    PetscBool accept;
+    PETSC_CHECK(TSAdaptCheckStage(this->tsadapt, ts, t, Y, &accept));
+    return accept == PETSC_TRUE;
+}
+
+std::tuple<Int, Real, bool>
+TimeStepAdapt::choose(TS ts, Real h)
+{
+    CALL_STACK_MSG();
+    Int next_sc;
+    Real next_h;
+    PetscBool accept;
+    PETSC_CHECK(TSAdaptChoose(this->tsadapt, ts, h, &next_sc, &next_h, &accept));
+    return { next_sc, next_h, accept == PETSC_TRUE };
+}
+
+std::tuple<Real, Real>
+TimeStepAdapt::get_clip() const
+{
+    CALL_STACK_MSG();
+    Real low, high;
+    PETSC_CHECK(TSAdaptGetClip(this->tsadapt, &low, &high));
+    return { low, high };
+}
+
+Real
+TimeStepAdapt::get_max_ignore() const
+{
+    CALL_STACK_MSG();
+    Real max_ignore;
+    PETSC_CHECK(TSAdaptGetMaxIgnore(this->tsadapt, &max_ignore));
+    return max_ignore;
+}
+
+std::tuple<Real, Real>
+TimeStepAdapt::get_safety() const
+{
+    CALL_STACK_MSG();
+    Real safety, reject_safety;
+    PETSC_CHECK(TSAdaptGetSafety(this->tsadapt, &safety, &reject_safety));
+    return { safety, reject_safety };
+}
+
+Real
+TimeStepAdapt::get_scale_solve_failed() const
+{
+    CALL_STACK_MSG();
+    Real scale;
+    PETSC_CHECK(TSAdaptGetScaleSolveFailed(this->tsadapt, &scale));
+    return scale;
+}
+
+std::tuple<Real, Real>
+TimeStepAdapt::get_step_limits() const
+{
+    CALL_STACK_MSG();
+    Real hmin, hmax;
+    PETSC_CHECK(TSAdaptGetStepLimits(this->tsadapt, &hmin, &hmax));
+    return { hmin, hmax };
+}
+
+std::string
+TimeStepAdapt::get_type() const
+{
+    CALL_STACK_MSG();
+    TSAdaptType type;
+    PETSC_CHECK(TSAdaptGetType(this->tsadapt, &type));
+    return { type };
+}
+
+void
+TimeStepAdapt::reset()
+{
+    CALL_STACK_MSG();
+    PETSC_CHECK(TSAdaptReset(this->tsadapt));
+}
+
+void
+TimeStepAdapt::set_always_accept(bool flag)
+{
+    CALL_STACK_MSG();
+    PETSC_CHECK(TSAdaptSetAlwaysAccept(this->tsadapt, flag ? PETSC_TRUE : PETSC_FALSE));
+}
+
+void
+TimeStepAdapt::set_clip(Real low, Real high)
+{
+    CALL_STACK_MSG();
+    PETSC_CHECK(TSAdaptSetClip(this->tsadapt, low, high));
+}
+
+void
+TimeStepAdapt::set_max_ignore(Real max_ignore)
+{
+    CALL_STACK_MSG();
+    PETSC_CHECK(TSAdaptSetMaxIgnore(this->tsadapt, max_ignore));
+}
+
+void
+TimeStepAdapt::set_safety(Real safety, Real reject_safety)
+{
+    CALL_STACK_MSG();
+    PETSC_CHECK(TSAdaptSetSafety(this->tsadapt, safety, reject_safety));
+}
+
+void
+TimeStepAdapt::set_scale_solve_failed(Real scale)
+{
+    CALL_STACK_MSG();
+    PETSC_CHECK(TSAdaptSetScaleSolveFailed(this->tsadapt, scale));
+}
+
+void
+TimeStepAdapt::set_step_limits(Real hmin, Real hmax)
+{
+    CALL_STACK_MSG();
+    PETSC_CHECK(TSAdaptSetStepLimits(this->tsadapt, hmin, hmax));
+}
+
+void
+TimeStepAdapt::set_type(const std::string & type)
+{
+    CALL_STACK_MSG();
+    PETSC_CHECK(TSAdaptSetType(this->tsadapt, type.c_str()));
+}
+
+void
+TimeStepAdapt::set_type(Type type)
+{
+    CALL_STACK_MSG();
+    if (type == NONE)
+        set_type(TSADAPTNONE);
+    else if (type == BASIC)
+        set_type(TSADAPTBASIC);
+    else if (type == DSP)
+        set_type(TSADAPTDSP);
+    else if (type == CFL)
+        set_type(TSADAPTCFL);
+    else if (type == GLEE)
+        set_type(TSADAPTGLEE);
+    else if (type == HISTORY)
+        set_type(TSADAPTHISTORY);
+    else
+        throw Exception("Unknown time stepping adaptivity method");
+}
+
+TimeStepAdapt
+TimeStepAdapt::from_ts(TS ts)
+{
+    CALL_STACK_MSG();
+    TSAdapt adapt;
+    PETSC_CHECK(TSGetAdapt(ts, &adapt));
+    return TimeStepAdapt(adapt);
+}
+
+} // namespace godzilla
