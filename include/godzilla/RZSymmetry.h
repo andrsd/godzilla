ce35d376ff7a92e389bd516f4921a8a4c6715de8
David Andrs <andrsd@gmail.com>
2024-03-05 09:00:56 -0700

    Adding RZSymmetry object
    This is a user-facing object to setup axisymmetric problems. It is the application
    responsibility to include this in the input file syntax and use this.

---
+++ include/godzilla/RZSymmetry.h
@@ -0,0 +1,33 @@
+// SPDX-FileCopyrightText: 2024 David Andrs <andrsd@gmail.com>
+// SPDX-License-Identifier: MIT
+
+#pragma once
+
+#include "godzilla/Object.h"
+#include "godzilla/Types.h"
+#include "godzilla/DenseVector.h"
+
+namespace godzilla {
+
+class DiscreteProblemInterface;
+
+class RZSymmetry : public Object {
+public:
+    RZSymmetry(const Parameters & params);
+
+    void create() override;
+    Real get_value(Real time, const DenseVector<Real, 2> & x);
+
+private:
+    /// Discrete problem this object is part of
+    DiscreteProblemInterface * dpi;
+    /// Axis vector
+    const std::vector<Real> & axis;
+    /// Axis point
+    const std::vector<Real> & pt;
+
+public:
+    static Parameters parameters();
+};
+
+} // namespace godzilla
