3d14fd94e9ba3046de3936d6482b78af0e0fbe08
David Andrs <andrsd@gmail.com>
2024-06-05 13:23:34 -0600

    Removing unneeded typedef

--- include/godzilla/Types.h
+++ include/godzilla/Types.h
@@ -13,45 +13,6 @@ typedef PetscReal Real;
 typedef PetscScalar Scalar;
 typedef PetscErrorCode ErrorCode;
 
-/// This is the API that we hand to PETSc for fields.
-///
-/// @param dim The spatial dimension
-/// @param Nf The number of input fields
-/// @param NfAux The number of input auxiliary fields
-/// @param uOff The offset of each field in u[]
-/// @param uOff_x The offset of each field in u_x[]
-/// @param u The field values at this point in space
-/// @param u_t The field time derivative at this point in space (or `nullptr`)
-/// @param u_x The field derivatives at this point in space
-/// @param aOff The offset of each auxiliary field in u[]
-/// @param aOff_x The offset of each auxiliary field in u_x[]
-/// @param a The auxiliary field values at this point in space
-/// @param a_t The auxiliary field time derivative at this point in space (or `nullptr`)
-/// @param a_x The auxiliary field derivatives at this point in space
-/// @param t The current time
-/// @param x The coordinates of this point
-/// @param numConstants The number of constants
-/// @param constants The value of each constant
-/// @param f The value of the function at this point in space
-typedef void PetscFieldFunc(Int dim,
-                            Int Nf,
-                            Int NfAux,
-                            const Int uOff[],
-                            const Int uOff_x[],
-                            const Scalar u[],
-                            const Scalar u_t[],
-                            const Scalar u_x[],
-                            const Int aOff[],
-                            const Int aOff_x[],
-                            const Scalar a[],
-                            const Scalar a_t[],
-                            const Scalar a_x[],
-                            Real t,
-                            const Real x[],
-                            Int numConstants,
-                            const Scalar constants[],
-                            Scalar f[]);
-
 typedef ErrorCode PetscFunc(Int dim, Real time, const Real x[], Int Nc, Scalar u[], void * ctx);
 
 //
