41e0ffb9ea7e6bb731d38f0805faa322e2187591
David Andrs <andrsd@gmail.com>
2024-06-06 16:04:51 -0600

    Putting back a removed test

--- test/src/ParsedFunction_test.cpp
+++ test/src/ParsedFunction_test.cpp
@@ -73,3 +73,47 @@ TEST(ParsedFunctionTest, multi_eval)
     EXPECT_EQ(u[0], 3.);
     EXPECT_EQ(u[1], 4.);
 }
+
+TEST(ParsedFunctionTest, eval_via_parser)
+{
+    TestApp app;
+
+    Parameters mesh_params = RectangleMesh::parameters();
+    mesh_params.set<App *>("_app") = &app;
+    mesh_params.set<Int>("nx") = 2;
+    mesh_params.set<Int>("ny") = 1;
+    RectangleMesh mesh(mesh_params);
+
+    Parameters prob_params = GTestFENonlinearProblem::parameters();
+    prob_params.set<App *>("_app") = &app;
+    prob_params.set<MeshObject *>("_mesh_obj") = &mesh;
+    GTestFENonlinearProblem prob(prob_params);
+    app.set_problem(&prob);
+
+    mesh.create();
+    prob.create();
+
+    Parameters params = ParsedFunction::parameters();
+    params.set<App *>("_app") = &app;
+    params.set<std::string>("_name") = "exact_fn";
+    params.set<std::vector<std::string>>("function") = { "t + x + y" };
+    ParsedFunction obj(params);
+
+    mu::Parser parser;
+    obj.register_callback(parser);
+
+    parser.SetExpr("exact_fn(t, x, y, z)");
+
+    Real time = 1;
+    Real xx[2] = { 2, 3 };
+    Real zero = 0.;
+    parser.DefineVar("t", &time);
+    parser.DefineVar("x", &(xx[0]));
+    parser.DefineVar("y", &(xx[1]));
+    parser.DefineVar("z", &zero);
+
+    int n_num;
+    mu::value_type * val = parser.Eval(n_num);
+    EXPECT_EQ(1, n_num);
+    EXPECT_DOUBLE_EQ(val[0], 6.);
+}
