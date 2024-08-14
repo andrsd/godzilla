9e230aa0126ace073691e76512c24d5f965f479d
David Andrs <andrsd@gmail.com>
2024-08-14 11:09:34 -0600

    Adding grad_shape for TET4

--- include/godzilla/FEShapeFns.h
+++ include/godzilla/FEShapeFns.h
@@ -65,6 +65,54 @@ grad_shape<TRI3, 2>(const DenseMatrix<Real, 3, 2> & coords, Real volume)
     return grads;
 }
 
+/// Compute gradients of shape functions of TET4 in 3-D
+template <>
+inline DenseMatrix<Real, 3, 4>
+grad_shape<TET4, 3>(const DenseMatrix<Real, 4, 3> & coords, Real volume)
+{
+    // See [1] for details
+    // [1] https://en.wikipedia.org/wiki/Barycentric_coordinate_system
+    CALL_STACK_MSG();
+    auto x1 = coords(0, 0);
+    auto y1 = coords(0, 1);
+    auto z1 = coords(0, 2);
+
+    auto x2 = coords(1, 0);
+    auto y2 = coords(1, 1);
+    auto z2 = coords(1, 2);
+
+    auto x3 = coords(2, 0);
+    auto y3 = coords(2, 1);
+    auto z3 = coords(2, 2);
+
+    auto x4 = coords(3, 0);
+    auto y4 = coords(3, 1);
+    auto z4 = coords(3, 2);
+
+    DenseMatrix<Real, 3, 4> grads;
+    auto ddx1 = -y2 * z3 + y2 * z4 + y3 * z2 - y3 * z4 - y4 * z2 + y4 * z3;
+    auto ddx2 = y1 * z3 - y1 * z4 - y3 * z1 + y3 * z4 + y4 * z1 - y4 * z3;
+    auto ddx3 = -y1 * z2 + y1 * z4 + y2 * z1 - y2 * z4 - y4 * z1 + y4 * z2;
+    auto ddx4 = y1 * z2 - y1 * z3 - y2 * z1 + y2 * z3 + y3 * z1 - y3 * z2;
+
+    auto ddy1 = x2 * z3 - x2 * z4 - x3 * z2 + x3 * z4 + x4 * z2 - x4 * z3;
+    auto ddy2 = -x1 * z3 + x1 * z4 + x3 * z1 - x3 * z4 - x4 * z1 + x4 * z3;
+    auto ddy3 = x1 * z2 - x1 * z4 - x2 * z1 + x2 * z4 + x4 * z1 - x4 * z2;
+    auto ddy4 = -x1 * z2 + x1 * z3 + x2 * z1 - x2 * z3 - x3 * z1 + x3 * z2;
+
+    auto ddz1 = -x2 * y3 + x2 * y4 + x3 * y2 - x3 * y4 - x4 * y2 + x4 * y3;
+    auto ddz2 = x1 * y3 - x1 * y4 - x3 * y1 + x3 * y4 + x4 * y1 - x4 * y3;
+    auto ddz3 = -x1 * y2 + x1 * y4 + x2 * y1 - x2 * y4 - x4 * y1 + x4 * y2;
+    auto ddz4 = x1 * y2 - x1 * y3 - x2 * y1 + x2 * y3 + x3 * y1 - x3 * y2;
+
+    grads.set_col(0, { ddx1, ddy1, ddz1 });
+    grads.set_col(1, { ddx2, ddy2, ddz2 });
+    grads.set_col(2, { ddx3, ddy3, ddz3 });
+    grads.set_col(3, { ddx4, ddy4, ddz4 });
+    grads.scale(1. / 6. / volume);
+    return grads;
+}
+
 template <ElementType ELEM_TYPE, Int DIM, Int N_ELEM_NODES = get_num_element_nodes(ELEM_TYPE)>
 inline Array1D<DenseMatrix<Real, DIM, N_ELEM_NODES>>
 calc_grad_shape(const Array1D<DenseVector<Real, DIM>> & coords,
