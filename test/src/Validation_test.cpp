6e23e71665967748d1c01750eedc34b139440ebc
David Andrs <andrsd@gmail.com>
2024-10-24 11:34:49 -0600

    Templating `validation::in`

--- test/src/Validation_test.cpp
+++ test/src/Validation_test.cpp
@@ -3,8 +3,22 @@
 
 using namespace godzilla;
 
-TEST(ValidationTest, in)
+TEST(ValidationTest, in_const_char)
 {
     EXPECT_TRUE(validation::in("a", { "q", "a", "z" }));
     EXPECT_FALSE(validation::in("p", { "q", "a", "z" }));
+}
+
+TEST(ValidationTest, in_str)
+{
+    std::string a = "a";
+    EXPECT_TRUE(validation::in(a, { "q", "a", "z" }));
+    std::string p = "p";
+    EXPECT_FALSE(validation::in(p, { "q", "a", "z" }));
+}
+
+TEST(ValidationTest, in_int)
+{
+    EXPECT_TRUE(validation::in(1, { 1, 2, 3, 4 }));
+    EXPECT_FALSE(validation::in(10, { 1, 2, 3, 4 }));
 }
