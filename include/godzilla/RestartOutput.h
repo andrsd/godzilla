425a795ad272675ff44647499a68fd6dd38af750
David Andrs <andrsd@gmail.com>
2025-01-26 08:18:43 -0700

    Adding restart output

---
+++ include/godzilla/RestartOutput.h
@@ -0,0 +1,28 @@
+// SPDX-FileCopyrightText: 2025 David Andrs <andrsd@gmail.com>
+// SPDX-License-Identifier: MIT
+
+#pragma once
+
+#include "RestartInterface.h"
+#include "godzilla/FileOutput.h"
+
+namespace godzilla {
+
+class RestartOutput : public FileOutput {
+public:
+    explicit RestartOutput(const Parameters & params);
+
+    void create() override;
+    void output_step() override;
+
+private:
+    std::string get_file_ext() const override;
+
+    /// Restart interface
+    RestartInterface * ri;
+
+public:
+    static Parameters parameters();
+};
+
+} // namespace godzilla
---
+++ src/RestartOutput.cpp
@@ -0,0 +1,50 @@
+// SPDX-FileCopyrightText: 2025 David Andrs <andrsd@gmail.com>
+// SPDX-License-Identifier: MIT
+
+#include "godzilla/RestartOutput.h"
+#include "godzilla/CallStack.h"
+#include "godzilla/Parameters.h"
+#include "godzilla/Problem.h"
+#include "godzilla/RestartFile.h"
+#include "godzilla/RestartInterface.h"
+
+namespace godzilla {
+
+Parameters
+RestartOutput::parameters()
+{
+    auto params = FileOutput::parameters();
+    params.set<std::vector<std::string>>("on") = { "final" };
+    return params;
+}
+
+RestartOutput::RestartOutput(const Parameters & params) :
+    FileOutput(params),
+    ri(dynamic_cast<RestartInterface *>(get_problem()))
+{
+}
+
+void
+RestartOutput::create()
+{
+    CALL_STACK_MSG();
+    FileOutput::create();
+    if (ri == nullptr)
+        log_warning("RestartOutput works only with problems that support restart.");
+}
+
+void
+RestartOutput::output_step()
+{
+    CALL_STACK_MSG();
+    RestartFile file(get_file_name(), FileAccess::WRITE);
+    this->ri->write_restart_file(file);
+}
+
+std::string
+RestartOutput::get_file_ext() const
+{
+    return { "restart.h5" };
+}
+
+} // namespace godzilla
