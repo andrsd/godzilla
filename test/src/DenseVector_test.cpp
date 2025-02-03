64f2f7d7c9eb6945a92ccbae021034a6924cbf72
David Andrs <andrsd@gmail.com>
2025-02-02 18:13:50 -0700

    Using EXPECT_DOUBLE_EQ in DenseVector tests

--- test/src/DenseVector_test.cpp
+++ test/src/DenseVector_test.cpp
@@ -12,42 +12,42 @@ TEST(DenseVectorTest, ctor)
     for (Int i = 0; i < 3; i++)
         v(i) = 10. + (Real) i;
     for (Int i = 0; i < 3; i++)
-        EXPECT_EQ(v(i), 10 + i);
+        EXPECT_DOUBLE_EQ(v(i), 10 + i);
 }
 
 TEST(DenseVectorTest, ctor_std_vector)
 {
     DenseVector<Real, 3> v({ 2., 3., 4. });
-    EXPECT_EQ(v(0), 2.);
-    EXPECT_EQ(v(1), 3.);
-    EXPECT_EQ(v(2), 4.);
+    EXPECT_DOUBLE_EQ(v(0), 2.);
+    EXPECT_DOUBLE_EQ(v(1), 3.);
+    EXPECT_DOUBLE_EQ(v(2), 4.);
 }
 
 TEST(DenseVectorTest, zero)
 {
     DenseVector<Real, 3> a({ 1, 2, 3 });
     a.zero();
-    EXPECT_EQ(a(0), 0.);
-    EXPECT_EQ(a(1), 0.);
-    EXPECT_EQ(a(2), 0.);
+    EXPECT_DOUBLE_EQ(a(0), 0.);
+    EXPECT_DOUBLE_EQ(a(1), 0.);
+    EXPECT_DOUBLE_EQ(a(2), 0.);
 }
 
 TEST(DenseVectorTest, set_values)
 {
     DenseVector<Real, 3> a;
     a.set_values(2.);
-    EXPECT_EQ(a(0), 2.);
-    EXPECT_EQ(a(1), 2.);
-    EXPECT_EQ(a(2), 2.);
+    EXPECT_DOUBLE_EQ(a(0), 2.);
+    EXPECT_DOUBLE_EQ(a(1), 2.);
+    EXPECT_DOUBLE_EQ(a(2), 2.);
 }
 
 TEST(DenseVectorTest, scale)
 {
     DenseVector<Real, 3> a({ 2., 3., 4. });
     a.scale(3.);
-    EXPECT_EQ(a(0), 6.);
-    EXPECT_EQ(a(1), 9.);
-    EXPECT_EQ(a(2), 12.);
+    EXPECT_DOUBLE_EQ(a(0), 6.);
+    EXPECT_DOUBLE_EQ(a(1), 9.);
+    EXPECT_DOUBLE_EQ(a(2), 12.);
 }
 
 TEST(DenseVectorTest, add)
@@ -55,18 +55,18 @@ TEST(DenseVectorTest, add)
     DenseVector<Real, 3> a({ 2., 3., 4. });
     DenseVector<Real, 3> b({ 4., 2., -1. });
     a.add(b);
-    EXPECT_EQ(a(0), 6.);
-    EXPECT_EQ(a(1), 5.);
-    EXPECT_EQ(a(2), 3.);
+    EXPECT_DOUBLE_EQ(a(0), 6.);
+    EXPECT_DOUBLE_EQ(a(1), 5.);
+    EXPECT_DOUBLE_EQ(a(2), 3.);
 }
 
 TEST(DenseVectorTest, add_scalar)
 {
     DenseVector<Real, 3> a({ 2., 3., 4. });
     a.add(2);
-    EXPECT_EQ(a(0), 4.);
-    EXPECT_EQ(a(1), 5.);
-    EXPECT_EQ(a(2), 6.);
+    EXPECT_DOUBLE_EQ(a(0), 4.);
+    EXPECT_DOUBLE_EQ(a(1), 5.);
+    EXPECT_DOUBLE_EQ(a(2), 6.);
 }
 
 TEST(DenseVectorTest, op_add)
@@ -74,9 +74,9 @@ TEST(DenseVectorTest, op_add)
     DenseVector<Real, 3> a({ 2., 3., 4. });
     DenseVector<Real, 3> b({ 4., 2., -1. });
     DenseVector<Real, 3> c = a + b;
-    EXPECT_EQ(c(0), 6.);
-    EXPECT_EQ(c(1), 5.);
-    EXPECT_EQ(c(2), 3.);
+    EXPECT_DOUBLE_EQ(c(0), 6.);
+    EXPECT_DOUBLE_EQ(c(1), 5.);
+    EXPECT_DOUBLE_EQ(c(2), 3.);
 }
 
 TEST(DenseVectorTest, subtract)
@@ -84,9 +84,9 @@ TEST(DenseVectorTest, subtract)
     DenseVector<Real, 3> a({ 2., 3., 4. });
     DenseVector<Real, 3> b({ 4., 2., -1. });
     a.subtract(b);
-    EXPECT_EQ(a(0), -2.);
-    EXPECT_EQ(a(1), 1.);
-    EXPECT_EQ(a(2), 5.);
+    EXPECT_DOUBLE_EQ(a(0), -2.);
+    EXPECT_DOUBLE_EQ(a(1), 1.);
+    EXPECT_DOUBLE_EQ(a(2), 5.);
 }
 
 TEST(DenseVectorTest, op_subtract)
@@ -94,16 +94,16 @@ TEST(DenseVectorTest, op_subtract)
     DenseVector<Real, 3> a({ 2., 3., 4. });
     DenseVector<Real, 3> b({ 4., 2., -1. });
     DenseVector<Real, 3> c = a - b;
-    EXPECT_EQ(c(0), -2.);
-    EXPECT_EQ(c(1), 1.);
-    EXPECT_EQ(c(2), 5.);
+    EXPECT_DOUBLE_EQ(c(0), -2.);
+    EXPECT_DOUBLE_EQ(c(1), 1.);
+    EXPECT_DOUBLE_EQ(c(2), 5.);
 }
 
 TEST(DenseVectorTest, dot_vec_vec)
 {
     DenseVector<Real, 3> a({ 2., 3., 4. });
     DenseVector<Real, 3> b({ 4., 2., -1. });
-    EXPECT_EQ(dot(a, b), 10.);
+    EXPECT_DOUBLE_EQ(dot(a, b), 10.);
 }
 
 TEST(DenseVectorTest, dot_row_vec)
@@ -111,7 +111,7 @@ TEST(DenseVectorTest, dot_row_vec)
     DenseMatrix<Real, 1, 3> a;
     a.set_row(0, { 2., 3., 4. });
     DenseVector<Real, 3> b({ 4., 2., -1. });
-    EXPECT_EQ(dot(a, b), 10.);
+    EXPECT_DOUBLE_EQ(dot(a, b), 10.);
 }
 
 TEST(DenseVectorTest, dot_row_row)
@@ -120,7 +120,7 @@ TEST(DenseVectorTest, dot_row_row)
     a.set_row(0, { 2., 3., 4. });
     DenseMatrix<Real, 1, 3> b;
     b.set_row(0, { 4., 2., -1. });
-    EXPECT_EQ(dot(a, b), 10.);
+    EXPECT_DOUBLE_EQ(dot(a, b), 10.);
 }
 
 TEST(DenseVectorTest, dot_1x1)
@@ -129,28 +129,28 @@ TEST(DenseVectorTest, dot_1x1)
     a(0, 0) = 2.;
     DenseMatrix<Real, 1, 1> b;
     b(0, 0) = 3.;
-    EXPECT_EQ(dot(a, b), 6.);
+    EXPECT_DOUBLE_EQ(dot(a, b), 6.);
 }
 
 TEST(DenseVectorTest, sum)
 {
     DenseVector<Real, 3> b({ 4., 2., -1. });
     Real sum = b.sum();
-    EXPECT_EQ(sum, 5.);
+    EXPECT_DOUBLE_EQ(sum, 5.);
 }
 
 TEST(DenseVectorTest, magnitude)
 {
     DenseVector<Real, 2> v({ 3., 4. });
     Real mag = v.magnitude();
-    EXPECT_EQ(mag, 5.);
+    EXPECT_DOUBLE_EQ(mag, 5.);
 }
 
 TEST(DenseVectorTest, magnitude_vec)
 {
     DenseVector<Real, 2> v({ 3., 4. });
     Real mag = magnitude(v);
-    EXPECT_EQ(mag, 5.);
+    EXPECT_DOUBLE_EQ(mag, 5.);
 }
 
 TEST(DenseVectorTest, pointwise_mult_vec)
@@ -159,9 +159,9 @@ TEST(DenseVectorTest, pointwise_mult_vec)
     DenseVector<Real, N> a({ 2., 3., 4. });
     DenseVector<Real, N> b({ 4., 2., -1. });
     DenseVector<Real, N> res = pointwise_mult(a, b);
-    EXPECT_EQ(res(0), 8.);
-    EXPECT_EQ(res(1), 6.);
-    EXPECT_EQ(res(2), -4.);
+    EXPECT_DOUBLE_EQ(res(0), 8.);
+    EXPECT_DOUBLE_EQ(res(1), 6.);
+    EXPECT_DOUBLE_EQ(res(2), -4.);
 }
 
 TEST(DenseVectorTest, pointwise_mult_row)
@@ -172,9 +172,9 @@ TEST(DenseVectorTest, pointwise_mult_row)
     DenseMatrix<Real, 1, N> b;
     b.set_row(0, { 4., 2., -1. });
     DenseMatrix<Real, 1, N> res = pointwise_mult(a, b);
-    EXPECT_EQ(res(0, 0), 8.);
-    EXPECT_EQ(res(0, 1), 6.);
-    EXPECT_EQ(res(0, 2), -4.);
+    EXPECT_DOUBLE_EQ(res(0, 0), 8.);
+    EXPECT_DOUBLE_EQ(res(0, 1), 6.);
+    EXPECT_DOUBLE_EQ(res(0, 2), -4.);
 }
 
 TEST(DenseVectorTest, pointwise_div_col_col)
@@ -183,9 +183,9 @@ TEST(DenseVectorTest, pointwise_div_col_col)
     DenseVector<Real, N> a({ 2., 6., 4. });
     DenseVector<Real, N> b({ 4., 2., -1. });
     DenseVector<Real, N> res = pointwise_div<Real, N>(a, b);
-    EXPECT_EQ(res(0), 0.5);
-    EXPECT_EQ(res(1), 3.);
-    EXPECT_EQ(res(2), -4.);
+    EXPECT_DOUBLE_EQ(res(0), 0.5);
+    EXPECT_DOUBLE_EQ(res(1), 3.);
+    EXPECT_DOUBLE_EQ(res(2), -4.);
 }
 
 TEST(DenseVectorTest, pointwise_div_row_row)
@@ -196,27 +196,27 @@ TEST(DenseVectorTest, pointwise_div_row_row)
     DenseMatrix<Real, 1, N> b;
     b.set_row(0, { 4., 2., -1. });
     auto res = pointwise_div(a, b);
-    EXPECT_EQ(res(0, 0), 0.5);
-    EXPECT_EQ(res(0, 1), 3.);
-    EXPECT_EQ(res(0, 2), -4.);
+    EXPECT_DOUBLE_EQ(res(0, 0), 0.5);
+    EXPECT_DOUBLE_EQ(res(0, 1), 3.);
+    EXPECT_DOUBLE_EQ(res(0, 2), -4.);
 }
 
 TEST(DenseVectorTest, op_mult_scalar)
 {
     DenseVector<Real, 3> a({ 2., 3., 4. });
     DenseVector<Real, 3> b = 3. * a;
-    EXPECT_EQ(b(0), 6.);
-    EXPECT_EQ(b(1), 9.);
-    EXPECT_EQ(b(2), 12.);
+    EXPECT_DOUBLE_EQ(b(0), 6.);
+    EXPECT_DOUBLE_EQ(b(1), 9.);
+    EXPECT_DOUBLE_EQ(b(2), 12.);
 }
 
 TEST(DenseVectorTest, op_mult_scalar_post)
 {
     DenseVector<Real, 3> a({ 2., 3., 4. });
     DenseVector<Real, 3> b = a * 3.;
-    EXPECT_EQ(b(0), 6.);
-    EXPECT_EQ(b(1), 9.);
-    EXPECT_EQ(b(2), 12.);
+    EXPECT_DOUBLE_EQ(b(0), 6.);
+    EXPECT_DOUBLE_EQ(b(1), 9.);
+    EXPECT_DOUBLE_EQ(b(2), 12.);
 }
 
 TEST(DenseVectorTest, op_mult_row_mat)
@@ -255,9 +255,9 @@ TEST(DenseVectorTest, cross_prod_3)
     DenseVector<Real, 3> a({ -2., 5, 1. });
     DenseVector<Real, 3> b({ 3, 1, 2 });
     auto v = cross_product(a, b);
-    EXPECT_EQ(v(0), 9.);
-    EXPECT_EQ(v(1), 7.);
-    EXPECT_EQ(v(2), -17.);
+    EXPECT_DOUBLE_EQ(v(0), 9.);
+    EXPECT_DOUBLE_EQ(v(1), 7.);
+    EXPECT_DOUBLE_EQ(v(2), -17.);
 }
 
 TEST(DenseVectorTest, mat_row)
@@ -291,7 +291,7 @@ TEST(DenseVectorTest, mat_col)
 TEST(DenseVectorTest, avg)
 {
     DenseVector<Real, 3> a({ -2., 5, 3. });
-    EXPECT_EQ(a.avg(), 2.);
+    EXPECT_DOUBLE_EQ(a.avg(), 2.);
 }
 
 TEST(DenseVectorTest, op_inc)
@@ -299,18 +299,18 @@ TEST(DenseVectorTest, op_inc)
     DenseVector<Real, 3> a({ -2., 5, 3. });
     DenseVector<Real, 3> b({ 1., -1, 2. });
     a += b;
-    EXPECT_EQ(a(0), -1.);
-    EXPECT_EQ(a(1), 4.);
-    EXPECT_EQ(a(2), 5.);
+    EXPECT_DOUBLE_EQ(a(0), -1.);
+    EXPECT_DOUBLE_EQ(a(1), 4.);
+    EXPECT_DOUBLE_EQ(a(2), 5.);
 }
 
 TEST(DenseVectorTest, op_inc_scalar)
 {
     DenseVector<Real, 3> a({ -2., 5, 3. });
     a += 2.;
-    EXPECT_EQ(a(0), 0.);
-    EXPECT_EQ(a(1), 7.);
-    EXPECT_EQ(a(2), 5.);
+    EXPECT_DOUBLE_EQ(a(0), 0.);
+    EXPECT_DOUBLE_EQ(a(1), 7.);
+    EXPECT_DOUBLE_EQ(a(2), 5.);
 }
 
 TEST(DenseVectorTest, op_dec)
@@ -318,55 +318,55 @@ TEST(DenseVectorTest, op_dec)
     DenseVector<Real, 3> a({ -2., 5, 3. });
     DenseVector<Real, 3> b({ 1., -1, 2. });
     a -= b;
-    EXPECT_EQ(a(0), -3.);
-    EXPECT_EQ(a(1), 6.);
-    EXPECT_EQ(a(2), 1.);
+    EXPECT_DOUBLE_EQ(a(0), -3.);
+    EXPECT_DOUBLE_EQ(a(1), 6.);
+    EXPECT_DOUBLE_EQ(a(2), 1.);
 }
 
 TEST(DenseVectorTest, op_unary_minus)
 {
     DenseVector<Real, 3> a({ -2., 5, 3. });
     auto b = -a;
-    EXPECT_EQ(b(0), 2.);
-    EXPECT_EQ(b(1), -5.);
-    EXPECT_EQ(b(2), -3.);
+    EXPECT_DOUBLE_EQ(b(0), 2.);
+    EXPECT_DOUBLE_EQ(b(1), -5.);
+    EXPECT_DOUBLE_EQ(b(2), -3.);
 }
 
 TEST(DenseVectorTest, min)
 {
     DenseVector<Real, 3> a({ 5, -2, 10 });
-    EXPECT_EQ(a.min(), -2);
+    EXPECT_DOUBLE_EQ(a.min(), -2);
 }
 
 TEST(DenseVectorTest, max)
 {
     DenseVector<Real, 3> a({ 5, -2, 10 });
-    EXPECT_EQ(a.max(), 10);
+    EXPECT_DOUBLE_EQ(a.max(), 10);
 }
 
 TEST(DenseVectorTest, abs)
 {
     DenseVector<Real, 3> v({ -5, 2, -10 });
     v.abs();
-    EXPECT_EQ(v(0), 5.);
-    EXPECT_EQ(v(1), 2.);
-    EXPECT_EQ(v(2), 10.);
+    EXPECT_DOUBLE_EQ(v(0), 5.);
+    EXPECT_DOUBLE_EQ(v(1), 2.);
+    EXPECT_DOUBLE_EQ(v(2), 10.);
 }
 
 TEST(DenseVectorTest, normalize)
 {
     DenseVector<Real, 2> v({ 3., 4. });
     v.normalize();
-    EXPECT_EQ(v(0), 3. / 5.);
-    EXPECT_EQ(v(1), 4. / 5.);
+    EXPECT_DOUBLE_EQ(v(0), 3. / 5.);
+    EXPECT_DOUBLE_EQ(v(1), 4. / 5.);
 }
 
 TEST(DenseVectorTest, normalized)
 {
     DenseVector<Real, 2> v({ 3., 4. });
     auto n = v.normalized();
-    EXPECT_EQ(n(0), 3. / 5.);
-    EXPECT_EQ(n(1), 4. / 5.);
+    EXPECT_DOUBLE_EQ(n(0), 3. / 5.);
+    EXPECT_DOUBLE_EQ(n(1), 4. / 5.);
 }
 
 TEST(DenseVectorTest, out)
@@ -401,33 +401,33 @@ TEST(DenseVectorTest, tensor_product)
 TEST(DynDenseVectorTest, ctor_empty)
 {
     DynDenseVector<Real> v;
-    EXPECT_EQ(v.size(), 0);
+    EXPECT_DOUBLE_EQ(v.size(), 0);
 }
 
 TEST(DynDenseVectorTest, ctor)
 {
     DynDenseVector<Real> v(3);
-    EXPECT_EQ(v.size(), 3);
+    EXPECT_DOUBLE_EQ(v.size(), 3);
     for (Int i = 0; i < 3; i++)
         v(i) = 10. + (Real) i;
     for (Int i = 0; i < 3; i++)
-        EXPECT_EQ(v(i), 10 + i);
+        EXPECT_DOUBLE_EQ(v(i), 10 + i);
 }
 
 TEST(DynDenseVectorTest, ctor_init)
 {
     DynDenseVector<Real> v(3, 123.);
     for (Int i = 0; i < 3; i++)
-        EXPECT_EQ(v(i), 123.);
+        EXPECT_DOUBLE_EQ(v(i), 123.);
 }
 
 TEST(DynDenseVectorTest, ctor_std_vector)
 {
     DynDenseVector<Real> v(3);
     v.set_values({ 2., 3., 4. });
-    EXPECT_EQ(v(0), 2.);
-    EXPECT_EQ(v(1), 3.);
-    EXPECT_EQ(v(2), 4.);
+    EXPECT_DOUBLE_EQ(v(0), 2.);
+    EXPECT_DOUBLE_EQ(v(1), 3.);
+    EXPECT_DOUBLE_EQ(v(2), 4.);
 }
 
 TEST(DynDenseVectorTest, zero)
@@ -435,18 +435,18 @@ TEST(DynDenseVectorTest, zero)
     DynDenseVector<Real> a(3);
     a.set_values({ 1, 2, 3 });
     a.zero();
-    EXPECT_EQ(a(0), 0.);
-    EXPECT_EQ(a(1), 0.);
-    EXPECT_EQ(a(2), 0.);
+    EXPECT_DOUBLE_EQ(a(0), 0.);
+    EXPECT_DOUBLE_EQ(a(1), 0.);
+    EXPECT_DOUBLE_EQ(a(2), 0.);
 }
 
 TEST(DynDenseVectorTest, set_values)
 {
     DynDenseVector<Real> a(3);
     a.set_values(2.);
-    EXPECT_EQ(a(0), 2.);
-    EXPECT_EQ(a(1), 2.);
-    EXPECT_EQ(a(2), 2.);
+    EXPECT_DOUBLE_EQ(a(0), 2.);
+    EXPECT_DOUBLE_EQ(a(1), 2.);
+    EXPECT_DOUBLE_EQ(a(2), 2.);
 }
 
 TEST(DynDenseVectorTest, scale)
@@ -454,9 +454,9 @@ TEST(DynDenseVectorTest, scale)
     DynDenseVector<Real> a(3);
     a.set_values({ 2., 3., 4. });
     a.scale(3.);
-    EXPECT_EQ(a(0), 6.);
-    EXPECT_EQ(a(1), 9.);
-    EXPECT_EQ(a(2), 12.);
+    EXPECT_DOUBLE_EQ(a(0), 6.);
+    EXPECT_DOUBLE_EQ(a(1), 9.);
+    EXPECT_DOUBLE_EQ(a(2), 12.);
 }
 
 TEST(DynDenseVectorTest, add)
@@ -466,9 +466,9 @@ TEST(DynDenseVectorTest, add)
     DynDenseVector<Real> b(3);
     b.set_values({ 4., 2., -1. });
     a.add(b);
-    EXPECT_EQ(a(0), 6.);
-    EXPECT_EQ(a(1), 5.);
-    EXPECT_EQ(a(2), 3.);
+    EXPECT_DOUBLE_EQ(a(0), 6.);
+    EXPECT_DOUBLE_EQ(a(1), 5.);
+    EXPECT_DOUBLE_EQ(a(2), 3.);
 }
 
 TEST(DynDenseVectorTest, add_scalar)
@@ -476,9 +476,9 @@ TEST(DynDenseVectorTest, add_scalar)
     DynDenseVector<Real> a(3);
     a.set_values({ 2., 3., 4. });
     a.add(2);
-    EXPECT_EQ(a(0), 4.);
-    EXPECT_EQ(a(1), 5.);
-    EXPECT_EQ(a(2), 6.);
+    EXPECT_DOUBLE_EQ(a(0), 4.);
+    EXPECT_DOUBLE_EQ(a(1), 5.);
+    EXPECT_DOUBLE_EQ(a(2), 6.);
 }
 
 TEST(DynDenseVectorTest, op_add)
@@ -488,9 +488,9 @@ TEST(DynDenseVectorTest, op_add)
     DynDenseVector<Real> b(3);
     b.set_values({ 4., 2., -1. });
     DynDenseVector<Real> c = a + b;
-    EXPECT_EQ(c(0), 6.);
-    EXPECT_EQ(c(1), 5.);
-    EXPECT_EQ(c(2), 3.);
+    EXPECT_DOUBLE_EQ(c(0), 6.);
+    EXPECT_DOUBLE_EQ(c(1), 5.);
+    EXPECT_DOUBLE_EQ(c(2), 3.);
 }
 
 TEST(DynDenseVectorTest, subtract)
@@ -500,9 +500,9 @@ TEST(DynDenseVectorTest, subtract)
     DynDenseVector<Real> b(3);
     b.set_values({ 4., 2., -1. });
     a.subtract(b);
-    EXPECT_EQ(a(0), -2.);
-    EXPECT_EQ(a(1), 1.);
-    EXPECT_EQ(a(2), 5.);
+    EXPECT_DOUBLE_EQ(a(0), -2.);
+    EXPECT_DOUBLE_EQ(a(1), 1.);
+    EXPECT_DOUBLE_EQ(a(2), 5.);
 }
 
 TEST(DynDenseVectorTest, op_subtract)
@@ -512,9 +512,9 @@ TEST(DynDenseVectorTest, op_subtract)
     DynDenseVector<Real> b(3);
     b.set_values({ 4., 2., -1. });
     DynDenseVector<Real> c = a - b;
-    EXPECT_EQ(c(0), -2.);
-    EXPECT_EQ(c(1), 1.);
-    EXPECT_EQ(c(2), 5.);
+    EXPECT_DOUBLE_EQ(c(0), -2.);
+    EXPECT_DOUBLE_EQ(c(1), 1.);
+    EXPECT_DOUBLE_EQ(c(2), 5.);
 }
 
 TEST(DynDenseVectorTest, dot_vec_vec)
@@ -523,7 +523,7 @@ TEST(DynDenseVectorTest, dot_vec_vec)
     a.set_values({ 2., 3., 4. });
     DynDenseVector<Real> b(3);
     b.set_values({ 4., 2., -1. });
-    EXPECT_EQ(dot(a, b), 10.);
+    EXPECT_DOUBLE_EQ(dot(a, b), 10.);
 }
 
 TEST(DynDenseVectorTest, sum)
@@ -531,7 +531,7 @@ TEST(DynDenseVectorTest, sum)
     DynDenseVector<Real> b(3);
     b.set_values({ 4., 2., -1. });
     Real sum = b.sum();
-    EXPECT_EQ(sum, 5.);
+    EXPECT_DOUBLE_EQ(sum, 5.);
 }
 
 TEST(DynDenseVectorTest, magnitude)
@@ -539,7 +539,7 @@ TEST(DynDenseVectorTest, magnitude)
     DynDenseVector<Real> v(2);
     v.set_values({ 3., 4. });
     Real mag = v.magnitude();
-    EXPECT_EQ(mag, 5.);
+    EXPECT_DOUBLE_EQ(mag, 5.);
 }
 
 TEST(DynDenseVectorTest, pointwise_mult)
@@ -549,9 +549,9 @@ TEST(DynDenseVectorTest, pointwise_mult)
     DynDenseVector<Real> b(3);
     b.set_values({ 4., 2., -1. });
     DynDenseVector<Real> res = pointwise_mult(a, b);
-    EXPECT_EQ(res(0), 8.);
-    EXPECT_EQ(res(1), 6.);
-    EXPECT_EQ(res(2), -4.);
+    EXPECT_DOUBLE_EQ(res(0), 8.);
+    EXPECT_DOUBLE_EQ(res(1), 6.);
+    EXPECT_DOUBLE_EQ(res(2), -4.);
 }
 
 TEST(DynDenseVectorTest, pointwise_div)
@@ -561,9 +561,9 @@ TEST(DynDenseVectorTest, pointwise_div)
     DynDenseVector<Real> b(3);
     b.set_values({ 4., 2., -1. });
     DynDenseVector<Real> res = pointwise_div(a, b);
-    EXPECT_EQ(res(0), 0.5);
-    EXPECT_EQ(res(1), 3.);
-    EXPECT_EQ(res(2), -4.);
+    EXPECT_DOUBLE_EQ(res(0), 0.5);
+    EXPECT_DOUBLE_EQ(res(1), 3.);
+    EXPECT_DOUBLE_EQ(res(2), -4.);
 }
 
 TEST(DynDenseVectorTest, op_mult_scalar)
@@ -571,9 +571,9 @@ TEST(DynDenseVectorTest, op_mult_scalar)
     DynDenseVector<Real> a(3);
     a.set_values({ 2., 3., 4. });
     DynDenseVector<Real> b = 3. * a;
-    EXPECT_EQ(b(0), 6.);
-    EXPECT_EQ(b(1), 9.);
-    EXPECT_EQ(b(2), 12.);
+    EXPECT_DOUBLE_EQ(b(0), 6.);
+    EXPECT_DOUBLE_EQ(b(1), 9.);
+    EXPECT_DOUBLE_EQ(b(2), 12.);
 }
 
 TEST(DynDenseVectorTest, op_mult_scalar_post)
@@ -581,9 +581,9 @@ TEST(DynDenseVectorTest, op_mult_scalar_post)
     DynDenseVector<Real> a(3);
     a.set_values({ 2., 3., 4. });
     DynDenseVector<Real> b = a * 3.;
-    EXPECT_EQ(b(0), 6.);
-    EXPECT_EQ(b(1), 9.);
-    EXPECT_EQ(b(2), 12.);
+    EXPECT_DOUBLE_EQ(b(0), 6.);
+    EXPECT_DOUBLE_EQ(b(1), 9.);
+    EXPECT_DOUBLE_EQ(b(2), 12.);
 }
 
 TEST(DynDenseVectorTest, cross_prod_3)
@@ -593,16 +593,16 @@ TEST(DynDenseVectorTest, cross_prod_3)
     DynDenseVector<Real> b(3);
     b.set_values({ 3, 1, 2 });
     auto v = cross_product(a, b);
-    EXPECT_EQ(v(0), 9.);
-    EXPECT_EQ(v(1), 7.);
-    EXPECT_EQ(v(2), -17.);
+    EXPECT_DOUBLE_EQ(v(0), 9.);
+    EXPECT_DOUBLE_EQ(v(1), 7.);
+    EXPECT_DOUBLE_EQ(v(2), -17.);
 }
 
 TEST(DynDenseVectorTest, avg)
 {
     DynDenseVector<Real> a(3);
     a.set_values({ -2., 5, 3. });
-    EXPECT_EQ(a.avg(), 2.);
+    EXPECT_DOUBLE_EQ(a.avg(), 2.);
 }
 
 TEST(DynDenseVectorTest, op_inc)
@@ -612,9 +612,9 @@ TEST(DynDenseVectorTest, op_inc)
     DynDenseVector<Real> b(3);
     b.set_values({ 1., -1, 2. });
     a += b;
-    EXPECT_EQ(a(0), -1.);
-    EXPECT_EQ(a(1), 4.);
-    EXPECT_EQ(a(2), 5.);
+    EXPECT_DOUBLE_EQ(a(0), -1.);
+    EXPECT_DOUBLE_EQ(a(1), 4.);
+    EXPECT_DOUBLE_EQ(a(2), 5.);
 }
 
 TEST(DynDenseVectorTest, op_inc_scalar)
@@ -622,9 +622,9 @@ TEST(DynDenseVectorTest, op_inc_scalar)
     DynDenseVector<Real> a(3);
     a.set_values({ -2., 5, 3. });
     a += 2.;
-    EXPECT_EQ(a(0), 0.);
-    EXPECT_EQ(a(1), 7.);
-    EXPECT_EQ(a(2), 5.);
+    EXPECT_DOUBLE_EQ(a(0), 0.);
+    EXPECT_DOUBLE_EQ(a(1), 7.);
+    EXPECT_DOUBLE_EQ(a(2), 5.);
 }
 
 TEST(DynDenseVectorTest, op_dec)
@@ -634,9 +634,9 @@ TEST(DynDenseVectorTest, op_dec)
     DynDenseVector<Real> b(3);
     b.set_values({ 1., -1, 2. });
     a -= b;
-    EXPECT_EQ(a(0), -3.);
-    EXPECT_EQ(a(1), 6.);
-    EXPECT_EQ(a(2), 1.);
+    EXPECT_DOUBLE_EQ(a(0), -3.);
+    EXPECT_DOUBLE_EQ(a(1), 6.);
+    EXPECT_DOUBLE_EQ(a(2), 1.);
 }
 
 TEST(DynDenseVectorTest, op_unary_minus)
@@ -644,23 +644,23 @@ TEST(DynDenseVectorTest, op_unary_minus)
     DynDenseVector<Real> a(3);
     a.set_values({ -2., 5, 3. });
     auto b = -a;
-    EXPECT_EQ(b(0), 2.);
-    EXPECT_EQ(b(1), -5.);
-    EXPECT_EQ(b(2), -3.);
+    EXPECT_DOUBLE_EQ(b(0), 2.);
+    EXPECT_DOUBLE_EQ(b(1), -5.);
+    EXPECT_DOUBLE_EQ(b(2), -3.);
 }
 
 TEST(DynDenseVectorTest, min)
 {
     DynDenseVector<Real> a(3);
     a.set_values({ 5, -2, 10 });
-    EXPECT_EQ(a.min(), -2);
+    EXPECT_DOUBLE_EQ(a.min(), -2);
 }
 
 TEST(DynDenseVectorTest, max)
 {
     DynDenseVector<Real> a(3);
     a.set_values({ 5, -2, 10 });
-    EXPECT_EQ(a.max(), 10);
+    EXPECT_DOUBLE_EQ(a.max(), 10);
 }
 
 TEST(DynDenseVectorTest, abs)
@@ -668,9 +668,9 @@ TEST(DynDenseVectorTest, abs)
     DynDenseVector<Real> v(3);
     v.set_values({ -5, 2, -10 });
     v.abs();
-    EXPECT_EQ(v(0), 5.);
-    EXPECT_EQ(v(1), 2.);
-    EXPECT_EQ(v(2), 10.);
+    EXPECT_DOUBLE_EQ(v(0), 5.);
+    EXPECT_DOUBLE_EQ(v(1), 2.);
+    EXPECT_DOUBLE_EQ(v(2), 10.);
 }
 
 TEST(DynDenseVectorTest, normalize)
@@ -678,8 +678,8 @@ TEST(DynDenseVectorTest, normalize)
     DynDenseVector<Real> v(2);
     v.set_values({ 3., 4. });
     v.normalize();
-    EXPECT_EQ(v(0), 3. / 5.);
-    EXPECT_EQ(v(1), 4. / 5.);
+    EXPECT_DOUBLE_EQ(v(0), 3. / 5.);
+    EXPECT_DOUBLE_EQ(v(1), 4. / 5.);
 }
 
 TEST(DynDenseVectorTest, normalized)
@@ -687,8 +687,8 @@ TEST(DynDenseVectorTest, normalized)
     DynDenseVector<Real> v(2);
     v.set_values({ 3., 4. });
     auto n = v.normalized();
-    EXPECT_EQ(n(0), 3. / 5.);
-    EXPECT_EQ(n(1), 4. / 5.);
+    EXPECT_DOUBLE_EQ(n(0), 3. / 5.);
+    EXPECT_DOUBLE_EQ(n(1), 4. / 5.);
 }
 
 TEST(DynDenseVectorTest, resize)
@@ -696,7 +696,7 @@ TEST(DynDenseVectorTest, resize)
     DynDenseVector<Real> v(2);
     v.set_values({ 3., 4. });
     v.resize(3);
-    EXPECT_EQ(v.size(), 3);
+    EXPECT_DOUBLE_EQ(v.size(), 3);
 }
 
 TEST(DynDenseVectorTest, out)
