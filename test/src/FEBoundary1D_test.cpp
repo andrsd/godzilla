8f78b0216aa1c7cfd610aabc78d99f29e0d599df
David Andrs <andrsd@gmail.com>
2025-09-06 16:03:31 -0600

    Updating FEBoundary tests
    - using `EssentialBoundaryInfo` and `EssentialBoundaryInfo` - adding
    tests for `for_each_`

--- test/src/FEBoundary1D_test.cpp
+++ test/src/FEBoundary1D_test.cpp
@@ -1,6 +1,7 @@
 #include "gmock/gmock.h"
 #include "TestApp.h"
 #include "TestMesh1D.h"
+#include "godzilla/Array1D.h"
 #include "godzilla/MeshObject.h"
 #include "godzilla/FEGeometry.h"
 #include "godzilla/FEVolumes.h"
@@ -12,17 +13,10 @@ using namespace testing;
 
 namespace {
 
-class TestBoundary1D : public fe::BoundaryInfo<EDGE2, 1, 2> {
+class TestEssentialBoundary1D : public fe::EssentialBoundaryInfo<EDGE2, 1, 2> {
 public:
-    TestBoundary1D(UnstructuredMesh * mesh,
-                   const Array1D<DenseMatrix<Real, 1, 2>> * grad_phi,
-                   const IndexSet & facets) :
-        fe::BoundaryInfo<EDGE2, 1, 2>(mesh, grad_phi, facets)
-    {
-    }
-
-    TestBoundary1D(UnstructuredMesh * mesh, const IndexSet & facets) :
-        fe::BoundaryInfo<EDGE2, 1, 2>(mesh, facets)
+    TestEssentialBoundary1D(UnstructuredMesh * mesh, const IndexSet & vertices) :
+        fe::EssentialBoundaryInfo<EDGE2, 1, 2>(mesh, vertices)
     {
     }
 
@@ -30,22 +24,69 @@ public:
     create() override
     {
         CALL_STACK_MSG();
-        compute_face_normals();
-        create_vertex_index_set();
-        compute_nodal_normals();
+        EssentialBoundaryInfo::create();
+        this->vals.create(this->num_vertices());
     }
 
     void
     destroy() override
     {
         CALL_STACK_MSG();
-        free();
+        this->vals.destroy();
+        EssentialBoundaryInfo::destroy();
     }
+
+    void
+    compute()
+    {
+        for_each_vertex([&](Int idx, Int vtx) { this->vals(idx) = vtx; });
+    }
+
+    Array1D<Int> vals;
+};
+
+class TestNaturalBoundary1D : public fe::NaturalBoundaryInfo<EDGE2, 1, 2> {
+public:
+    TestNaturalBoundary1D(UnstructuredMesh * mesh,
+                          const Array1D<DenseMatrix<Real, 1, 2>> * grad_phi,
+                          const IndexSet & facets) :
+        fe::NaturalBoundaryInfo<EDGE2, 1, 2>(mesh, grad_phi, facets)
+    {
+    }
+
+    TestNaturalBoundary1D(UnstructuredMesh * mesh, const IndexSet & facets) :
+        fe::NaturalBoundaryInfo<EDGE2, 1, 2>(mesh, facets)
+    {
+    }
+
+    void
+    create() override
+    {
+        CALL_STACK_MSG();
+        NaturalBoundaryInfo::create();
+        this->vals.create(this->num_facets());
+    }
+
+    void
+    destroy() override
+    {
+        CALL_STACK_MSG();
+        this->vals.destroy();
+        NaturalBoundaryInfo::destroy();
+    }
+
+    void
+    compute()
+    {
+        for_each_facet([&](Int idx, Int facet) { this->vals(idx) = facet; });
+    }
+
+    Array1D<Int> vals;
 };
 
 } // namespace
 
-TEST(FEBoundaryTest, normals_1d)
+TEST(FEBoundaryTest, test_1d)
 {
     TestApp app;
 
@@ -59,22 +100,23 @@ TEST(FEBoundaryTest, normals_1d)
     auto grad_phi = fe::calc_grad_shape<EDGE2, 1>(*m, fe_volume);
 
     {
-        IndexSet bnd_facets = points_from_label(m->get_label("left"));
-        TestBoundary1D bnd(m, &grad_phi, bnd_facets);
+        auto bnd_facets = points_from_label(m->get_label("left"));
+        auto vertices = m->get_cone_recursive_vertices(bnd_facets);
+        vertices.sort_remove_dups();
+
+        TestEssentialBoundary1D bnd(m, vertices);
         bnd.create();
-        EXPECT_DOUBLE_EQ(bnd.normal(0)(0), -1);
-        EXPECT_DOUBLE_EQ(bnd.nodal_normal(0)(0), -1);
-        EXPECT_DOUBLE_EQ(bnd.length(0), 1.);
+        bnd.compute();
+        EXPECT_EQ(bnd.vals(0), 2);
         bnd.destroy();
     }
 
     {
         IndexSet bnd_facets = points_from_label(m->get_label("right"));
-        TestBoundary1D bnd(m, bnd_facets);
+        TestNaturalBoundary1D bnd(m, bnd_facets);
         bnd.create();
+        bnd.compute();
         EXPECT_DOUBLE_EQ(bnd.normal(0)(0), 1);
-        EXPECT_DOUBLE_EQ(bnd.nodal_normal(0)(0), 1);
-        EXPECT_DOUBLE_EQ(bnd.length(0), 1.);
         bnd.destroy();
     }
 
--- test/src/FEBoundary3D_test.cpp
+++ test/src/FEBoundary3D_test.cpp
@@ -14,17 +14,10 @@ using namespace testing;
 
 namespace {
 
-class TestBoundary3D : public fe::BoundaryInfo<TET4, 3, 4> {
+class TestEssentialBoundary3D : public fe::EssentialBoundaryInfo<TET4, 3, 4> {
 public:
-    TestBoundary3D(UnstructuredMesh * mesh,
-                   const Array1D<DenseMatrix<Real, 3, 4>> * grad_phi,
-                   const IndexSet & facets) :
-        fe::BoundaryInfo<TET4, 3, 4>(mesh, grad_phi, facets)
-    {
-    }
-
-    TestBoundary3D(UnstructuredMesh * mesh, const IndexSet & facets) :
-        fe::BoundaryInfo<TET4, 3, 4>(mesh, facets)
+    TestEssentialBoundary3D(UnstructuredMesh * mesh, const IndexSet & facets) :
+        fe::EssentialBoundaryInfo<TET4, 3, 4>(mesh, facets)
     {
     }
 
@@ -32,22 +25,64 @@ public:
     create() override
     {
         CALL_STACK_MSG();
-        compute_face_normals();
-        create_vertex_index_set();
-        compute_nodal_normals();
+        EssentialBoundaryInfo::create();
+        this->vals.create(this->num_vertices());
     }
 
     void
     destroy() override
     {
         CALL_STACK_MSG();
-        free();
+        this->vals.destroy();
+        EssentialBoundaryInfo::destroy();
     }
+
+    void
+    compute()
+    {
+        for_each_vertex([&](Int idx, Int vtx) { this->vals(idx) = vtx; });
+    }
+
+    Array1D<Int> vals;
+};
+
+class TestNaturalBoundary3D : public fe::NaturalBoundaryInfo<TET4, 3, 4> {
+public:
+    TestNaturalBoundary3D(UnstructuredMesh * mesh,
+                          const Array1D<DenseMatrix<Real, 3, 4>> * grad_phi,
+                          const IndexSet & facets) :
+        fe::NaturalBoundaryInfo<TET4, 3, 4>(mesh, grad_phi, facets)
+    {
+    }
+
+    void
+    create() override
+    {
+        CALL_STACK_MSG();
+        NaturalBoundaryInfo::create();
+        this->vals.create(this->num_facets());
+    }
+
+    void
+    destroy() override
+    {
+        CALL_STACK_MSG();
+        NaturalBoundaryInfo::destroy();
+        this->vals.destroy();
+    }
+
+    void
+    compute()
+    {
+        for_each_facet([&](Int idx, Int facet) { this->vals(idx) = facet; });
+    }
+
+    Array1D<Int> vals;
 };
 
 } // namespace
 
-TEST(FEBoundaryTest, normals_3d)
+TEST(FEBoundaryTest, test_3d)
 {
     TestApp app;
 
@@ -64,89 +99,74 @@ TEST(FEBoundaryTest, normals_3d)
     {
         auto label = m->get_label("left");
         auto bnd_facets = points_from_label(label);
-        TestBoundary3D bnd(m, &grad_phi, bnd_facets);
+        auto vertices = m->get_cone_recursive_vertices(bnd_facets);
+        vertices.sort_remove_dups();
+
+        TestEssentialBoundary3D bnd(m, vertices);
         bnd.create();
-        EXPECT_DOUBLE_EQ(bnd.normal(0)(0), -1);
-        EXPECT_DOUBLE_EQ(bnd.normal(0)(1), 0);
-        EXPECT_DOUBLE_EQ(bnd.normal(0)(2), 0);
+        bnd.compute();
 
-        EXPECT_DOUBLE_EQ(bnd.nodal_normal(0)(0), -1);
-        EXPECT_DOUBLE_EQ(bnd.nodal_normal(0)(1), 0);
-        EXPECT_DOUBLE_EQ(bnd.nodal_normal(0)(2), 0);
-        EXPECT_DOUBLE_EQ(bnd.nodal_normal(1)(0), -1);
-        EXPECT_DOUBLE_EQ(bnd.nodal_normal(1)(1), 0);
-        EXPECT_DOUBLE_EQ(bnd.nodal_normal(1)(2), 0);
-        EXPECT_DOUBLE_EQ(bnd.nodal_normal(2)(0), -1);
-        EXPECT_DOUBLE_EQ(bnd.nodal_normal(2)(1), 0);
-        EXPECT_DOUBLE_EQ(bnd.nodal_normal(2)(2), 0);
+        EXPECT_EQ(bnd.num_vertices(), 3);
 
-        EXPECT_DOUBLE_EQ(bnd.length(0), 0.5);
+        EXPECT_EQ(bnd.vals(0), 1);
+
         bnd.destroy();
     }
     {
         auto label = m->get_label("front");
         auto bnd_facets = points_from_label(label);
-        TestBoundary3D bnd(m, &grad_phi, bnd_facets);
+        TestNaturalBoundary3D bnd(m, &grad_phi, bnd_facets);
         bnd.create();
+        bnd.compute();
+
+        EXPECT_EQ(bnd.num_facets(), 1);
+
         EXPECT_DOUBLE_EQ(bnd.normal(0)(0), 0);
         EXPECT_DOUBLE_EQ(bnd.normal(0)(1), -1);
         EXPECT_DOUBLE_EQ(bnd.normal(0)(2), 0);
 
-        EXPECT_DOUBLE_EQ(bnd.nodal_normal(0)(0), 0);
-        EXPECT_DOUBLE_EQ(bnd.nodal_normal(0)(1), -1);
-        EXPECT_DOUBLE_EQ(bnd.nodal_normal(0)(2), 0);
-        EXPECT_DOUBLE_EQ(bnd.nodal_normal(1)(0), 0);
-        EXPECT_DOUBLE_EQ(bnd.nodal_normal(1)(1), -1);
-        EXPECT_DOUBLE_EQ(bnd.nodal_normal(1)(2), 0);
-        EXPECT_DOUBLE_EQ(bnd.nodal_normal(2)(0), 0);
-        EXPECT_DOUBLE_EQ(bnd.nodal_normal(2)(1), -1);
-        EXPECT_DOUBLE_EQ(bnd.nodal_normal(2)(2), 0);
+        EXPECT_DOUBLE_EQ(bnd.facet_area(0), 0.5);
 
-        EXPECT_DOUBLE_EQ(bnd.area(0), 0.5);
+        EXPECT_EQ(bnd.vals(0), 6);
+
         bnd.destroy();
     }
     {
         auto label = m->get_label("bottom");
         auto bnd_facets = points_from_label(label);
-        TestBoundary3D bnd(m, &grad_phi, bnd_facets);
+        TestNaturalBoundary3D bnd(m, &grad_phi, bnd_facets);
         bnd.create();
+        bnd.compute();
+
+        EXPECT_EQ(bnd.num_facets(), 1);
+
         EXPECT_DOUBLE_EQ(bnd.normal(0)(0), 0);
         EXPECT_DOUBLE_EQ(bnd.normal(0)(1), 0);
         EXPECT_DOUBLE_EQ(bnd.normal(0)(2), -1);
 
-        EXPECT_DOUBLE_EQ(bnd.nodal_normal(0)(0), 0);
-        EXPECT_DOUBLE_EQ(bnd.nodal_normal(0)(1), 0);
-        EXPECT_DOUBLE_EQ(bnd.nodal_normal(0)(2), -1);
-        EXPECT_DOUBLE_EQ(bnd.nodal_normal(1)(0), 0);
-        EXPECT_DOUBLE_EQ(bnd.nodal_normal(1)(1), 0);
-        EXPECT_DOUBLE_EQ(bnd.nodal_normal(1)(2), -1);
-        EXPECT_DOUBLE_EQ(bnd.nodal_normal(2)(0), 0);
-        EXPECT_DOUBLE_EQ(bnd.nodal_normal(2)(1), 0);
-        EXPECT_DOUBLE_EQ(bnd.nodal_normal(2)(2), -1);
+        EXPECT_DOUBLE_EQ(bnd.facet_area(0), 0.5);
 
-        EXPECT_DOUBLE_EQ(bnd.area(0), 0.5);
+        EXPECT_EQ(bnd.vals(0), 5);
+
         bnd.destroy();
     }
     {
         auto label = m->get_label("slanted");
         auto bnd_facets = points_from_label(label);
-        TestBoundary3D bnd(m, &grad_phi, bnd_facets);
+        TestNaturalBoundary3D bnd(m, &grad_phi, bnd_facets);
         bnd.create();
+        bnd.compute();
+
+        EXPECT_EQ(bnd.num_facets(), 1);
+
         EXPECT_DOUBLE_EQ(bnd.normal(0)(0), 0.57735026918962584);
         EXPECT_DOUBLE_EQ(bnd.normal(0)(1), 0.57735026918962584);
         EXPECT_DOUBLE_EQ(bnd.normal(0)(2), 0.57735026918962584);
 
-        EXPECT_DOUBLE_EQ(bnd.nodal_normal(0)(0), 0.57735026918962584);
-        EXPECT_DOUBLE_EQ(bnd.nodal_normal(0)(1), 0.57735026918962584);
-        EXPECT_DOUBLE_EQ(bnd.nodal_normal(0)(2), 0.57735026918962584);
-        EXPECT_DOUBLE_EQ(bnd.nodal_normal(1)(0), 0.57735026918962584);
-        EXPECT_DOUBLE_EQ(bnd.nodal_normal(1)(1), 0.57735026918962584);
-        EXPECT_DOUBLE_EQ(bnd.nodal_normal(1)(2), 0.57735026918962584);
-        EXPECT_DOUBLE_EQ(bnd.nodal_normal(2)(0), 0.57735026918962584);
-        EXPECT_DOUBLE_EQ(bnd.nodal_normal(2)(1), 0.57735026918962584);
-        EXPECT_DOUBLE_EQ(bnd.nodal_normal(2)(2), 0.57735026918962584);
+        EXPECT_DOUBLE_EQ(bnd.facet_area(0), 0.5 * std::sqrt(3));
 
-        EXPECT_DOUBLE_EQ(bnd.area(0), 0.5 * std::sqrt(3));
+        EXPECT_EQ(bnd.vals(0), 8);
+
         bnd.destroy();
     }
 
