733a3393d8543ee13044fd2b263cf6d094c7ba19
David Andrs <andrsd@gmail.com>
2025-09-23 19:42:01 -0600

    Moving `compute_boundary_local`
    Moved from `TransientProblemInterface` to `ExplicitProblemInterface`.
    `TransientProblemInterface` should be agnostic of DM type while
    `compute_boundary_local` uses `DMPLEX`.

--- src/ExplicitFELinearProblem.cpp
+++ src/ExplicitFELinearProblem.cpp
@@ -130,7 +130,7 @@ ExplicitFELinearProblem::compute_solution_vector_local()
     CALL_STACK_MSG();
     auto & loc_sln = get_solution_vector_local();
     global_to_local(get_solution_vector(), INSERT_VALUES, loc_sln);
-    TransientProblemInterface::compute_boundary_local(get_time(), loc_sln);
+    ExplicitProblemInterface::compute_boundary_local(get_time(), loc_sln);
 }
 
 void
