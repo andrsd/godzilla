46b5e46094c55ebc201772d48b1180ae2c1de91d
David Andrs <andrsd@gmail.com>
2025-10-25 16:29:16 -0600

    Updating mesh-part tool

--- tools/mesh-part/main.cpp
+++ tools/mesh-part/main.cpp
@@ -3,53 +3,59 @@
 
 #include "cxxopts/cxxopts.hpp"
 #include "fmt/printf.h"
+#include "godzilla/Exception.h"
 #include "godzilla/Init.h"
 #include "godzilla/App.h"
+#include "godzilla/CommandLineInterface.h"
+#include "godzilla/MeshFactory.h"
+#include "godzilla/FileMesh.h"
 #include "godzilla/UnstructuredMesh.h"
 #include "godzilla/ExodusIIOutput.h"
+#include "mpicpp-lite/mpicpp-lite.h"
 
-const char * version = "1.0";
+const std::string version = "1.0";
 
-#if 0
 using namespace godzilla;
 
-class MeshPartApp : public App {
+mpi::Communicator comm_world;
+
+class MeshPartApp : public App, public CommandLineInterface {
 public:
     MeshPartApp(int argc, const char * const * argv);
 
-    void process_command_line(const cxxopts::ParseResult & result) override;
+    const std::string &
+    get_version() const override
+    {
+        return version;
+    }
+
+    int
+    run() override
+    {
+        return CommandLineInterface::run();
+    }
 
 protected:
-    void create_command_line_options() override;
-    MeshObject * load_mesh(const std::string & file_name);
+    cxxopts::Options create_command_line_options() override;
+    cxxopts::ParseResult parse_command_line(cxxopts::Options & opts) override;
+    void process_command_line(cxxopts::Options & opts,
+                              const cxxopts::ParseResult & result) override;
+    Qtr<UnstructuredMesh> load_mesh(const std::string & file_name);
     void partition_mesh_file(const std::string & mesh_file_name);
     void save_partition(UnstructuredMesh * mesh, const std::string & file_name);
 };
 
 MeshPartApp::MeshPartApp(int argc, const char * const * argv) :
-    App(mpi::Communicator(PETSC_COMM_WORLD), "mesh-part", argc, argv)
+    App(comm_world, "mesh-part"),
+    CommandLineInterface(*this, argc, argv)
 {
 }
 
-void
-MeshPartApp::process_command_line(const cxxopts::ParseResult & result)
-{
-    get_command_line_opts().parse_positional({ "mesh-file" });
-    auto res = parse_command_line();
-    if (res.count("help"))
-        fmt::print("{}", get_command_line_opts().help());
-    else if (res.count("version"))
-        fmt::print("{}, version {}\n", get_name(), get_version());
-    else if (res.count("mesh-file"))
-        partition_mesh_file(res["mesh-file"].as<std::string>());
-    else
-        fmt::print("{}", get_command_line_opts().help());
-}
-
-void
+cxxopts::Options
 MeshPartApp::create_command_line_options()
 {
-    auto opts = get_command_line_opts();
+    cxxopts::Options opts(get_app_name());
+
     opts.add_option("", "h", "help", "Show this help page", cxxopts::value<bool>(), "");
     opts.add_option("", "v", "version", "Show the version", cxxopts::value<bool>(), "");
     opts.add_option("",
@@ -60,28 +66,46 @@ MeshPartApp::create_command_line_options()
                     "");
     opts.add_option("", "", "mesh-file", "The mesh file name", cxxopts::value<std::string>(), "");
     opts.positional_help("<mesh-file>");
+    return opts;
 }
 
-MeshObject *
+cxxopts::ParseResult
+MeshPartApp::parse_command_line(cxxopts::Options & opts)
+{
+    opts.parse_positional({ "mesh-file" });
+    return CommandLineInterface::parse_command_line(opts);
+}
+
+void
+MeshPartApp::process_command_line(cxxopts::Options & opts, const cxxopts::ParseResult & res)
+{
+    if (res.count("help"))
+        fmt::print("{}", opts.help());
+    else if (res.count("version"))
+        fmt::print("{}, version {}\n", get_name(), get_version());
+    else if (res.count("mesh-file"))
+        partition_mesh_file(res["mesh-file"].as<std::string>());
+    else
+        fmt::print("{}", opts.help());
+}
+
+Qtr<UnstructuredMesh>
 MeshPartApp::load_mesh(const std::string & file_name)
 {
-    auto pars = ExodusIIMesh::parameters();
+    auto pars = FileMesh::parameters();
     pars.set<App *>("_app", this);
     pars.set<std::string>("file", file_name);
-    auto mesh = new ExodusIIMesh(pars);
-    mesh->create();
-    return mesh;
+    return MeshFactory::create<FileMesh>(pars);
 }
 
 void
 MeshPartApp::partition_mesh_file(const std::string & mesh_file_name)
 {
-    auto mesh_obj = load_mesh(mesh_file_name);
-    auto mesh = mesh_obj->get_mesh<UnstructuredMesh>();
+    auto mesh = load_mesh(mesh_file_name);
     mesh->distribute(0);
 
-    std::string out_file_name = fmt::format("out");
-    save_partition(mesh, out_file_name);
+    auto out_file_name = fmt::format("out");
+    save_partition(mesh.get(), out_file_name);
 }
 
 void
@@ -89,22 +113,26 @@ MeshPartApp::save_partition(UnstructuredMesh * mesh, const std::string & file_na
 {
     auto pars = ExodusIIOutput::parameters();
     pars.set<App *>("_app", this);
-    pars.set<UnstructuredMesh *>("_mesh", mesh);
     pars.set<std::string>("file", file_name);
     ExodusIIOutput out(pars);
-    out.create();
-    out.output_mesh();
+    // FIXME: this needs exodusII refactoring done
+    // out.create();
+    // out.output_mesh();
 }
-#endif
+
 // ---
 
 int
 main(int argc, char * argv[])
 {
-#if 0
     Init init(argc, argv);
     MeshPartApp app(argc, argv);
-    app.run();
-#endif
-    return 0;
+    try {
+        app.run();
+        return 0;
+    }
+    catch (Exception & e) {
+        fmt::println("{}", e.what());
+        return -1;
+    }
 }
