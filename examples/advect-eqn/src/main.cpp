c0783c6984dd7a0f8c246de2b23a197e8f81eb9a
David Andrs <andrsd@gmail.com>
2025-10-25 14:41:50 -0600

    Updating 'advect-eqn' example

--- examples/advect-eqn/src/main.cpp
+++ examples/advect-eqn/src/main.cpp
@@ -1,36 +1,64 @@
 #include "godzilla/App.h"
+#include "godzilla/CallStack.h"
 #include "godzilla/Init.h"
+#include "godzilla/MeshFactory.h"
 #include "godzilla/Registry.h"
+#include "godzilla/LineMesh.h"
+#include "godzilla/ExodusIIOutput.h"
 #include "AdvectionEquation.h"
 #include "InflowBC.h"
 #include "OutflowBC.h"
-
-godzilla::Registry registry;
-
-void
-register_objects(godzilla::Registry & r)
-{
-    godzilla::App::register_objects(r);
-    REGISTER_OBJECT(r, AdvectionEquation);
-    REGISTER_OBJECT(r, InflowBC);
-    REGISTER_OBJECT(r, OutflowBC);
-}
 
 int
 main(int argc, char * argv[])
 {
     try {
-        mpi::Communicator comm(MPI_COMM_WORLD);
+        mpi::Communicator comm;
         godzilla::Init init(argc, argv);
-        register_objects(registry);
 
-        godzilla::App app(comm, registry, "advect-eqn", argc, argv);
+        godzilla::App app(comm, "advect-eqn");
+
+        auto mesh_pars = LineMesh::parameters();
+        mesh_pars.set<godzilla::App *>("_app", &app)
+            .set<Int>("nx", 5)
+            .set<Real>("xmin", 0)
+            .set<Real>("xmax", 1);
+        auto mesh = MeshFactory::create<LineMesh>(mesh_pars);
+
+        auto prob_pars = AdvectionEquation::parameters();
+        prob_pars.set<godzilla::App *>("_app", &app)
+            .set<Mesh *>("mesh", mesh.get())
+            .set<Real>("dt", 1e-3)
+            .set<Real>("end_time", 5e-3)
+            .set<std::string>("scheme", "ssp-rk-2");
+        AdvectionEquation prob(prob_pars);
+        app.set_problem(&prob);
+
+        auto bc_left_pars = InflowBC::parameters();
+        bc_left_pars.set<godzilla::App *>("_app", &app)
+            .set<std::vector<std::string>>("boundary", { "left" })
+            .set<Real>("vel", 1.);
+        prob.add_boundary_condition<InflowBC>(bc_left_pars);
+
+        auto bc_right_pars = OutflowBC::parameters();
+        bc_right_pars.set<godzilla::App *>("_app", &app)
+            .set<std::vector<std::string>>("boundary", { "right" });
+        prob.add_boundary_condition<OutflowBC>(bc_right_pars);
+
+        auto out_pars = ExodusIIOutput::parameters();
+        out_pars.set<App *>("_app", &app);
+        out_pars.set<std::string>("file", "out");
+        prob.add_output<ExodusIIOutput>(out_pars);
+
+        prob.create();
+
         app.run();
 
         return 0;
     }
     catch (Exception & e) {
         fmt::println("{}", e.what());
+        print_call_stack(e.get_call_stack());
         return -1;
     }
     catch (...) {
