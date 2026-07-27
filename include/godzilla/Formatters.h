0ab39a77f052236b3bb04f703bdc4d7d87750542
David Andrs <andrsd@gmail.com>
2026-07-27 13:19:13 -0600

    Adding formatter for `PolytopeType`

--- include/godzilla/Formatters.h
+++ include/godzilla/Formatters.h
@@ -94,3 +94,51 @@ struct fmt::formatter<fs::path> {
         return fmt::format_to(ctx.out(), "{}", path.string());
     }
 };
+
+template <>
+struct fmt::formatter<godzilla::PolytopeType> {
+    constexpr auto
+    parse(fmt::format_parse_context & ctx)
+    {
+        return ctx.begin();
+    }
+
+    template <typename FormatContext>
+    auto
+    format(godzilla::PolytopeType e, FormatContext & ctx) const
+    {
+        switch (e) {
+        case godzilla::PolytopeType::POINT:
+            return fmt::format_to(ctx.out(), "POINT");
+        case godzilla::PolytopeType::SEGMENT:
+            return fmt::format_to(ctx.out(), "SEGMENT");
+        case godzilla::PolytopeType::POINT_PRISM_TENSOR:
+            return fmt::format_to(ctx.out(), "POINT_PRISM_TENSOR");
+        case godzilla::PolytopeType::TRIANGLE:
+            return fmt::format_to(ctx.out(), "TRIANGLE");
+        case godzilla::PolytopeType::QUADRILATERAL:
+            return fmt::format_to(ctx.out(), "QUADRILATERAL");
+        case godzilla::PolytopeType::SEG_PRISM_TENSOR:
+            return fmt::format_to(ctx.out(), "SEG_PRISM_TENSOR");
+        case godzilla::PolytopeType::TETRAHEDRON:
+            return fmt::format_to(ctx.out(), "TETRAHEDRON");
+        case godzilla::PolytopeType::HEXAHEDRON:
+            return fmt::format_to(ctx.out(), "HEXAHEDRON");
+        case godzilla::PolytopeType::PYRAMID:
+            return fmt::format_to(ctx.out(), "PYRAMID");
+        case godzilla::PolytopeType::TRI_PRISM:
+            return fmt::format_to(ctx.out(), "TRI_PRISM");
+        case godzilla::PolytopeType::TRI_PRISM_TENSOR:
+            return fmt::format_to(ctx.out(), "TRI_PRISM_TENSOR");
+        case godzilla::PolytopeType::QUAD_PRISM_TENSOR:
+            return fmt::format_to(ctx.out(), "QUAD_PRISM_TENSOR");
+        case godzilla::PolytopeType::FV_GHOST:
+            return fmt::format_to(ctx.out(), "FV_GHOST");
+        case godzilla::PolytopeType::INTERIOR_GHOST:
+            return fmt::format_to(ctx.out(), "INTERIOR_GHOST");
+        default:
+            return fmt::format_to(ctx.out(), "Unknow");
+        }
+        godzilla::utils::unreachable();
+    }
+};
